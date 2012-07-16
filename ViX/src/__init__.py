e694c1aed44119693f547ef7172ed41a7f370f32
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-07-16 01:28:11 +0100

    update translation files location.

--- ViX/src/__init__.py
+++ ViX/src/__init__.py
@@ -4,14 +4,13 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os,gettext
 
 PluginLanguageDomain = "ViX"
-PluginLanguagePath = "SystemPlugins/ViX/po"
+PluginLanguagePath = "SystemPlugins/ViX/locale"
 
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
 	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
 #	print "[ViX] set language to ", lang
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
-	gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE, ""))
 
 def _(txt):
 	t = gettext.dgettext(PluginLanguageDomain, txt)
