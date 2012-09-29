ce85b7d059544034f013083be8f317ee26d8c28f
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-09-29 18:18:53 +0100

    [BackupManager] change /var/keys to /usr/keys/ as /var/keys is a symlink.

--- ViX/src/BackupManager.py
+++ ViX/src/BackupManager.py
@@ -1142,8 +1142,8 @@ class BackupFiles(Screen):
 			self.selectedFiles.append('/usr/lib/sabnzbd')
 		if path.exists('/etc/samba') and not '/etc/samba' in self.selectedFiles:
 			self.selectedFiles.append('/etc/samba')
-		if path.exists('/var/keys') and not '/etc/CCcam.cfg' in self.selectedFiles:
-			self.selectedFiles.append('/var/keys')
+		if path.exists('/usr/keys') and not '/etc/CCcam.cfg' in self.selectedFiles:
+			self.selectedFiles.append('/usr/keys')
 
 		config.backupmanager.backupdirs.setValue(self.selectedFiles)
 		config.backupmanager.backupdirs.save()
