be570f5a04d41656ee34e697ff3d35f938891d85
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-11-29 15:18:15 +0000

    [Translation] update code.

--- ViX/src/__init__.py
+++ ViX/src/__init__.py
@@ -12,16 +12,14 @@ def localeInit():
 	else:
 		lang = language.getLanguage()[:2]
 	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-#	print "[ViX] set language to ", lang
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 def _(txt):
 	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
-		#print "[CrossEPG] fallback to default translation for", txt
-		t = gettext.dgettext('enigma2', txt)
+		print "[" + PluginLanguageDomain + "] fallback to default translation for", txt
+		t = gettext.gettext(txt)
 	return t
-
 
 localeInit()
 language.addCallback(localeInit)
