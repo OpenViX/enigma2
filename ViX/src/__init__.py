a8e1dd438e76ccecdfeb5eaea19a92df569f64b3
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-12-03 17:37:12 +0000

    [translations] tweak log code.

--- ViX/src/__init__.py
+++ ViX/src/__init__.py
@@ -7,17 +7,12 @@ PluginLanguageDomain = "ViX"
 PluginLanguagePath = "SystemPlugins/ViX/locale"
 
 def localeInit():
-	if os.path.exists(resolveFilename(SCOPE_PLUGINS, os.path.join(PluginLanguagePath, language.getLanguage()))):
-		lang = language.getLanguage()
-	else:
-		lang = language.getLanguage()[:2]
-	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 def _(txt):
 	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
-		print ("[" + PluginLanguageDomain + "] fallback to default translation for", txt)
+		print "[" + PluginLanguageDomain + "] fallback to default translation for " + txt
 		t = gettext.gettext(txt)
 	return t
 
