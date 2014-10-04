7925a3f1287b0259d2df6e32f33008397d5627e3
prl <prl@ozemail.com.au>
2014-10-04 10:39:31 +1000

    Fix bug #301 - [HelpMenuList, Rc] Bug fixes
    [HelpMenuList]
    Work around innovative use of Screen.helpList in
    Plugins.Extensions.MediaPlayer.MediaPlayer to prevent a UI crash
    when help is used in the MediaPlayer.
    Fix bug that prevented extended help entries from being displayed.
    
    [Rc]
    Fixed a potential crash bug when the remote id specified in
    config.misc.rcused was used. Doesn't appear to affect current
    production code.
    Removed some old debug prints.
    
    (cherry picked from commit f43f2571c911cee747e2e5c968f3a8d757937be6)
    (cherry picked from commit 3e70e257c6ea456e31a0cfa37f52bcc9d2bc3bcc)
    (cherry picked from commit 7ffd0e089b285213617df2992cfdcade29a4f373)

--- lib/python/Screens/Rc.py
+++ lib/python/Screens/Rc.py
@@ -76,7 +76,6 @@ class Rc:
 			self["rc"].setPixmapNum(config.misc.rcused.value)
 		else:
 			self["rc"].setPixmapNum(0)
-		print "[Rc] initRc", self.selectpics
 		rcHeight = self["rc"].getSize()[1]
 		for kp in self.selectpics:
 			nbreaks = len(kp.pixmaps)
@@ -84,7 +83,6 @@ class Rc:
 			n = 1
 			for pic in kp.pixmaps:
 				pic.activeYPos = (rcHeight * n + roundup) / nbreaks
-				print "[Rc]", pic.pixmapName, pic.getOffset(), pic.activeYPos
 				n += 1
 
 	def getRcPositions(self):
@@ -161,7 +159,7 @@ class RcPositions:
 			try:
 				self.rc = self.rcs[2]
 			except:
-				self.rc = self.rcs[config.misc.rcused.value]["keypos"]
+				self.rc = self.rcs[config.misc.rcused.value]
 
 	def getRc(self):
 		return self.rc
