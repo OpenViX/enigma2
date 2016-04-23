1658d3811d411464c827d521e31ac5b134824529
Andrew Blackburn <andyblac@icloud.com>
2016-04-23 01:33:11 +0100

    [core] cleanup.

--- ViX/src/plugin.py
+++ ViX/src/plugin.py
@@ -12,6 +12,7 @@ from SoftcamManager import SoftcamAutostart
 from ScriptRunner import ScriptRunnerAutostart
 from IPKInstaller import IpkgInstaller
 
+config.misc.restorewizardrun = ConfigBoolean(default=False)
 
 def checkConfigBackup():
 	try:
@@ -52,105 +53,74 @@ else:
 
 def VIXMenu(session):
 	import ui
-
 	return ui.VIXMenu(session)
-
 
 def UpgradeMain(session, **kwargs):
 	session.open(VIXMenu)
-
 
 def startSetup(menuid):
 	if menuid != "setup":
 		return []
 	return [(_("ViX"), UpgradeMain, "vix_menu", 1010)]
 
-
-config.misc.restorewizardrun = ConfigBoolean(default=False)
-
-
 def RestoreWizard(*args, **kwargs):
 	from RestoreWizard import RestoreWizard
-
 	return RestoreWizard(*args, **kwargs)
-
 
 def SoftcamManager(session):
 	from SoftcamManager import VIXSoftcamManager
-
 	return VIXSoftcamManager(session)
-
 
 def SoftcamMenu(session, **kwargs):
 	session.open(SoftcamManager)
-
 
 def SoftcamSetup(menuid):
 	if menuid == "cam":
 		return [(_("Softcam Manager"), SoftcamMenu, "softcamsetup", 1005)]
 	return []
 
-
 def BackupManager(session):
 	from BackupManager import VIXBackupManager
-
 	return VIXBackupManager(session)
-
 
 def BackupManagerMenu(session, **kwargs):
 	session.open(BackupManager)
 
-
 def ImageManager(session):
 	from ImageManager import VIXImageManager
-
 	return VIXImageManager(session)
-
 
 def ImageMangerMenu(session, **kwargs):
 	session.open(ImageManager)
 
-
 def MountManager(session):
 	from MountManager import VIXDevicesPanel
-
 	return VIXDevicesPanel(session)
-
 
 def MountManagerMenu(session, **kwargs):
 	session.open(MountManager)
 
-
 def ScriptRunner(session):
 	from ScriptRunner import VIXScriptRunner
-
 	return VIXScriptRunner(session)
-
 
 def ScriptRunnerMenu(session, **kwargs):
 	session.open(ScriptRunner)
 
-
 def SwapManager(session):
 	from SwapManager import VIXSwap
-
 	return VIXSwap(session)
-
 
 def SwapManagerMenu(session, **kwargs):
 	session.open(SwapManager)
-
 
 def filescan_open(list, session, **kwargs):
 	filelist = [x.path for x in list]
 	session.open(IpkgInstaller, filelist)  # list
 
-
 def filescan(**kwargs):
 	from Components.Scanner import Scanner, ScanPath
-
-	return \
-		Scanner(mimetypes=["application/x-debian-package"],
+	return Scanner(mimetypes=["application/x-debian-package"],
 				paths_to_scan=
 				[
 					ScanPath(path="ipk", with_subdirs=True),
@@ -176,12 +146,10 @@ def Plugins(**kwargs):
 	if config.misc.firstrun.value and not config.misc.restorewizardrun.value and backupAvailable == 1:
 		plist.append(PluginDescriptor(name=_("Restore Wizard"), where=PluginDescriptor.WHERE_WIZARD, needsRestart=False, fnc=(3, RestoreWizard)))
 	plist.append(PluginDescriptor(name=_("Ipkg"), where=PluginDescriptor.WHERE_FILESCAN, needsRestart=False, fnc=filescan))
-
 	plist.append(PluginDescriptor(name=_("ViX Backup Manager"), where=PluginDescriptor.WHERE_VIXMENU, fnc=BackupManagerMenu))
 	plist.append(PluginDescriptor(name=_("ViX Image Manager"), where=PluginDescriptor.WHERE_VIXMENU, fnc=ImageMangerMenu))
 	plist.append(PluginDescriptor(name=_("ViX Mount Manager"), where=PluginDescriptor.WHERE_VIXMENU, fnc=MountManagerMenu))
 	plist.append(PluginDescriptor(name=_("ViX Script Runner"), where=PluginDescriptor.WHERE_VIXMENU, fnc=ScriptRunnerMenu))
 	plist.append(PluginDescriptor(name=_("ViX Swap Manager"), where=PluginDescriptor.WHERE_VIXMENU, fnc=SwapManagerMenu))
-
 	return plist
 
