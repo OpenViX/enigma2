b8e5a7095879f8e1e0a5da53283e1badb66017ce
Huevos <rowland@moralet.com>
2017-08-22 20:56:46 +0200

    frontend.cpp: Skip delivery sytem test

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -2748,6 +2748,7 @@ int eDVBFrontend::isCompatibleWith(ePtr<iDVBFrontendParameters> &feparm)
 
 bool eDVBFrontend::supportsDeliverySystem(const fe_delivery_system_t &sys, bool obeywhitelist)
 {
+	return true;
 	std::map<fe_delivery_system_t, bool>::iterator it = m_delsys.find(sys);
 	if (it != m_delsys.end() && it->second)
 	{
