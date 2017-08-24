e406ad029eebc2b45224d555ba50152791971211
Huevos <rowland@moralet.com>
2017-08-24 18:24:52 +0200

    [frontend.cpp] skip simulate test

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -566,7 +566,8 @@ int eDVBFrontend::openFrontend()
 	m_state=stateIdle;
 	m_tuning=0;
 
-	if (!m_simulate)
+	//if (!m_simulate)
+	if (1)
 	{
 		eDebug("[eDVBFrontend] opening frontend %d", m_dvbid);
 		if (m_fd < 0)
@@ -675,8 +676,11 @@ int eDVBFrontend::openFrontend()
 		{
 			m_simulate_fe->m_delsys = m_delsys;
 		}
-		m_sn = eSocketNotifier::create(eApp, m_fd, eSocketNotifier::Read, false);
-		CONNECT(m_sn->activated, eDVBFrontend::feEvent);
+		if (!m_simulate)
+		{
+			m_sn = eSocketNotifier::create(eApp, m_fd, eSocketNotifier::Read, false);
+			CONNECT(m_sn->activated, eDVBFrontend::feEvent);
+		}
 	}
 	else
 	{
