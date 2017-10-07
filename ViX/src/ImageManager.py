4b399cef3e2d79c3519cbb6edc5a6d3afea55d09
ViX-Sicilian <stefd@hotmail.co.uk>
2017-10-07 10:26:12 +0100

    [ImageManager] Add Formuler F1 support (tc-transcoding version).

--- ViX/src/ImageManager.py
+++ ViX/src/ImageManager.py
@@ -1075,6 +1075,7 @@ class ImageManagerDownload(Screen):
 				'et8500'          : 'ET-8500',
 				'et9x00'          : 'ET-9x00',
 				'formuler4turbo'  : 'Formuler4turbo',
+				'formuler1tc'     : 'Formuler1tc',				
 				'gb800se'         : 'GiGaBlue-HD800SE',
 				'gb800seplus'     : 'GiGaBlue-HD800SE-PLUS',
 				'gb800ue'         : 'GiGaBlue-HD800UE',
