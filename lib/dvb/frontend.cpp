80e7b405b2b7ef24bef4690e4262e054508f12e0
Athanasios Oikonomou <athoik@gmail.com>
2017-11-18 10:11:38 +0200

    Add setting for forcing DVB API 3 when reading frontend data
    By default, frontend statistics will be read using the new DVB API 5 once available. The new setting forces reading frontend statistics using obsolete DVB API 3 instead.

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1246,7 +1246,7 @@ int eDVBFrontend::readFrontendData(int type)
 				int signalquality = 0;
 				int signalqualitydb = 0;
 #if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 10
-				if (m_dvbversion >= DVB_VERSION(5, 10))
+				if (m_dvbversion >= DVB_VERSION(5, 10) && !eConfigManager::getConfigBoolValue("config.usage.force_dvb_api_3", false))
 				{
 					dtv_property prop[1];
 					prop[0].cmd = DTV_STAT_CNR;
@@ -1308,7 +1308,7 @@ int eDVBFrontend::readFrontendData(int type)
 				if (!m_simulate)
 				{
 #if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 10
-					if (m_dvbversion >= DVB_VERSION(5, 10))
+					if (m_dvbversion >= DVB_VERSION(5, 10) && !eConfigManager::getConfigBoolValue("config.usage.force_dvb_api_3", false))
 					{
 						dtv_property prop[1];
 						prop[0].cmd = DTV_STAT_SIGNAL_STRENGTH;
