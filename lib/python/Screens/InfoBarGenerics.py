f40b855ad614af446839b93ea37bc3fe50cb6c51
rossi2000 <rossi_2000@hotmail.com>
2017-12-01 13:43:50 +0000

    [SystemInfo] add HasInfoButton

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1489,7 +1489,7 @@ class InfoBarEPG:
 
 	def InfoPressed(self):
 		if isStandardInfoBar(self) or isMoviePlayerInfoBar(self):
-			if getBrandOEM() in ('broadmedia', 'ceryon', 'dags', 'formuler', 'gfutures', 'gigablue', 'ini', 'octagon', 'odin', 'skylake', 'tiviar', 'xcore', 'xp', 'xtrend'):			
+			if SystemInfo['HasInfoButton']:
 				self.openEventView()
 			else:
 				self.showDefaultEPG()
