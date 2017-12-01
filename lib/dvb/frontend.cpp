d4d271117c97c366c8ce7a39c7bfa79589fbfc60
Huevos <rowland@moralet.com>
2017-12-01 18:45:29 +0100

    [frontend.cpp] Remove duplication

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1086,10 +1086,6 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 	{
 		ret = (int)((((double(snr) / (65535.0 / 100.0)) * 0.1800) - 1.0000) * 100);
 	}
-	else if (!strcmp(m_description, "DVB-S2 NIM(45208 FBC)"))
-	{
-		ret = (int)((((double(snr) / (65535.0 / 100.0)) * 0.1950) - 1.0000) * 100);
-	}
 	else if (!strcmp(m_description, "Vuplus DVB-S NIM(7376 FBC)")) // VU+ Solo4k
 	{
 		ret = (int)((((double(snr) / (65535.0 / 100.0)) * 0.1480) + 0.9560) * 100);
