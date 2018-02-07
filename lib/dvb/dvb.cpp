d1d19e6246760f124f6168e015f575588b61ac44
Athanasios Oikonomou <athoik@gmail.com>
2018-02-08 01:13:59 +0200

    dvb: make sure variable c is initialized
    This commit fixes the following warning:
    dvb/dvb.cpp: In member function 'RESULT eDVBResourceManager::allocateFrontend(ePtr<eDVBAllocatedFrontend>&, ePtr<iDVBFrontendParameters>&, bool)':
    dvb/dvb.cpp:869:4: warning: 'c' may be used uninitialized in this function [-Wmaybe-uninitialized]
        if (c > bestval)

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -847,6 +847,7 @@ RESULT eDVBResourceManager::allocateFrontend(ePtr<eDVBAllocatedFrontend> &fe, eP
 	foundone = 0;
 	check_fbc_leaf_linkable = false;
 	current_fbc_setid = -1;
+	c = 0;
 
 	for (eSmartPtrList<eDVBRegisteredFrontend>::iterator i(frontends.begin()); i != frontends.end(); ++i)
 	{
