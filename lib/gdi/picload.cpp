de8acc541a74e4e78cc5c3d6431d7b41bf587e06
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2018-02-12 16:55:19 +0000

    Revert "add upward compatibility to giflib-5"
    This reverts commit 0deb78b795cf7644c74c78d8cfe1b28d96fa620f.

--- lib/gdi/picload.cpp
+++ lib/gdi/picload.cpp
@@ -479,14 +479,7 @@ static void gif_load(Cfilepara* filepara, bool forceRGB = false)
 	int cmaps;
 	int extcode;
 
-#if !defined(GIFLIB_MAJOR) || ( GIFLIB_MAJOR < 5)
 	gft = DGifOpenFileName(filepara->file);
-#else
-	{
-		int err;
-		gft = DGifOpenFileName(filepara->file, &err);
-	}
-#endif
 	if (gft == NULL)
 		return;
 	do
@@ -576,24 +569,11 @@ static void gif_load(Cfilepara* filepara, bool forceRGB = false)
 	}
 	while (rt != TERMINATE_RECORD_TYPE);
 
-#if !defined(GIFLIB_MAJOR) || ( GIFLIB_MAJOR < 5) || (GIFLIB_MAJOR == 5 && GIFLIB_MINOR == 0)
 	DGifCloseFile(gft);
-#else
-	{
-		int err;
-		DGifCloseFile(gft, &err);
-	}
-#endif
 	return;
 ERROR_R:
-	eDebug("[Picload] <Error gif>");
+	eDebug("[ePicLoad] <Error gif>");
 	DGifCloseFile(gft);
-#else
-	{
-		int err;
-		DGifCloseFile(gft, &err);
-	}
-#endif
 }
 
 //---------------------------------------------------------------------------------------------
