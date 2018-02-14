de409f724398a1fb2e29c037f202693c95fc9805
Huevos <rowland@moralet.com>
2018-02-14 02:46:54 +0100

    Revert "[Jobview] Red button is active so give it some text"
    This reverts commit fab689f681cb46d25e527ddd7fbbee50e2a989d5.

--- lib/python/Screens/TaskView.py
+++ lib/python/Screens/TaskView.py
@@ -35,7 +35,6 @@ class JobView(InfoBarNotifications, Screen, ConfigListScreen):
 		self["backgroundable"] = Boolean(backgroundable)
 
 		self["key_blue"] = StaticText(_("Background"))
-		self["key_red"] = StaticText(_("Cancel"))
 
 		self.onShow.append(self.windowShow)
 		self.onHide.append(self.windowHide)
