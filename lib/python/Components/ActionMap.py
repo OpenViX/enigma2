948c011dadd251e19a8b392f6797b844d5d23d78
IanSav <IanSav@users.noreply.github.com>
2018-04-07 00:54:52 +1000

    [ActionMap] HelpableActionMaps multiple contexts (#253)
    Allow HelpableActionMap and HelpableNumberActionMap to accept a list of contexts as well as a single context. This makes it possible to convert ActionMap and NumberActionMap objects to helpable without changing their basic structure. Single-context helpable ActionMaps are retained for backwards compatibility.
    
    Simplify the ActionMap logging, print only one line instead of two for keymap errors.
    
    PEP8 clean-up and fixes.
    
    (This change will be required if the improved Beyonwiz HELP system is ever adopted.)
    
    Most of the code written by Prl for the Beyonwiz image (Nov-2014 - Jan-2015)

--- lib/python/Components/ActionMap.py
+++ lib/python/Components/ActionMap.py
@@ -1,9 +1,12 @@
 from enigma import eActionMap
+from Tools.KeyBindings import queryKeyBinding
 
 class ActionMap:
 	def __init__(self, contexts=None, actions=None, prio=0):
-		if not actions: actions = {}
-		if not contexts: contexts = []
+		if not actions:
+			actions = {}
+		if not contexts:
+			contexts = []
 		self.actions = actions
 		self.contexts = contexts
 		self.prio = prio
@@ -43,14 +46,14 @@ class ActionMap:
 		self.checkBind()
 
 	def action(self, context, action):
-		print " ".join(("[ActionMap]", context, action))
 		if action in self.actions:
+			print "[ActionMap] Keymap '%s' -> Action = '%s'" % (context, action)
 			res = self.actions[action]()
 			if res is not None:
 				return res
 			return 1
 		else:
-			print "[ActionMap] unknown action %s/%s! typo in keymap?" % (context, action)
+			print "[ActionMap] Keymap '%s' -> Unknown action '%s'! (Typo in keymap?)" % (context, action)
 			return 0
 
 	def destroy(self):
@@ -69,7 +72,8 @@ class NumberActionMap(ActionMap):
 class HelpableActionMap(ActionMap):
 	"""An Actionmap which automatically puts the actions into the helpList.
 
-	Note that you can only use ONE context here!"""
+	A context list is allowed, and for backward compatibility,
+	a single string context name also is allowed"""
 
 	# sorry for this complicated code.
 	# it's not more than converting a "documented" actionmap
@@ -79,26 +83,36 @@ class HelpableActionMap(ActionMap):
 	# the collected helpstrings (with correct context, action) is
 	# added to the screen's "helpList", which will be picked up by
 	# the "HelpableScreen".
-	def __init__(self, parent, context, actions=None, prio=0):
-		if not actions: actions = {}
-		alist = [ ]
-		adict = { }
-		for (action, funchelp) in actions.iteritems():
-			# check if this is a tuple
-			if isinstance(funchelp, tuple):
-				alist.append((action, funchelp[1]))
-				adict[action] = funchelp[0]
-			else:
-				adict[action] = funchelp
 
-		ActionMap.__init__(self, [context], adict, prio)
+	def __init__(self, parent, contexts, actions=None, prio=0, description=None):
+		self.description = description
+		if not actions:
+			actions = {}
+		if not hasattr(contexts, '__iter__'):
+			contexts = [contexts]
+		adict = {}
+		for context in contexts:
+			alist = []
+			for (action, funchelp) in actions.iteritems():
+				# check if this is a tuple
+				if isinstance(funchelp, tuple):
+					if queryKeyBinding(context, action):
+						alist.append((action, funchelp[1]))
+					adict[action] = funchelp[0]
+				else:
+					if queryKeyBinding(context, action):
+						alist.append((action, None))
+					adict[action] = funchelp
+			parent.helpList.append((self, context, alist))
 
-		parent.helpList.append((self, context, alist))
+		ActionMap.__init__(self, contexts, adict, prio)
 
-class HelpableNumberActionMap(ActionMap):
+
+class HelpableNumberActionMap(NumberActionMap, HelpableActionMap):
 	"""An Actionmap which automatically puts the actions into the helpList.
 
-	Note that you can only use ONE context here!"""
+	A context list is allowed, and for backward compatibility,
+	a single string context name also is allowed"""
 
 	# sorry for this complicated code.
 	# it's not more than converting a "documented" actionmap
@@ -108,29 +122,11 @@ class HelpableNumberActionMap(ActionMap):
 	# the collected helpstrings (with correct context, action) is
 	# added to the screen's "helpList", which will be picked up by
 	# the "HelpableScreen".
-	def __init__(self, parent, context, actions=None, prio=0):
-		if not actions: actions = {}
-		alist = [ ]
-		adict = { }
-		for (action, funchelp) in actions.iteritems():
-			# check if this is a tuple
-			if isinstance(funchelp, tuple):
-				alist.append((action, funchelp[1]))
-				adict[action] = funchelp[0]
-			else:
-				adict[action] = funchelp
 
-		ActionMap.__init__(self, [context], adict, prio)
+	def __init__(self, parent, contexts, actions=None, prio=0, description=None):
+		# Initialise NumberActionMap with empty context and actions
+		# so that the underlying ActionMap is only initialised with
+		# these once, via the HelpableActionMap.
 
-		parent.helpList.append((self, context, alist))
-
-	def action(self, contexts, action):
-		numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-		if action in numbers and self.actions.has_key(action):
-			res = self.actions[action](int(action))
-			if res is not None:
-				return res
-			return 1
-		else:
-			return ActionMap.action(self, contexts, action)
-
+		NumberActionMap.__init__(self, [], {})
+		HelpableActionMap.__init__(self, parent, contexts, actions, prio, description)
