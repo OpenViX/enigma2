b7323eb8b6f952443ee4cafa4d40906f07b664b3
Athanasios Oikonomou <athoik@gmail.com>
2018-09-01 14:10:18 +0300

    LoadPixmap: load SVG when extension is .svg
    Support loading svg in LoadPixmap when extension is .svg
    
    (cherry picked from commit af68be21ad929e1c4509c49812e7875b2c2e780a)

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,10 +1,13 @@
-from enigma import loadPNG, loadJPG
+from enigma import loadPNG, loadJPG, loadSVG
 
 def LoadPixmap(path, desktop = None, cached = False):
 	if path[-4:] == ".png":
 		ptr = loadPNG(path)
 	elif path[-4:] == ".jpg":
-		ptr = loadJPG(path)
+		# don't cache unless caller explicity requests caching
+		ptr = loadJPG(path, 1 if cached == True else 0)
+	elif path[-4:] == ".svg":
+		ptr = loadSVG(path)
 	elif path[-1:] == ".":
 		alpha = loadPNG(path + "a.png")
 		ptr = loadJPG(path + "rgb.jpg", alpha)
