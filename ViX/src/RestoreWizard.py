1fab78710a9d54a735d3cadb14a277c6540189ad
Huevos <rowland@moralet.com>
2018-09-11 03:50:02 +0200

    [RestoreWizard] Fix looping when network is not available

--- ViX/src/RestoreWizard.py
+++ ViX/src/RestoreWizard.py
@@ -270,9 +270,17 @@ class RestoreWizard(WizardLanguage, Rc):
 
 	def doRestorePluginsTestComplete(self, result=None, retval=None, extra_args=None):
 		print '[RestoreWizard] Stage 4: Feeds Test Result', result
-		if result.find('bad address') != -1:
+		if result.find('wget returned 4') != -1:
 			self.NextStep = 'reboot'
-			self.buildListRef = self.session.openWithCallback(self.buildListfinishedCB, MessageBox, _("Your %s %s is not connected to the internet, Please try using the Backup Manager to restore plugins later.") % (getMachineBrand(), getMachineName()), type=MessageBox.TYPE_INFO, timeout=30, wizard=True)
+			self.buildListRef = self.session.openWithCallback(self.buildListfinishedCB, MessageBox, _("Your %s %s is not connected to a network. Please try using the Backup Manager to restore plugins later when a network connection is available.") % (getMachineBrand(), getMachineName()), type=MessageBox.TYPE_INFO, timeout=30, wizard=True)
+			self.buildListRef.setTitle(_("Restore wizard"))
+		elif result.find('wget returned 8') != -1:
+			self.NextStep = 'reboot'
+			self.buildListRef = self.session.openWithCallback(self.buildListfinishedCB, MessageBox, _("Your %s %s could not connect to the plugin feeds at this time. Please try using the Backup Manager to restore plugins later.") % (getMachineBrand(), getMachineName()), type=MessageBox.TYPE_INFO, timeout=30, wizard=True)
+			self.buildListRef.setTitle(_("Restore wizard"))
+		elif result.find('bad address') != -1:
+			self.NextStep = 'reboot'
+			self.buildListRef = self.session.openWithCallback(self.buildListfinishedCB, MessageBox, _("Your %s %s is not connected to the internet. Please try using the Backup Manager to restore plugins later.") % (getMachineBrand(), getMachineName()), type=MessageBox.TYPE_INFO, timeout=30, wizard=True)
 			self.buildListRef.setTitle(_("Restore wizard"))
 		elif result.find('wget returned 1') != -1 or result.find('wget returned 255') != -1 or result.find('404 Not Found') != -1:
 			self.NextStep = 'reboot'
