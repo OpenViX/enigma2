5da609bf3499d5ebb08dae65f9b5c0eb6dd4d14f
Tony Whalley <awhalley@waitrose.com>
2018-10-07 14:18:12 +0200

    sf8008 fix E2 restart issue OpenATV 7677d32

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -35,6 +35,8 @@ def setLCDMiniTVMode(value):
 
 class Standby2(Screen):
 	def Power(self):
+		if getBoxType() in ('sf8008'):
+			open("/proc/stb/hdmi/output", "w").write("on")
 		print "[Standby] leave standby"
 		self.close(True)
 
@@ -106,6 +108,8 @@ class Standby2(Screen):
 			self.avswitch.setInput("SCART")
 		else:
 			self.avswitch.setInput("AUX")
+		if getBoxType() in ('sf8008'):
+			open("/proc/stb/hdmi/output", "w").write("off")
 		self.onFirstExecBegin.append(self.__onFirstExecBegin)
 		self.onClose.append(self.__onClose)
 
