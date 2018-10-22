488e61f9b3958761f51a6b20a4742ca6373350e9
Athanasios Oikonomou <athoik@gmail.com>
2018-10-22 14:53:39 +0300

    Add support for T2MI PLP ID
    This commit adds support for reading t2mi_plp_id from satellites.xml
    Also supports saving t2mi_plp_id using extendable v5 lamedb format.
    Of course v4 lamedb format still lives on with an extra field at the end.
    
    Additionally the t2mi_plp_id inserted again before the onid, tsid in tuple,
    like we did for MIS/PLS.
    We need to consider using dictionary or directly store the eDVBFrontendParametersSatellite/Cable/...
    instead of using tuple in the future.
    
    Finally this commit comes with a huge FIXME HACK ALERT. The T2MI PLP ID should be
    passed in drivers. But there is no field for that. So either a "crazy" hack like
    MIS/PLS required, where one property extra unused bits are used or create a
    new property and patch all kernels, uapi, etc. Option B can take years (although
    is the right way for the whole DVB ecosystem). Option A sucks, but it works.
    This time I choose to use an unused property from ISDBT (ISDBT users please don't
    shoot me if you plan to add support in enigma2...). Of course having a new
    T2MI PLP ID property in kernel is something to consider as soon as possible.
    
    The driver will set the lowest 2 bytes value given with 0x80000000 to t2mi_plp_id.
    (If there's 0x80000000, the driver will use the lowest 2 bytes value)

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -93,12 +93,14 @@ void eDVBFrontendParametersSatellite::set(const S2SatelliteDeliverySystemDescrip
 		is_id = descriptor.getInputStreamIdentifier();
 		pls_mode = eDVBFrontendParametersSatellite::PLS_Gold;
 		pls_code = descriptor.getScramblingSequenceIndex();
+		t2mi_plp_id = eDVBFrontendParametersSatellite::No_T2MI_PLP_Id;
 	}
 	else // default DVB-S2 physical layer scrambling sequence of index n = 0 is used
 	{
 		is_id = eDVBFrontendParametersSatellite::No_Stream_Id_Filter;
 		pls_mode = eDVBFrontendParametersSatellite::PLS_Gold;
 		pls_code = eDVBFrontendParametersSatellite::PLS_Default_Gold_Code;
+		t2mi_plp_id = eDVBFrontendParametersSatellite::No_T2MI_PLP_Id;
 	}
 }
 
@@ -129,9 +131,10 @@ void eDVBFrontendParametersSatellite::set(const SatelliteDeliverySystemDescripto
 	is_id = eDVBFrontendParametersSatellite::No_Stream_Id_Filter;
 	pls_mode = eDVBFrontendParametersSatellite::PLS_Gold;
 	pls_code = eDVBFrontendParametersSatellite::PLS_Default_Gold_Code;
+	t2mi_plp_id = eDVBFrontendParametersSatellite::No_T2MI_PLP_Id;
 	if (system == System_DVB_S2)
 	{
-		eDebug("[eDVBFrontendParametersSatellite] SAT DVB-S2 freq %d, %s, pos %d, sr %d, fec %d, modulation %d, rolloff %d, is_id %d, pls_mode %d, pls_code %d",
+		eDebug("[eDVBFrontendParametersSatellite] SAT DVB-S2 freq %d, %s, pos %d, sr %d, fec %d, modulation %d, rolloff %d, is_id %d, pls_mode %d, pls_code %d t2mi_plp_id %d",
 			frequency,
 			polarisation ? "hor" : "vert",
 			orbital_position,
@@ -140,7 +143,8 @@ void eDVBFrontendParametersSatellite::set(const SatelliteDeliverySystemDescripto
 			rolloff,
 			is_id,
 			pls_mode,
-			pls_code);
+			pls_code,
+			t2mi_plp_id);
 	}
 	else
 	{
@@ -368,6 +372,8 @@ RESULT eDVBFrontendParameters::calculateDifference(const iDVBFrontendParameters 
 			else if (sat.pls_mode != osat.pls_mode)
 				diff = 1<<27;
 			else if (sat.pls_code != osat.pls_code)
+				diff = 1<<27;
+			else if (sat.t2mi_plp_id != osat.t2mi_plp_id)
 				diff = 1<<27;
 			else if (exact && sat.fec != osat.fec && sat.fec != eDVBFrontendParametersSatellite::FEC_Auto && osat.fec != eDVBFrontendParametersSatellite::FEC_Auto)
 				diff = 1<<27;
@@ -1415,7 +1421,7 @@ void eDVBFrontend::getFrontendStatus(ePtr<iDVBFrontendStatus> &dest)
 void eDVBFrontend::getTransponderData(ePtr<iDVBTransponderData> &dest, bool original)
 {
 	int type = -1;
-	struct dtv_property p[16];
+	struct dtv_property p[18];
 	struct dtv_properties cmdseq;
 	oparm.getSystem(type);
 	cmdseq.props = p;
@@ -1442,6 +1448,7 @@ void eDVBFrontend::getTransponderData(ePtr<iDVBTransponderData> &dest, bool orig
 			{
 				p[cmdseq.num++].cmd = DTV_SCRAMBLING_SEQUENCE_INDEX;
 			}
+			p[cmdseq.num++].cmd = DTV_ISDBT_SB_SEGMENT_IDX; /* FIXME HACK ALERT use unused by enigma2 ISDBT SEGMENT IDX to pass T2MI PLP ID */
 		}
 		else if (type == feCable)
 		{
@@ -1987,7 +1994,7 @@ void eDVBFrontend::setFrontend(bool recvEvents)
 		if (recvEvents)
 			m_sn->start();
 		feEvent(-1); // flush events
-		struct dtv_property p[16];
+		struct dtv_property p[17];
 		struct dtv_properties cmdseq;
 		cmdseq.props = p;
 		cmdseq.num = 0;
@@ -2077,6 +2084,8 @@ void eDVBFrontend::setFrontend(bool recvEvents)
 				{
 					p[cmdseq.num].cmd = DTV_STREAM_ID, p[cmdseq.num].u.data = parm.is_id | (parm.pls_code << 8) | (parm.pls_mode << 26), cmdseq.num++;
 				}
+				/* FIXME HACK ALERT use unused by enigma2 ISDBT SEGMENT IDX to pass T2MI PLP ID */
+				p[cmdseq.num].cmd = DTV_ISDBT_SB_SEGMENT_IDX, p[cmdseq.num].u.data = (parm.t2mi_plp_id == NO_STREAM_ID_FILTER ? 0 : (0x80000000|parm.t2mi_plp_id)), cmdseq.num++;
 			}
 		}
 		else if (type == iDVBFrontend::feCable)
@@ -2339,7 +2348,7 @@ RESULT eDVBFrontend::prepare_sat(const eDVBFrontendParametersSatellite &feparm, 
 	res = m_sec->prepare(*this, feparm, satfrequency, 1 << m_slotid, tunetimeout);
 	if (!res)
 	{
-		eDebugNoSimulate("[eDVBFrontend%d] prepare_sat System %d Freq %d Pol %d SR %d INV %d FEC %d orbpos %d system %d modulation %d pilot %d, rolloff %d, is_id %d, pls_mode %d, pls_code %d",
+		eDebugNoSimulate("[eDVBFrontend%d] prepare_sat System %d Freq %d Pol %d SR %d INV %d FEC %d orbpos %d system %d modulation %d pilot %d, rolloff %d, is_id %d, pls_mode %d, pls_code %d, t2mi_plp_id %d",
 			m_dvbid,
 			feparm.system,
 			feparm.frequency,
@@ -2354,7 +2363,8 @@ RESULT eDVBFrontend::prepare_sat(const eDVBFrontendParametersSatellite &feparm, 
 			feparm.rolloff,
 			feparm.is_id,
 			feparm.pls_mode,
-			feparm.pls_code);
+			feparm.pls_code,
+			feparm.t2mi_plp_id);
 		if ((unsigned int)satfrequency < fe_info.frequency_min || (unsigned int)satfrequency > fe_info.frequency_max)
 		{
 			eDebugNoSimulate("[eDVBFrontend%d] %d mhz out of tuner range.. dont tune", m_dvbid, satfrequency / 1000);
