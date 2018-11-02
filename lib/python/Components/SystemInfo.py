37c2f985e1937d365a82f6952e3fd2b608b82408
Huevos <rowland@moralet.com>
2018-11-02 11:41:23 +0100

    [SystemInfo] Add comment

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -104,17 +104,17 @@ SystemInfo["no_YPbPr"] = getHaveYUV() in ('False',)
 SystemInfo["yellow_RCA_no_scart"] = getHaveSCART() in ('False',) and (getHaveRCA() in ('True',) or getHaveAVJACK() in ('True',))
 # Machines that have neither yellow RCA nor Scart sockets
 SystemInfo["no_yellow_RCA__no_scart"] = getHaveRCA() in ('False',) and (getHaveSCART() in ('False',) and getHaveAVJACK() in ('False',))
-SystemInfo["VideoModes"] = getChipSetString() in (
+SystemInfo["VideoModes"] = getChipSetString() in ( # 2160p and 1080p capable hardware
 		'5272s', '7251', '7251s', '7252', '7252s', '7366', '7376', '7444s', '72604', '3798mv200', '3798cv200', 'hi3798mv200', 'hi3798cv200'
 	) and (
 		["720p", "1080p", "2160p", "1080i", "576p", "576i", "480p", "480i"], # normal modes
 		{"720p", "1080p", "2160p", "1080i"} # widescreen modes
-	) or getChipSetString() in (
+	) or getChipSetString() in ( # 1080p capable hardware
 		'7241', '7356', '73565', '7358', '7362', '73625', '7424', '7425', '7552'
 	) and (
 		["720p", "1080p", "1080i", "576p", "576i", "480p", "480i"], # normal modes
 		{"720p", "1080p", "1080i"} # widescreen modes
-	) or ( # default modes
+	) or ( # default modes (neither 2160p nor 1080p capable hardware)
 		["720p", "1080i", "576p", "576i", "480p", "480i"], # normal modes
 		{"720p", "1080i"} # widescreen modes
 	)
