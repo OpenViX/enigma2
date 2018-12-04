9eecffbf55c28c5b13bfe34b4eb583cee170a354
prl <prl@ozemail.com.au>
2018-12-05 10:49:06 +1100

    [HelpMenu, HelpMenuList] Prepare for change of HelpMenuList to a source
    Move Components/HelpMenuList.py to Components/Sources/HelpMenuList.py,
    and adjust imports so that it works in the new location.
    
    No substantive changes.
    
    (cherry picked from commit 1056fbc6689dcf0e466b468428a71f9885977e1c)
    (cherry picked from commit a43eb6418a2e3cf5db3f22531e744831c1bda66c)
    (cherry picked from commit 5909af81681457d619ba40374e4c4a54a2698ca2)

--- lib/python/Components/Sources/HelpMenuList.py
+++ lib/python/Components/Sources/HelpMenuList.py
@@ -1,4 +1,4 @@
-from GUIComponent import GUIComponent
+from Components.GUIComponent import GUIComponent
 
 from enigma import eListboxPythonMultiContent, eListbox, gFont
 from Components.MultiContent import MultiContentEntryText
