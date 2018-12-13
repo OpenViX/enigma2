08e5e7222b094eca12f1a8882a2c057f67e243d0
Huevos <rowland@moralet.com>
2018-12-13 02:57:11 +0100

    [estring.cpp] comment out more noise

--- lib/base/estring.cpp
+++ lib/base/estring.cpp
@@ -630,8 +630,8 @@ std::string convertDVBUTF8(const unsigned char *data, int len, int table, int ts
 	if (pconvertedLen)
 		*pconvertedLen = convertedLen;
 
-	if (convertedLen < len)
-		eDebug("[convertDVBUTF8] %d chars converted, and %d chars left..", convertedLen, len-convertedLen);
+	//if (convertedLen < len)
+	//	eDebug("[convertDVBUTF8] %d chars converted, and %d chars left..", convertedLen, len-convertedLen);
 	//eDebug("[convertDVBUTF8] table=0x%02X twochar=%d output:%s\n", table, useTwoCharMapping, output.c_str());
 
 	//eDebug("[convertDVBUTF8] table=0x%02X tsid:onid=0x%X:0x%X data[0..14]=%s   output:%s\n",
