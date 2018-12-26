bf924fff3e42db322e6b22eab878f51ed30cb442
Taapat <taapat@gmail.com>
2018-12-26 14:16:01 +0200

    TimerEdit: show stop on red button for runing timers

--- lib/python/Screens/TimerEdit.py
+++ lib/python/Screens/TimerEdit.py
@@ -24,6 +24,7 @@ class TimerEditList(Screen):
 	DISABLE = 2
 	CLEANUP = 3
 	DELETE = 4
+	STOP = 5
 
 	def __init__(self, session, menu_path = ""):
 		Screen.__init__(self, session)
@@ -167,7 +168,11 @@ class TimerEditList(Screen):
 		cur = self["timerlist"].getCurrent()
 		if cur:
 			self["description"].setText(cur.description)
-			if self.key_red_choice != self.DELETE:
+			if cur.state == 2 and self.key_red_choice != self.STOP:
+				self["actions"].actions.update({"red":self.stopTimerQuestion})
+				self["key_red"].setText(_("Stop"))
+				self.key_red_choice = self.STOP
+			elif cur.state != 2 and self.key_red_choice != self.DELETE:
 				self["actions"].actions.update({"red":self.removeTimerQuestion})
 				self["key_red"].setText(_("Delete"))
 				self.key_red_choice = self.DELETE
@@ -282,6 +287,11 @@ class TimerEditList(Screen):
 			self.session.nav.RecordTimer.cleanup()
 			self.refill()
 			self.updateState()
+
+	def stopTimerQuestion(self):
+		cur = self["timerlist"].getCurrent()
+		if cur:
+			self.session.openWithCallback(self.removeTimer, MessageBox, _("Do you really want to stop current event and delete timer %s?") % (cur.name))
 
 	def removeTimerQuestion(self):
 		cur = self["timerlist"].getCurrent()
