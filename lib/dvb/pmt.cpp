0b9b6b97a6fccad4f963ac16b47caefc8eddf850
Huevos <rowland@moralet.com>
2018-12-30 20:07:33 +0100

    Revert "Add DRA/HEVC channel support"
    This reverts commit 4fce6e3a6403b044621de314b6dc8a2742abe2fb.

--- lib/dvb/pmt.cpp
+++ lib/dvb/pmt.cpp
@@ -375,7 +375,6 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 	int cached_apid_mpeg = -1;
 	int cached_apid_aache = -1;
 	int cached_apid_aac = -1;
-	int cached_apid_dra = -1;
 	int cached_vpid = -1;
 	int cached_tpid = -1;
 	int ret = -1;
@@ -398,7 +397,6 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		cached_apid_ddp = m_service->getCacheEntry(eDVBService::cDDPPID);
 		cached_apid_aache = m_service->getCacheEntry(eDVBService::cAACHEAPID);
 		cached_apid_aac = m_service->getCacheEntry(eDVBService::cAACAPID);
-		cached_apid_dra = m_service->getCacheEntry(eDVBService::cDRAAPID);
 		cached_tpid = m_service->getCacheEntry(eDVBService::cTPID);
 	}
 
@@ -413,7 +411,6 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		int autoaudio_ddp = -1;
 		int autoaudio_aache = -1;
 		int autoaudio_aac = -1;
-		int autoaudio_dra = -1;
 		int autoaudio_level = 4;
 
 		std::string configvalue;
@@ -478,8 +475,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 			 || program.audioStreams[i].pid == cached_apid_ddp
 			 || program.audioStreams[i].pid == cached_apid_mpeg
 			 || program.audioStreams[i].pid == cached_apid_aache
-			 || program.audioStreams[i].pid == cached_apid_aac
-			 || program.audioStreams[i].pid == cached_apid_dra)
+			 || program.audioStreams[i].pid == cached_apid_aac)
 			{
 				/* if we find the cached pids, this will be our default stream */
 				audio_cached = i;
@@ -490,8 +486,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 				|| (program.audioStreams[i].pid == cached_apid_ac4)
 				|| (program.audioStreams[i].pid == cached_apid_ddp)
 				|| (program.audioStreams[i].pid == cached_apid_aache)
-				|| (program.audioStreams[i].pid == cached_apid_aac)
-				|| (program.audioStreams[i].pid == cached_apid_dra)))
+				|| (program.audioStreams[i].pid == cached_apid_aac)))
 			{
 				first_non_mpeg = i;
 			}
@@ -521,8 +516,6 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 								autoaudio_aache = i;
 							else if (program.audioStreams[i].type == audioStream::atAAC && (autoaudio_level > x || autoaudio_aac == -1))
 								autoaudio_aac = i;
-							else if (program.audioStreams[i].type == audioStream::atDRA && (autoaudio_level > x || autoaudio_dra == -1))
-								autoaudio_dra = i;
 							autoaudio_level = x;
 							languageFound = true;
 							break;
@@ -696,15 +689,6 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 			audioStream s;
 			s.type = audioStream::atAAC;
 			s.pid = cached_apid_aac;
-			s.rdsPid = -1;
-			program.audioStreams.push_back(s);
-			++cnt;
-		}
-		if ( cached_apid_dra != -1 )
-		{
-			audioStream s;
-			s.type = audioStream::atDRA;
-			s.pid = cached_apid_dra;
 			s.rdsPid = -1;
 			program.audioStreams.push_back(s);
 			++cnt;
