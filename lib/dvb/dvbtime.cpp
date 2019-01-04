d47d33c1aa4467957a0606ec71cb2bb4f93d68ee
Huevos <rowland@moralet.com>
2019-01-05 00:50:19 +0100

    {dvbtime.cpp] add minimum "modified julian date"
    Do not allow dates from before time epoch
    
    Thanks Athoik with help with this solution

--- lib/dvb/dvbtime.cpp
+++ lib/dvb/dvbtime.cpp
@@ -110,6 +110,9 @@ static void parseDVBdate(tm& t, int mjd)
 {
 	int k;
 
+	if (mjd < 40587) // MJD time is before Time epoch, cannot be represented.
+		mjd = 40587; // use MDJ = 1970-01-01.
+	
 	t.tm_year = (int) ((mjd - 15078.2) / 365.25);
 	t.tm_mon = (int) ((mjd - 14956.1 - (int)(t.tm_year * 365.25)) / 30.6001);
 	t.tm_mday = (int) (mjd - 14956 - (int)(t.tm_year * 365.25) - (int)(t.tm_mon * 30.6001));
@@ -117,7 +120,7 @@ static void parseDVBdate(tm& t, int mjd)
 	t.tm_year = t.tm_year + k;
 	t.tm_mon = t.tm_mon - 1 - k * 12;
 	t.tm_mon--;
-
+	
 	t.tm_isdst =  0;
 	t.tm_gmtoff = 0;
 }
