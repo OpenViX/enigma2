acf4db36639ec08b289173b78194129db5e6b960
Ev0-BH <Ev0_BhT@outlook.com>
2019-01-17 21:36:45 +0000

    [TransponderInfo] fix small issue with some servers that use email address for username

--- lib/python/Components/Converter/TransponderInfo.py
+++ lib/python/Components/Converter/TransponderInfo.py
@@ -52,11 +52,9 @@ class TransponderInfo(Converter):
  					transponderdata["fec_inner"], transponderdata["modulation"], tsid, onid, transponderdata["detailed_satpos" in self.type and "orbital_position" or "orb_pos"])
 			except:
 				return ""
-		if "@" in ref:
-			return _("Stream") + " " + ref.rsplit("@", 1)[1].split("/")[0]
-		elif "://" in ref:
-			return _("Stream") + " " + ref.rsplit("://", 1)[1].split("/")[0]
-		return ""
+		if "://" in ref:
+			return _("Stream") + " " + ref.rsplit("://", 1)[1].split('/1:0:')[0].split('//')[0].split('/')[0].split('@')[-1]
+ 		return ""
 
 	text = property(getText)
 
