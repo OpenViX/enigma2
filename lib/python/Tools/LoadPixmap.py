71b1b9b7b5edf8ae8e66aa8d45d3d98eb248fb28
Simon Capewell <SimonCapewell@users.noreply.github.com>
2019-01-20 23:06:32 +0000

    Default cache strategy changed to png only

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -2,11 +2,18 @@ from enigma import loadPNG, loadJPG
 from Tools.LRUCache import lru_cache
 
 def LoadPixmap(path, desktop=None, cached=None):
-	if cached is None or cached:
+	# default to caching pngs, and not caching jpg (which tend not to be used by skins)
+	# PNGs are also statically cached in the c++ loadPNG method with no ability to clear or refresh, so there's
+	# currently no point adding file modified checking
+	# but there is still overhead fetching them back to the Python layer proportional to their size
+	# hence caching here which reduces load time to a constant <2ms
+	if cached is None:
+		cached = path[-4:] == ".png"
+	if cached:
 		ret = _cached_load(path, desktop)
-		return ret
 	else:
-		return _load(path, desktop)
+		ret = _load(path, desktop)
+	return ret
 
 @lru_cache(maxsize=256)
 def _cached_load(path, desktop):
