5197fd4fa0686e060a77ff7a3062658d9b41333f
Huevos <rowland@moralet.com>
2019-01-22 21:55:55 +0100

    [RecordTimer.py] Save to xml on clean up

--- RecordTimer.py
+++ RecordTimer.py
@@ -1351,3 +1351,11 @@ class RecordTimer(timer.Timer):
 
 	def shutdown(self):
 		self.saveTimer()
+
+	def cleanup(self):
+		timer.Timer.cleanup(self)
+		self.saveTimer()
+
+	def cleanupDaily(self, days):
+		timer.Timer.cleanupDaily(self, days)
+		self.saveTimer()
