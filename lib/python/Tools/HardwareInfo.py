e5f9cd47ff2f4d6371563176664b41a96e315e2c
Huevos <rowland@moralet.com>
2019-02-19 11:33:53 +0100

    [HardwareInfo] make "is_nextgen" dual core 750 MHz and above

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -53,6 +53,6 @@ class HardwareInfo:
 		return getBoxType() != 'dm800'
 
 	def is_nextgen(self):
-		if about.getCPUString() in ('BCM7346B2', 'BCM7425B2', 'BCM7429B0', 'ARMv7'):
-			return True
-		return False
+		if about.getCpuCoresInt() < 2 or about.getCPUSpeedMHzInt() < 750:
+			return False
+		return True
