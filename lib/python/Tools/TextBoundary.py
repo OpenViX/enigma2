c664ab104aac18d1802b6f99171be8019fbd3e9d
Simon Capewell <SimonCapewell@users.noreply.github.com>
2019-03-02 21:00:34 +0000

    More efficient getTextBoundarySize (#386)

--- lib/python/Tools/TextBoundary.py
+++ lib/python/Tools/TextBoundary.py
@@ -1,11 +1,7 @@
 from enigma import eLabel
 
-# Note that calling this function will result in a call to invalidate() 
-# on the instance object. This can be detrimental to UI performance, 
+# Calls onto the static function in eLabel. This avoids causing an invalidate
+# on the parent container which is detrimental to UI performance, 
 # particularly in a complex screen like the graph EPG
 def getTextBoundarySize(instance, font, targetSize, text):
-	dummy = eLabel(instance)
-	dummy.setFont(font)
-	dummy.resize(targetSize)
-	dummy.setText(text)
-	return dummy.calculateSize()
+	return eLabel.calculateTextSize(font, text, targetSize)
