42d2db098b2552328ef4859a0eb83bcd500fb4ef
littlesat <littlesat99@yahoo.com>
2019-03-04 20:51:05 +0100

    Standby: Streamline some code

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -267,7 +267,7 @@ class QuitMainloopScreen(Screen):
 
 inTryQuitMainloop = False
 
-def getReasons(session):
+def getReasons(session, retvalue=1):
 	recordings = session.nav.getRecordings()
 	jobs = len(job_manager.getPendingJobs())
 	reasons = []
@@ -284,32 +284,33 @@ def getReasons(session):
 			reasons.append((ngettext("%d job is running in the background!", "%d jobs are running in the background!", jobs) % jobs))
 	if eStreamServer.getInstance().getConnectedClients() or StreamServiceList:
 			reasons.append(_("Client is streaming from this box!"))
-	#if not reasons and internalHDDNotSleeping():
-	#		reasons.append(_("Harddisk is not in sleepmode it could be in use!"))
+#	if not reasons and internalHDDNotSleeping() and retvalue in (1, 2, 4, 42, 43):
+#			reasons.append(_("Harddisk is not in sleepmode it could be in use!"))
 	return "\n".join(reasons)
 
 class TryQuitMainloop(MessageBox):
-	def __init__(self, session, retvalue=1, timeout=-1, default_yes = False):
+	def __init__(self, session, retvalue=1, timeout=-1, default_yes=False, check_reasons=True):
 		self.retval = retvalue
 		self.connected = False
-		reason = getReasons(session)
-		if reason and not(internalHDDNotSleeping() and retvalue in (3, 6, 42)):
+		reason = check_reasons and getReasons(session, retvalue)
+		if reason:
 			text = { 1: _("Really shutdown your %s %s now?") % (MACHINEBRAND, MACHINENAME),
 				2: _("Really reboot your %s %s now?") % (MACHINEBRAND, MACHINENAME),
 				3: _("Really restart your %s %s now?") % (MACHINEBRAND, MACHINENAME),
 				4: _("Really upgrade the frontprocessor and reboot now?"),
 				42: _("Really upgrade your %s %s and reboot now?") % (MACHINEBRAND, MACHINENAME),
-				43: _("Really WOL now?") }.get(retvalue, _("Invalid reason"))
-			MessageBox.__init__(self, session, "%s\n%s" % (reason, text), type = MessageBox.TYPE_YESNO, timeout = timeout, default = default_yes)
-			self.skinName = "MessageBoxSimple"
-			session.nav.record_event.append(self.getRecordEvent)
-			self.connected = True
-			self.onShow.append(self.__onShow)
-			self.onHide.append(self.__onHide)
-		else:
-			self.skin = """<screen position="0,0" size="0,0"/>"""
-			Screen.__init__(self, session)
-			self.close(True)
+				43: _("Really WOL now?") }.get(retvalue, None)
+			if text:
+				MessageBox.__init__(self, session, "%s\n%s" % (reason, text), type=MessageBox.TYPE_YESNO, timeout=timeout, default=default_yes)
+				self.skinName = "MessageBoxSimple"
+				session.nav.record_event.append(self.getRecordEvent)
+				self.connected = True
+				self.onShow.append(self.__onShow)
+				self.onHide.append(self.__onHide)
+				return
+		self.skin = """<screen position="0,0" size="0,0"/>"""
+		Screen.__init__(self, session)
+		self.close(True)
 
 	def getRecordEvent(self, recservice, event):
 		#if event == iRecordableService.evEnd and checkTimeshiftRunning:
