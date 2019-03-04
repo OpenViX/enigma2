8e8274496a2b6356a1aeec52b8b5c035b8ece643
littlesat <littlesat99@yahoo.com>
2019-03-04 21:14:04 +0100

    NimManager: ensure hotswitchable is defined

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -514,6 +514,7 @@ class NIM(object):
 			}
 
 		# get multi type using delsys information
+		self.hotswitchable = False
 		if self.frontend_id is not None:
 			types = [type for type in nim_types if eDVBResourceManager.getInstance().frontendIsCompatible(self.frontend_id, type)]
 			print "[NIM] get types from delsys", types
