35db3944ce1a9711a4036453a5daf85315293aac
Persian Prince <persianpros@gmail.com>
2019-03-06 14:28:11 +0330

    Standby.py: Remove useless import
    I did cleanup all enimga2 python files before of useless imports, please keep them clean :(

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -14,9 +14,7 @@ from Components.SystemInfo import SystemInfo
 from GlobalActions import globalActionMap
 from enigma import eDVBVolumecontrol, eTimer, eDVBLocalTimeHandler, eServiceReference, eStreamServer
 from Components.Sources.StreamService import StreamServiceList
-import Screens.InfoBar
 from boxbranding import getMachineBrand, getMachineName, getBoxType
-from Tools.HardwareInfo import HardwareInfo
 
 inStandby = None
 
