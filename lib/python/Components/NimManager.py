02073cf50b7d1f4c72362b43f55418c6b453abeb
Huevos <rowland@moralet.com>
2019-03-20 10:01:56 +0100

    Revert "configModeDVBS default to True so when box is updated it is able to tune"
    This reverts commit b01a8c1f0a4b7076b6d1a1b1f242d7af0bcb6af9.

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1620,7 +1620,7 @@ def InitNimManager(nimmgr, update_slots = []):
 			if slot.type is not None:
 				print "[InitNimManager] pls add support for this frontend type!", slot.type
 
-		nim.configModeDVBS = ConfigYesNo(default=True)
+		nim.configModeDVBS = ConfigYesNo()
 		nim.configModeDVBC = ConfigYesNo()
 		nim.configModeDVBT = ConfigYesNo()
 		nim.configModeDVBATSC = ConfigYesNo()
