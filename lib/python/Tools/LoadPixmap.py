dec45bccb0525ab1768b45fe7a67dd90d560dd84
teamblue-e2 <teamblue@email.de>
2019-04-15 18:20:14 +0200

    Revert "Pixmap caching"
    This reverts commit 8984de462ea6ed66621a56bd9bde22a32cbdd3eb.

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,14 +1,13 @@
 from enigma import loadPNG, loadJPG
 
-# if cached is not supplied, defaults to caching PNGs and not caching JPGs
-def LoadPixmap(path, desktop=None, cached=None):
+def LoadPixmap(path, desktop = None, cached = False):
 	if path[-4:] == ".png":
-		ptr = loadPNG(path, 0, 0 if cached == 0 else 1)
+		ptr = loadPNG(path)
 	elif path[-4:] == ".jpg":
-		ptr = loadJPG(path, 0, 1 if cached == 1 else 0)
+		ptr = loadJPG(path)
 	elif path[-1:] == ".":
-		alpha = loadPNG(path + "a.png", 0)
-		ptr = loadJPG(path + "rgb.jpg", alpha, 0)
+		alpha = loadPNG(path + "a.png")
+		ptr = loadJPG(path + "rgb.jpg", alpha)
 	else:
 		raise Exception("neither .png nor .jpg, please fix file extension")
 	if ptr and desktop:
