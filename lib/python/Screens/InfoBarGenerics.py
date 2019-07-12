89d12234484bfb000506133808032d249df94906
littlesat <littlesat99@yahoo.com>
2019-07-13 00:40:19 +0200

    Remove some totally not required check
    (cherry picked from commit c0d39b5a259a231a9d9874c9402394157437c51d)

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2931,7 +2931,7 @@ class InfoBarSubserviceSelection:
 		if serviceRef:
 			service = self.session.nav.getCurrentService()
 			subservices = getActiveSubservicesForCurrentChannel(service)
-			if subservices and len(subservices) > 1 and serviceRef.toString() in [x[1] for x in subservices]:
+			if subservices and serviceRef.toString() in [x[1] for x in subservices]:
 				selection = [x[1] for x in subservices].index(serviceRef.toString())
 				selection += direction % len(subservices)
 				try:
@@ -2946,7 +2946,7 @@ class InfoBarSubserviceSelection:
 		if serviceRef:
 			service = self.session.nav.getCurrentService()
 			subservices = getActiveSubservicesForCurrentChannel(service)
-			if subservices and len(subservices) > 1 and (serviceRef.toString() in [x[1] for x in subservices] or service.subServices()):
+			if subservices and (serviceRef.toString() in [x[1] for x in subservices] or service.subServices()):
 				try:
 					selection = [x[1] for x in subservices].index(serviceRef.toString())
 				except:
