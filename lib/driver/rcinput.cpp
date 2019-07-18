88a9764657a547a0440d2f4ede6ac04a82e75f00
teamblue-e2 <teamblue@email.de>
2019-07-18 22:48:04 +0200

    gb7252 fix again KEY_CONTEXT_MENU...

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -132,7 +132,7 @@ void eRCDeviceInputDev::handleCode(long rccode)
 #endif
 
 #if KEY_CONTEXT_MENU_TO_KEY_AUX
-	if (ev->code == KEY_CONTENT_MENU)
+	if (ev->code == KEY_CONTEXT_MENU)
 	{
 		/* define when rc sends a KEY_CONTEXT_MENU event for its HDMIIN functionality */
 		ev->code = KEY_AUX;
