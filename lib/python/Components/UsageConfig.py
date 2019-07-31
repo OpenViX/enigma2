94959ce88ca0b6ba22a0fd58936a8eabddff1e41
LraiZer <lbound@hotmail.com>
2019-07-31 19:19:19 +0100

    epgcache: add opentv eit parser
    opentv eit parser only starts on found transponder dictionaries.
    EPG transponders are defined in the huffman dictionary filename,
    dict filenames take the form: otv_{namespace}_{onid}_{tsid}.dict
    
    eg. OpenTV EPG dictionary for 28.2E: otv_011a0000_0002_07d4.dict

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -684,6 +684,7 @@ def InitUsageConfig():
 	config.epg.viasat = ConfigYesNo(default = True)
 	config.epg.netmed = ConfigYesNo(default = True)
 	config.epg.virgin = ConfigYesNo(default = True)
+	config.epg.opentv = ConfigYesNo(default = False)
 
 	def EpgSettingsChanged(configElement):
 		mask = 0xffffffff
@@ -699,6 +700,8 @@ def InitUsageConfig():
 			mask &= ~(eEPGCache.NETMED_SCHEDULE | eEPGCache.NETMED_SCHEDULE_OTHER)
 		if not config.epg.virgin.value:
 			mask &= ~(eEPGCache.VIRGIN_NOWNEXT | eEPGCache.VIRGIN_SCHEDULE)
+		if not config.epg.opentv.value:
+			mask &= ~eEPGCache.OPENTV
 		eEPGCache.getInstance().setEpgSources(mask)
 	config.epg.eit.addNotifier(EpgSettingsChanged)
 	config.epg.mhw.addNotifier(EpgSettingsChanged)
@@ -706,6 +709,7 @@ def InitUsageConfig():
 	config.epg.viasat.addNotifier(EpgSettingsChanged)
 	config.epg.netmed.addNotifier(EpgSettingsChanged)
 	config.epg.virgin.addNotifier(EpgSettingsChanged)
+	config.epg.opentv.addNotifier(EpgSettingsChanged)
 
 	config.epg.histminutes = ConfigSelectionNumber(min = 0, max = 120, stepwidth = 15, default = 0, wraparound = True)
 	def EpgHistorySecondsChanged(configElement):
