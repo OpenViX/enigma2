92e8fa8881d7bc22d16676fccaa3d73393d65004
Huevos <rowland@moralet.com>
2019-08-10 21:38:36 +0200

    {GitCommitInfo] read from own repo

--- lib/python/Screens/GitCommitInfo.py
+++ lib/python/Screens/GitCommitInfo.py
@@ -28,7 +28,7 @@ E2Branches = {
 project = 0
 projects = [
 	("https://api.github.com/repos/oe-alliance/oe-alliance-core/commits?sha=4.3", "OE-A Core"),
-	("https://api.github.com/repos/OpenViX/enigma2/commits?sha=%s" % getattr(E2Branches, getImageType(), "Dev"), "Enigma2"),
+	("https://api.github.com/repos/Huevos/enigma2/commits?sha=%s" % getattr(E2Branches, getImageType(), "Dev"), "Enigma2"),
 	("https://api.github.com/repos/OpenViX/vix-core/commits", "ViX Core"),
 	("https://api.github.com/repos/OpenViX/skins/commits", "ViX Skins"),
 	("https://api.github.com/repos/oe-alliance/oe-alliance-plugins/commits", "OE-A Plugins"),
