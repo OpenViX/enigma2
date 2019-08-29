86920778b2928915640bae01c778d256ee306d5d
Littlesat <littlesat99@yahoo.com>
2019-08-29 18:40:01 +0200

    ChannelSelection: Toggle two line channellist with teletext button

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1347,6 +1347,7 @@ class ChannelSelectionBase(Screen):
 				"keyLeft": self.keyLeft,
 				"keyRight": self.keyRight,
 				"keyRecord": self.keyRecord,
+				"toggleTwoLines": self.toggleTwoLines,
 				"1": self.keyNumberGlobal,
 				"2": self.keyNumberGlobal,
 				"3": self.keyNumberGlobal,
@@ -1724,6 +1725,13 @@ class ChannelSelectionBase(Screen):
 		ref = self.getCurrentSelection()
 		if ref and not(ref.flags & (eServiceReference.isMarker|eServiceReference.isDirectory)):
 			Screens.InfoBar.InfoBar.instance.instantRecord(serviceRef=ref)
+
+	def toggleTwoLines(self):
+		if self.servicelist.mode == self.servicelist.MODE_FAVOURITES:
+			config.usage.servicelist_twolines.value = not config.usage.servicelist_twolines.value
+			config.usage.servicelist_twolines.save()
+		else:
+			return 0
 
 	def showFavourites(self):
 		self["key_green"].setText(_("Reception lists"))
