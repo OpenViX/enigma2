3a57a4f98d49971d7b96dc405d7bfa15a1ad80e7
TwolDE2 <tonywhalley@t-online.de>
2019-09-09 15:04:23 +0200

    Language allow deletion Fr & De languages, use EvO's English text in LanguageSelection delete msg" (#462)
    * Language - allow delete of Fr and De languages, completely block En deletion
    
    * LanguageSelection - add EvO's English text

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -136,7 +136,7 @@ class LanguageSelection(Screen):
 			if curlang == t[0]:
 				lang = t[1]
 				break
-		self.session.openWithCallback(self.delLangCB, MessageBox, _("To delete selected language , answer 'No'\n 'Yes', to delete all other languages,except English, French, German and your selection:\n\n") + _("%s") %(lang), default = False)
+		self.session.openWithCallback(self.delLangCB, MessageBox, _("Select 'Yes' to delete all languages except English and your chosen language:\n\nSelect 'No' to delete only the chosen language:\n\n") + _("%s") %(lang), default = False)
 
 	def delLangCB(self, answer):
 		if answer:
