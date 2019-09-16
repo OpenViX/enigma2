ff13308ebe0c75155e21775899118bd5ed6db40b
Littlesat <reijng1@medtronic.com>
2019-09-16 09:09:26 +0200

    Revert "Harddisk.py: Replace missing os.system with Console().ePopen"

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -294,7 +294,7 @@ class Harddisk:
 			return 0
 		cmd = 'umount ' + dev
 		print "[Harddisk] ", cmd
-		res = Console().ePopen(cmd)
+		res = os.system(cmd)
 		return (res >> 8)
 
 	def createPartition(self):
