a205f73da2a578bd7335280cc975e12618deec16
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2019-09-19 22:43:48 +0100

    Revert "Remove superfluous usage of skin_default in image paths, don't hardcode these kind of things. (#70)" (#75)
    This reverts commit 69e6d877d798b8fa2182d0376019f8aff36956bf.

--- ViX/src/IPKInstaller.py
+++ ViX/src/IPKInstaller.py
@@ -19,9 +19,9 @@ from Screens.Standby import TryQuitMainloop
 class VIXIPKInstaller(Screen):
 	skin = """
 	<screen name="VIXIPKInstaller" position="center,center" size="560,400">
-		<ePixmap pixmap="buttons/red.png" position="0,0" size="140,40" alphatest="on"/>
-		<ePixmap pixmap="buttons/green.png" position="140,0" size="140,40" alphatest="on"/>
-		<ePixmap pixmap="buttons/yellow.png" position="280,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/yellow.png" position="280,0" size="140,40" alphatest="on"/>
 		<widget name="key_red" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1"/>
 		<widget name="key_green" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1"/>
 		<widget name="key_yellow" position="280,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1"/>
@@ -162,16 +162,16 @@ class VIXIPKInstaller(Screen):
 class IpkgInstaller(Screen):
 	skin = """
 		<screen name="IpkgInstaller" position="center,center" size="550,450" >
-			<ePixmap pixmap="buttons/red.png" position="0,0" size="140,40" alphatest="on"/>
-			<ePixmap pixmap="buttons/green.png" position="140,0" size="140,40" alphatest="on"/>
-			<ePixmap pixmap="buttons/yellow.png" position="280,0" size="140,40" alphatest="on"/>
-			<ePixmap pixmap="buttons/blue.png" position="420,0" size="140,40" alphatest="on"/>
+			<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on"/>
+			<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on"/>
+			<ePixmap pixmap="skin_default/buttons/yellow.png" position="280,0" size="140,40" alphatest="on"/>
+			<ePixmap pixmap="skin_default/buttons/blue.png" position="420,0" size="140,40" alphatest="on"/>
 			<widget source="key_red" render="Label" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1"/>
 			<widget source="key_green" render="Label" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1"/>
 			<widget source="key_yellow" render="Label" position="280,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#a08500" transparent="1"/>
 			<widget source="key_blue" render="Label" position="420,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#18188b" transparent="1"/>
 			<widget name="list" position="5,50" size="540,360"/>
-			<ePixmap pixmap="div-h.png" position="0,410" zPosition="10" size="560,2" transparent="1" alphatest="on"/>
+			<ePixmap pixmap="skin_default/div-h.png" position="0,410" zPosition="10" size="560,2" transparent="1" alphatest="on"/>
 			<widget source="introduction" render="Label" position="5,420" zPosition="10" size="550,30" halign="center" valign="center" font="Regular;22" transparent="1" shadowColor="black" shadowOffset="-1,-1"/>
 		</screen>"""
 
--- ViX/src/SwapManager.py
+++ ViX/src/SwapManager.py
@@ -77,16 +77,16 @@ class StartSwap:
 class VIXSwap(Screen):
 	skin = """
 	<screen name="VIXSwap" position="center,center" size="620,250">
-		<ePixmap pixmap="buttons/red.png" position="10,0" size="140,40" alphatest="on"/>
-		<ePixmap pixmap="buttons/green.png" position="160,0" size="140,40" alphatest="on"/>
-		<ePixmap pixmap="buttons/yellow.png" position="310,0" size="140,40" alphatest="on"/>
-		<ePixmap pixmap="buttons/blue.png" position="460,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/red.png" position="10,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/green.png" position="160,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/yellow.png" position="310,0" size="140,40" alphatest="on"/>
+		<ePixmap pixmap="skin_default/buttons/blue.png" position="460,0" size="140,40" alphatest="on"/>
 		<widget name="key_red" position="10,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1"/>
 		<widget name="key_green" position="160,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1"/>
 		<widget name="key_yellow" position="310,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#a08500" transparent="1"/>
 		<widget name="key_blue" position="460,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1"/>
-		<widget name="autostart_off" position="10,50" zPosition="1" pixmap="icons/lock_off.png" size="32,32" alphatest="on"/>
-		<widget name="autostart_on" position="10,50" zPosition="2" pixmap="icons/lock_on.png" size="32,32" alphatest="on"/>
+		<widget name="autostart_off" position="10,50" zPosition="1" pixmap="skin_default/icons/lock_off.png" size="32,32" alphatest="on"/>
+		<widget name="autostart_on" position="10,50" zPosition="2" pixmap="skin_default/icons/lock_on.png" size="32,32" alphatest="on"/>
 		<widget name="lab1" position="50,50" size="360,30" font="Regular;20" valign="center" transparent="1"/>
 		<widget name="lab2" position="10,100" size="150,30" font="Regular;20" valign="center" transparent="1"/>
 		<widget name="lab3" position="10,150" size="150,30" font="Regular;20" valign="center" transparent="1"/>
