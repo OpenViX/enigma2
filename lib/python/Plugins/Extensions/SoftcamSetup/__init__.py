98cc8d9a1cf8ecc0cf8488eb9f7e9c1f4f8f83f1
norhap <alvaro@elfogondesegovia.com>
2019-11-15 08:53:42 +0100

    Revert "Update __init__.py"
    This reverts commit 6b4f5b0f2a4415c29c76b34b6ce34fdc8808beff.

--- lib/python/Plugins/Extensions/SoftcamSetup/__init__.py
+++ lib/python/Plugins/Extensions/SoftcamSetup/__init__.py
@@ -1,2 +1,22 @@
 # Embedded file name: /usr/lib/enigma2/python/Plugins/Extensions/SoftcamSetup/__init__.py
-pass
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+from os import environ as os_environ
+import gettext
+
+def localeInit():
+    lang = language.getLanguage()[:2]
+    os_environ['LANGUAGE'] = lang
+    gettext.bindtextdomain('SoftcamSetup', resolveFilename(SCOPE_PLUGINS, 'PLi/SoftcamSetup/locale'))
+
+
+def _(txt):
+    t = gettext.dgettext('SoftcamSetup', txt)
+    if t == txt:
+        print '[SoftcamSetup] fallback to default translation for', txt
+        t = gettext.gettext(txt)
+    return t
+
+
+localeInit()
+language.addCallback(localeInit)
