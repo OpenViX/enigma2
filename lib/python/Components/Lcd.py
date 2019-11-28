e2e94851228857ee4448c08f5f3c94f590b81c5f
norhap <alvaro@elfogondesegovia.com>
2019-11-28 20:29:20 +0100

    Update Lcd.py

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -243,7 +243,7 @@ def InitLcd():
 	if getBoxType() in ('et4x00', 'et5x00', 'et6x00', 'gb800se', 'gb800solo', 'iqonios300hd', 'mbmicro', 'sf128', 'sf138', 'tmsingle', 'tmnano2super', 'tmnanose', 'tmnanoseplus', 'tmnanosem2', 'tmnanosem2plus', 'tmnanosecombo', 'vusolo'):
 		detected = False
 	elif getBoxType() in ('gbtrio4k',):
-		detected = True		
+		detected = True
 	else:
 		detected = eDBoxLCD.getInstance().detected()
 
