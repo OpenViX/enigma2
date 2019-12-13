ccb40fab1be1898ab556d65ccc1af425f4a3528b
Huevos <rowland@moralet.com>
2019-12-13 16:28:55 +0100

    [NextEpgInfo.py] add colour conversion
    Based on work of nautilus7.
    
    Requires new file "Tools/Hex2strColor.py".
    
    Please test thoroughly.

--- lib/python/Components/Renderer/NextEpgInfo.py
+++ lib/python/Components/Renderer/NextEpgInfo.py
@@ -2,7 +2,9 @@ from Components.config import config
 from Components.VariableText import VariableText
 from Renderer import Renderer
 from enigma import eLabel, eEPGCache, eServiceReference
-from time import localtime, strftime
+from time import time, localtime, strftime
+from skin import parseColor
+from Tools.Hex2strColor import Hex2strColor
 
 class NextEpgInfo(Renderer, VariableText):
 	def __init__(self):
@@ -10,6 +12,12 @@ class NextEpgInfo(Renderer, VariableText):
 		VariableText.__init__(self)
 		self.epgcache = eEPGCache.getInstance()
 		self.numberOfItems = 1
+		self.hideLabel = 0
+		self.timecolor = ""
+		self.labelcolor = ""
+		self.foregroundColor = "\c00?0?0?0"
+		self.numOfSpaces = 1
+
 	GUI_WIDGET = eLabel
 
 	def changed(self, what):
@@ -18,22 +26,48 @@ class NextEpgInfo(Renderer, VariableText):
 		info = reference and self.source.info
 		if info:
 			currentEvent = self.source.getCurrentEvent()
-			if currentEvent:
-				if not self.epgcache.startTimeQuery(eServiceReference(reference.toString()), currentEvent.getBeginTime() + currentEvent.getDuration()):
-					if self.numberOfItems == 1:
+			if not self.epgcache.startTimeQuery(eServiceReference(reference.toString()), currentEvent.getBeginTime() + currentEvent.getDuration() if currentEvent else int(time())):
+				spaces = " " * self.numOfSpaces
+				if self.numberOfItems == 1:
+					event = self.epgcache.getNextTimeEntry()
+					if event:
+						if self.hideLabel:
+							self.text = "%s%s%s%s%s" % (self.timecolor, strftime(config.usage.time.short.value, localtime(event.getBeginTime())), spaces, self.foregroundColor, event.getEventName())
+						else:
+							self.text = "%s%s:%s%s%s" % (self.labelcolor, pgettext("now/next: 'next' event label", "Next"), spaces, self.foregroundColor, event.getEventName())
+				else:
+					for x in range(self.numberOfItems):
 						event = self.epgcache.getNextTimeEntry()
 						if event:
-							self.text = "%s: %s" % (pgettext("now/next: 'next' event label", "Next"), event.getEventName())
-					else:
-						for x in range(self.numberOfItems):
-							event = self.epgcache.getNextTimeEntry()
-							if event:
-								self.text = "%s\n%s %s" % (self.text, strftime(config.usage.time.short.value, localtime(event.getBeginTime())), event.getEventName())
-						self.text = self.text and "%s%s" % (pgettext("now/next: 'next' event label", "Next"), self.text) or ""
+							self.text += "%s%s%s%s%s\n" % (self.timecolor, strftime(config.usage.time.short.value, localtime(event.getBeginTime())), spaces, self.foregroundColor, event.getEventName())
+					if not self.hideLabel:
+						self.text = self.text and "%s%s\n%s" % (self.labelcolor, pgettext("now/next: 'next' event label", "Next"), self.text) or ""
 
 	def applySkin(self, desktop, parent):
+		attribs = []
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "NumberOfItems":
 				self.numberOfItems = int(value)
-				self.skinAttributes.remove((attrib, value))
+				attribs.append((attrib, value))
+			if attrib == "noLabel":
+				self.hideLabel = int(value)
+				attribs.append((attrib, value))
+			if attrib == "numOfSpaces":
+				self.numOfSpaces = int(value)
+				attribs.append((attrib, value))
+			if attrib == "timeColor":
+				self.timecolor = Hex2strColor(parseColor(value).argb())
+				attribs.append((attrib, value))
+			if attrib == "labelColor":
+				self.labelcolor = Hex2strColor(parseColor(value).argb())
+				attribs.append((attrib, value))
+			if attrib == "foregroundColor":
+				self.foregroundColor = Hex2strColor(parseColor(value).argb())
+				attribs.append((attrib, value))
+		for (attrib, value) in attribs:
+			self.skinAttributes.remove((attrib, value))
+		if self.timecolor == "": # fallback to foregroundColor
+			self.timecolor = self.foregroundColor
+		if self.labelcolor == "": # fallback to foregroundColor
+			self.labelcolor = self.foregroundColor
 		return Renderer.applySkin(self, desktop, parent)
