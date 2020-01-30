5b495fdeef7945b18636dd05e70409147e180616
Tony Whalley <awhalley@waitrose.com>
2020-01-30 16:04:01 +0100

    Harddisk - move SystemInfo(Udev) resolution to SystemInfo

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -49,10 +49,6 @@ class Harddisk:
 	def __init__(self, device, removable = False):
 		self.device = device
 
-		if os.access("/dev/.devfsd", os.F_OK):
-			SystemInfo["Udev"] = False
-		else:
-			SystemInfo["Udev"] = True
 		self.sdmmc = False
 		self.max_idle_time = 0
 		self.idle_running = False
