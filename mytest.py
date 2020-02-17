75e9f670a5f89ffad6bba42dba56e156434c717c
IanSav <IanSav@users.noreply.github.com>
2020-02-18 00:29:48 +1100

    [mytest.py] Activate geolocation initialisation

--- mytest.py
+++ mytest.py
@@ -32,6 +32,10 @@ if getImageArch() in ("aarch64"):
 
 from traceback import print_exc
 
+profile("Geolocation")
+import Tools.Geolocation
+Tools.Geolocation.InitGeolocation()
+
 profile("ClientMode")
 import Components.ClientMode
 Components.ClientMode.InitClientMode()
