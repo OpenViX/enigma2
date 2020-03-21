e8d05f822b1ca79997df235fc7aa88140b55391a
IanSav <IanSav@users.noreply.github.com>
2020-03-22 02:56:58 +1100

    [Timezones.py] Make settings time zone dominant
    This change allows the time zone in the "settings" file to override the "/etc/localtime" time zone setting.  This will allow users to restore their "settings" file from a backup and have the Linux, glib, TZ and Enigma2 time zones all be matched.

--- lib/python/Components/Timezones.py
+++ lib/python/Components/Timezones.py
@@ -79,8 +79,8 @@ def InitTimeZones():
 		msgs = []
 		if config.timezone.area.value == "Classic":
 			if config.timezone.val.value != tzLink:
-				msgs.append("time zone '%s' != '%s'" % (tzLink, config.timezone.val.value))
-				config.timezone.val.value = tzLink
+				msgs.append("time zone '%s' != '%s'" % (config.timezone.val.value, tzLink))
+				# config.timezone.val.value = tzLink
 		else:
 			tzSplit = tzLink.find("/")
 			if tzSplit == -1:
@@ -90,13 +90,14 @@ def InitTimeZones():
 				tzArea = tzLink[:tzSplit]
 				tzVal = tzLink[tzSplit + 1:]
 			if config.timezone.area.value != tzArea:
-				msgs.append("area '%s' != '%s'" % (tzArea, config.timezone.area.value))
-				config.timezone.area.value = tzArea
+				msgs.append("area '%s' != '%s'" % (config.timezone.area.value, tzArea))
+				# config.timezone.area.value = tzArea
 			if config.timezone.val.value != tzVal:
-				msgs.append("zone '%s' != '%s'" % (tzVal, config.timezone.val.value))
-				config.timezone.val.value = tzVal
+				msgs.append("zone '%s' != '%s'" % (config.timezone.val.value, tzVal))
+				# config.timezone.val.value = tzVal
 		if len(msgs):
-			print "[Timezones] Warning: System timezone does not match Enigma2 timezone (%s), setting Enigma2 to system timezone!" % ",".join(msgs)
+			# print "[Timezones] Warning: Enigma2 time zone does not match system time zone (%s), setting Enigma2 to system time zone!" % ",".join(msgs)
+			print "[Timezones] Warning: Enigma2 time zone does not match system time zone (%s), setting system to Enigma2 time zone!" % ",".join(msgs)
 	except (IOError, OSError):
 		pass
 
