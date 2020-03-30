bfc8633d4ee6e6b74060a641eb3cbcb5b19b7f1b
Athanasios Oikonomou <athoik@gmail.com>
2020-03-30 21:49:32 +0300

    [ChannelSelection] allow to delete an item in move mode
    This commit allows to delete channel item in move mode.
    Previously it was also possible to delete an item by pressing 4.
    Although when item was selected a crash was occuring.
    So we are display the delete and we are making sure crash won't happen.
    
    Note: merged from OpenPLi with conficts (that required changes) so needs testing.

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -284,6 +284,8 @@ class ChannelContextMenu(Screen):
 					_append_when_current_valid(current, menu, actions, (_("Disable move mode"), self.toggleMoveMode), level=0, key="red")
 				else:
 					_append_when_current_valid(current, menu, actions, (_("Enable move mode"), self.toggleMoveMode), level=1, key="red")
+				_append_when_current_valid(current, menu, (_("Remove entry"), self.removeEntry), level=0, key="4")
+				self.removeFunction = self.removeCurrentService
 				if not csel.entry_marked and not inBouquetRootList and current_root and not (current_root.flags & eServiceReference.isGroup):
 					_append_when_current_valid(current, menu, actions, (_("Add marker"), self.showMarkerInputBox), level=0, key="green")
 					if not csel.movemode:
@@ -1248,6 +1250,8 @@ class ChannelSelectionEdit:
 				self.removeCurrentService()
 
 	def removeCurrentService(self, bouquet=False):
+		if self.movemode and self.entry_marked:
+			self.toggleMoveMarked() # unmark current entry
 		self.editMode = True
 		ref = self.servicelist.getCurrent()
 		mutableList = self.getMutableList()
