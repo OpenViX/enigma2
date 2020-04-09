f696db06b58aae4d941859b419a6f7d0e49ef9e5
Vix-Sicilian <stefd@hotmail.co.uk>
2020-04-09 17:53:14 +0100

    Temp change to build series number.

--- ViX/src/ImageManager.py
+++ ViX/src/ImageManager.py
@@ -1404,7 +1404,7 @@ class ImageManagerDownload(Screen):
 		if "atv" in self.urli:
 			imagecat = [6.2,6.3, 6.4]
 		elif "www.openvix" in self.urli:
-			imagecat = [5.3]
+			imagecat = [5]
 
 		if not self.Pli and not self.imagesList:
 			for version in reversed(sorted(imagecat)):
