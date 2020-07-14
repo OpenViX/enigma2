1ef02c37f05debd590a4ea56c07caeeea22a56da
IanSav <IanSav@users.noreply.github.com>
2020-07-15 04:06:47 +1000

    [InstallWizard.py] Correct session open arguments
    The "" placeholders are left overs from the previous menu_path processing.

--- lib/python/Screens/InstallWizard.py
+++ lib/python/Screens/InstallWizard.py
@@ -109,10 +109,10 @@ class InstallWizard(Screen, ConfigListScreen):
 			self.session.open(InstallWizardIpkgUpdater, self.index, _('Please wait (downloading channel list)'), IpkgComponent.CMD_REMOVE, {'package': 'enigma2-plugin-settings-henksat-' + self.channellist_type.value})
 		elif self.index == self.INSTALL_PLUGINS and self.enabled.value:
 			from PluginBrowser import PluginDownloadBrowser
-			self.session.open(PluginDownloadBrowser, 0, True, "", "PluginDownloadBrowserWizard")
+			self.session.open(PluginDownloadBrowser, 0, True, "PluginDownloadBrowserWizard")
 		elif self.index == self.INSTALL_SKINS and self.enabled.value:
 			from SkinSelector import SkinSelector
-			self.session.open(SkinSelector, "", "SkinSelectorWizard")
+			self.session.open(SkinSelector, "SkinSelectorWizard")
 		return
 
 class InstallWizardIpkgUpdater(Screen):
