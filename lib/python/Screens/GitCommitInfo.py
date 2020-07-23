41bef2edddc35b96cf925ed6b0f81148c1a74d24
Vix-Sicilian <stefd@hotmail.co.uk>
2020-07-23 15:54:16 +0100

    Prepare to switch to OE-A 4.4.

--- lib/python/Screens/GitCommitInfo.py
+++ lib/python/Screens/GitCommitInfo.py
@@ -27,7 +27,7 @@ E2Branches = {
 
 project = 0
 projects = [
-	("https://api.github.com/repos/oe-alliance/oe-alliance-core/commits?sha=4.3", "OE-A Core"),
+	("https://api.github.com/repos/oe-alliance/oe-alliance-core/commits?sha=4.4", "OE-A Core"),
 	("https://api.github.com/repos/OpenViX/enigma2/commits?sha=%s" % getattr(E2Branches, getImageType(), "Dev"), "Enigma2"),
 	("https://api.github.com/repos/OpenViX/vix-core/commits", "ViX Core"),
 	("https://api.github.com/repos/OpenViX/skins/commits", "ViX Skins"),
