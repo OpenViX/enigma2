f70b24d5efc8a3ce0e55653a742d42a18c14292d
IanSav <IanSav@users.noreply.github.com>
2020-09-15 03:14:20 +1000

    [ConfigList.py] Updates to match config.py
    - Update to the newer action names.
    - Remove the keySelect Boolean helper as this is now directly handled by config.py itself.
    - Adjust keyToggleOW to keyToggle and match to the KEYA_TOGGLE action.
    - Be more consistent with the keyText() / keyTextCallback and keyMenu and keyMenuCallback method names.
    - Improve some HELP text.

--- lib/python/Components/ConfigList.py
+++ lib/python/Components/ConfigList.py
@@ -2,7 +2,7 @@ from enigma import eListbox, eListboxPythonConfigContent, ePoint, eRCInput, eTim
 from skin import parameters
 
 from Components.ActionMap import HelpableActionMap, HelpableNumberActionMap
-from Components.config import ConfigBoolean, ConfigElement, ConfigInteger, ConfigMacText, ConfigSelection, ConfigSequence, ConfigText, KEYA_0, KEYA_ASCII, KEYA_BACKSPACE, KEYA_DELETE, KEYA_END, KEYA_ERASE, KEYA_HOME, KEYA_LEFT, KEYA_NUMBERS, KEYA_RIGHT, KEYA_SELECT, KEYA_TIMEOUT, KEYA_TOGGLEOW, config, configfile
+from Components.config import ConfigBoolean, ConfigElement, ConfigInteger, ConfigMacText, ConfigSelection, ConfigSequence, ConfigText, KEYA_0, KEYA_ASCII, KEYA_BACKSPACE, KEYA_DELETE, KEYA_ERASE, KEYA_FIRST, KEYA_LAST, KEYA_LEFT, KEYA_NUMBERS, KEYA_RIGHT, KEYA_SELECT, KEYA_TIMEOUT, KEYA_TOGGLE, config, configfile
 from Components.GUIComponent import GUIComponent
 from Components.Pixmap import Pixmap
 from Components.Sources.Boolean import Boolean
@@ -167,7 +167,7 @@ class ConfigListScreen:
 			"select": (self.keySelect, _("Select, toggle, process or edit the current entry"))
 		}, prio=1, description=_("Common Setup Actions"))
 		self["navigationActions"] = HelpableActionMap(self, ["NavigationActions"], {
-			"top": (self.keyTop, _("Move to first line")),
+			"top": (self.keyTop, _("Move to first line / screen")),
 			"pageUp": (self.keyPageUp, _("Move up a screen")),
 			"up": (self.keyUp, _("Move up a line")),
 			"first": (self.keyFirst, _("Jump to first item in list or the start of text")),
@@ -176,7 +176,7 @@ class ConfigListScreen:
 			"last": (self.keyLast, _("Jump to last item in list or the end of text")),
 			"down": (self.keyDown, _("Move down a line")),
 			"pageDown": (self.keyPageDown, _("Move down a screen")),
-			"bottom": (self.keyBottom, _("Move to last line"))
+			"bottom": (self.keyBottom, _("Move to last line / screen"))
 		}, prio=1, description=_("Common Setup Actions"))
 		self["menuConfigActions"] = HelpableActionMap(self, "ConfigListActions", {
 			"menu": (self.keyMenu, _("Display selection list as a selection menu")),
@@ -186,7 +186,7 @@ class ConfigListScreen:
 			"backspace": (self.keyBackspace, _("Delete character to left of cursor or select AM times")),
 			"delete": (self.keyDelete, _("Delete character under cursor or select PM times")),
 			"erase": (self.keyErase, _("Delete all the text")),
-			"toggleOverwrite": (self.keyToggleOW, _("Toggle new text inserts before or overwrites existing text")),
+			"toggleOverwrite": (self.keyToggle, _("Toggle new text inserts before or overwrites existing text")),
 			"1": (self.keyNumberGlobal, _("Number or SMS style data entry")),
 			"2": (self.keyNumberGlobal, _("Number or SMS style data entry")),
 			"3": (self.keyNumberGlobal, _("Number or SMS style data entry")),
@@ -279,21 +279,8 @@ class ConfigListScreen:
 				else:
 					currConf.help_window.hide()
 
-	def keyText(self):
-		self.session.openWithCallback(self.VirtualKeyBoardCallback, VirtualKeyBoard, title=self.getCurrentEntry(), text=str(self.getCurrentValue()))
-
-	def VirtualKeyBoardCallback(self, callback=None):
-		if callback is not None:
-			prev = str(self.getCurrentValue())
-			self["config"].getCurrent()[1].setValue(callback)
-			self["config"].invalidateCurrent()
-			if callback != prev:
-				self.entryChanged()
-
 	def keySelect(self):
-		if isinstance(self.getCurrentItem(), ConfigBoolean):
-			self.keyRight()
-		elif isinstance(self.getCurrentItem(), ConfigSelection):
+		if isinstance(self.getCurrentItem(), ConfigSelection):
 			self.keyMenu()
 		elif isinstance(self.getCurrentItem(), ConfigText):
 			self.keyText()
@@ -303,17 +290,28 @@ class ConfigListScreen:
 	def keyOK(self):  # This is the deprecated version of keySelect!
 		self.keySelect()
 
+	def keyText(self):
+		self.session.openWithCallback(self.keyTextCallback, VirtualKeyBoard, title=self.getCurrentEntry(), text=str(self.getCurrentValue()))
+
+	def keyTextCallback(self, callback=None):
+		if callback is not None:
+			prev = str(self.getCurrentValue())
+			self["config"].getCurrent()[1].setValue(callback)
+			self["config"].invalidateCurrent()
+			if callback != prev:
+				self.entryChanged()
+
 	def keyMenu(self):
 		currConfig = self["config"].getCurrent()
 		if currConfig and currConfig[1].enabled and hasattr(currConfig[1], "description"):
 			self.session.openWithCallback(
-				self.handleKeyMenuCallback, ChoiceBox, title=currConfig[0],
+				self.keyMenuCallback, ChoiceBox, title=currConfig[0],
 				list=zip(currConfig[1].description, currConfig[1].choices),
 				selection=currConfig[1].getIndex(),
 				keys=[]
 			)
 
-	def handleKeyMenuCallback(self, answer):
+	def keyMenuCallback(self, answer):
 		if answer:
 			self["config"].getCurrent()[1].value = answer[1]
 			self["config"].invalidateCurrent()
@@ -365,8 +363,8 @@ class ConfigListScreen:
 		self["config"].handleKey(KEYA_ERASE)
 		self.entryChanged()
 
-	def keyToggleOW(self):
-		self["config"].handleKey(KEYA_TOGGLEOW)
+	def keyToggle(self):
+		self["config"].handleKey(KEYA_TOGGLE)
 		self.entryChanged()
 
 	def keyGotAscii(self):
