ec3d6b11f09c25362b53ec9ed289f87815819b7e
Huevos <rowland@moralet.com>
2020-10-11 18:41:22 +0200

    [InfoBar] add TV/Radio toggle for remotes with button limitations.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -144,5 +144,6 @@ SystemInfo["VideoModes"] = getChipSetString() in (  # 2160p and 1080p capable ha
 SystemInfo["LnbPowerAlwaysOn"] = getBoxType() in ("vusolo4k", "vuduo4k", "vuduo4kse", "vuultimo4k", "vuuno4k", "vuuno4kse", "gbquad4k", "gbue4k")
 
 SystemInfo["rc_model"] = rc_model.getRcFolder()
-SystemInfo["mapKeyInfoToEpgFunctions"] = SystemInfo["rc_model"] in ("vu", "vu2", "vu3", "vu4")
+SystemInfo["mapKeyInfoToEpgFunctions"] = SystemInfo["rc_model"] in ("vu", "vu2", "vu3", "vu4") # due to button limitations of the remote control
+SystemInfo["toggleTvRadioButtonEvents"] = SystemInfo["rc_model"] in ("ax4", "beyonwiz1", "beyonwiz2", "gb1", "gb4", "sf8008", "uniboxhde") # due to button limitations of the remote control
 
