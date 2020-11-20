0710fae8dd05d8a222e8e6850c507f83ca8c2372
Huevos <rowland@moralet.com>
2020-11-20 12:43:22 +0100

    [FallbackTunerSetup] inherit from Setup module

--- lib/python/Screens/FallbackTunerSetup.py
+++ lib/python/Screens/FallbackTunerSetup.py
@@ -1,48 +1,13 @@
-from Screens.Screen import Screen
-from Components.Label import Label
-from Components.ActionMap import ActionMap
-from Components.Pixmap import Pixmap
-from Components.Sources.Boolean import Boolean
-from Components.Sources.StaticText import StaticText
-from Components.config import config, configfile, ConfigText, ConfigSubsection, ConfigSelection, ConfigIP, ConfigYesNo, ConfigInteger, getConfigListEntry
-from Components.ConfigList import ConfigListScreen
-from Screens.HelpMenu import HelpableScreen
-
+from Components.config import config, configfile, ConfigText, ConfigSelection, ConfigIP, ConfigYesNo, ConfigInteger
+from Screens.Setup import Setup
 from enigma import getPeerStreamingBoxes
-
 import re
 
-class FallbackTunerSetup(ConfigListScreen, Screen):
+class FallbackTunerSetup(Setup):
 	def __init__(self, session):
-		Screen.__init__(self, session)
-		self.setTitle(_("Fallback Tuner Setup"))
-		self.skinName = ["FallbackTunerSetup", "Setup"]
-		self.onChangedEntry = []
-		self.session = session
-		ConfigListScreen.__init__(self, [], session = session, on_change = self.changedEntry)
-
-		self["actions2"] = ActionMap(["SetupActions"],
-		{
-			"ok": self.keyGo,
-			"menu": self.keyCancel,
-			"cancel": self.keyCancel,
-			"save": self.keyGo,
-		}, -2)
-
-		self["key_red"] = StaticText(_("Exit"))
-		self["key_green"] = StaticText(_("Save"))
-
-		self["description"] = Label("")
-		self["VKeyIcon"] = Boolean(False)
-		self["HelpWindow"] = Pixmap()
-		self["HelpWindow"].hide()
-
 		self.createConfig()
-		self.createSetup()
-
-		if not self.selectionChanged in self["config"].onSelectionChanged:
-			self["config"].onSelectionChanged.append(self.selectionChanged)
-		self.selectionChanged()
+		Setup.__init__(self, session=session, setup=None)
+		self.title = _("Fallback Tuner Setup")
 
 	def createConfig(self):
 		self.enabled = ConfigYesNo(default = config.usage.remote_fallback_enabled.value)
@@ -79,38 +44,23 @@ class FallbackTunerSetup(ConfigListScreen, Screen):
 			fallbackAddressTypeDefault = "ip"
 		self.fallbackAddressType = ConfigSelection(default = fallbackAddressTypeDefault, choices = fallbackAddressChoices)
 
-		self.enabledEntry = getConfigListEntry(_("Enable fallback remote receiver"), self.enabled,_('Enable usage of tuners from another Enigma2 receiver on the local network. Remote tuners will be used when tuners are not available on this receiver. (No free tuner or service type is not available.)'))
-		self.addressTypeEntry = getConfigListEntry(_("Fallback address type"), self.fallbackAddressType,_("'Network peer' automatically detects other Enigma2 receivers on the local network. You can also manually enter the URL or IP address."))
-		self.peerEntry = self.peer and getConfigListEntry(_("Network peers"), self.peer,_("Select a receiver to use for fallback tuners. If the host receiver is not listed, manually enter the URL or IP address"))
-		self.ipEntry = getConfigListEntry(_("Fallback receiver IP address"), self.ip,_("Enter the IP address of the receiver to use for fallback tuners."))
-		self.domainEntry = getConfigListEntry(_("Fallback remote receiver URL"), self.domain,_("Enter the URL/IP of the fallback remote receiver, e.g. '192.168.0.1'. The other details such as 'http://' and port number will be filled in automatically when you select save."))
-		self.portEntry = getConfigListEntry(_("Fallback receiver streaming port"), self.port,_("Default port is '%d'. Change if required.") % self.portDefault)
-
 	def createSetup(self):
-		self.list = [self.enabledEntry]
+		self.list = [(_("Enable fallback remote receiver"), self.enabled, _('Enable usage of tuners from another Enigma2 receiver on the local network. Remote tuners will be used when tuners are not available on this receiver. (No free tuner or service type is not available.)'))]
 		if self.enabled.value:
-			self.list.append(self.addressTypeEntry)
+			self.list.append((_("Fallback address type"), self.fallbackAddressType, _("'Network peer' automatically detects other Enigma2 receivers on the local network. You can also manually enter the URL or IP address.")))
 			if self.fallbackAddressType.value == "peer":
-				self.list.append(self.peerEntry)
+				self.list.append((_("Network peers"), self.peer, _("Select a receiver to use for fallback tuners. If the host receiver is not listed, manually enter the URL or IP address")))
 			elif self.fallbackAddressType.value == "ip":
-				self.list.append(self.ipEntry)
-				self.list.append(self.portEntry)
+				self.list.append((_("Fallback receiver IP address"), self.ip, _("Enter the IP address of the receiver to use for fallback tuners.")))
+				self.list.append((_("Fallback receiver streaming port"), self.port, _("Default port is '%d'. Change if required.") % self.portDefault))
 			else:
-				self.list.append(self.domainEntry)
+				self.list.append((_("Fallback remote receiver URL"), self.domain, _("Enter the URL/IP of the fallback remote receiver, e.g. '192.168.0.1'. The other details such as 'http://' and port number will be filled in automatically when you select save.")))
 
-		self["config"].list = self.list
-		self["config"].l.setList(self.list)
+		currentItem = self["config"].getCurrent()
+		self["config"].setList(self.list)
+		self.moveToItem(currentItem)
 
-	def selectionChanged(self):
-		self["description"].setText(self.getCurrentDescription())
-
-	def changedEntry(self):
-		if self["config"].getCurrent() in (self.enabledEntry, self.addressTypeEntry): # only do screen refresh if current entry requires this
-			self.createSetup()
-		for x in self.onChangedEntry:
-			x()
-
-	def keyGo(self):
+	def keySave(self):
 		config.usage.remote_fallback_enabled.value = self.enabled.value
 		if self.fallbackAddressType.value == "domain":
 			config.usage.remote_fallback.value = self.check_URL_format(self.domain.value)
