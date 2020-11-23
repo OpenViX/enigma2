f544349dee3ff7cfb3c71b7746a027467ec58466
Huevos <rowland@moralet.com>
2020-11-23 14:31:27 +0100

    [ClientModeScreen] inherit from Setup module

--- lib/python/Screens/ClientMode.py
+++ lib/python/Screens/ClientMode.py
@@ -1,88 +1,47 @@
-from Components.ActionMap import ActionMap
-from Components.config import config, configfile, getConfigListEntry, ConfigSubList, ConfigSubsection
-from Components.ConfigList import ConfigListScreen
-from Components.Label import Label
-from Components.Sources.StaticText import StaticText
+from Components.config import config, configfile, ConfigSubList, ConfigSubsection
 from Screens.MessageBox import MessageBox
-from Screens.Screen import Screen
-from Screens.Standby import TryQuitMainloop
+from Screens.Setup import Setup
+from Screens.Standby import TryQuitMainloop, QUIT_RESTART
 
-# for VKeyIcon
-from Components.Sources.Boolean import Boolean
 
-# for HelpWindow
-from Components.Pixmap import Pixmap
-from enigma import ePoint
-
-class ClientModeScreen(ConfigListScreen, Screen):
+class ClientModeScreen(Setup):
 	def __init__(self, session):
-		Screen.__init__(self, session)
-		self.setTitle(_("Client Mode"))
-		self.skinName = "Setup"
-		self.initial_state = config.clientmode.enabled.value
-		self.onChangedEntry = []
-		self.session = session
-		ConfigListScreen.__init__(self, [], session = session, on_change = self.changedEntry)
-
-		self["actions"] = ActionMap(["SetupActions", "MenuActions", "ColorActions"],
-		{
-			"ok": self.keyGo,
-			"menu": self.keyCancel,
-			"cancel": self.keyCancel,
-			"save": self.keyGo,
-		}, -2)
-
-		self["key_red"] = StaticText(_("Exit"))
-		self["key_green"] = StaticText(_("Save"))
-
-		self["description"] = Label("")
-
-		# VKeyIcon is the automatic "text" button on buttonbar. HelpWindow is remote control helper image.
-		self["HelpWindow"] = Pixmap()
-		self["HelpWindow"].hide()
-		self["VKeyIcon"] = Boolean(False)
-
-		self.createSetup()
-
-		if not self.selectionChanged in self["config"].onSelectionChanged:
-			self["config"].onSelectionChanged.append(self.selectionChanged)
-		self.selectionChanged()
+		Setup.__init__(self, session=session, setup=None)
+		self.title = _("Client Mode")
 
 	def createSetup(self):
-		setup_list = []
-		setup_list.append(getConfigListEntry(_("Enable client mode"), config.clientmode.enabled,_('Client mode sets up this receiver to stream from another receiver. In this mode no local tuners will be available and channel lists, EPG, etc, will come from the remote receiver. All tuner settings will be cleared.')))
+		self.list = []
+		self.list.append((_("Enable client mode"), config.clientmode.enabled,_('Client mode sets up this receiver to stream from another receiver. In this mode no local tuners will be available and channel lists, EPG, etc, will come from the remote receiver. All tuner settings will be cleared.')))
 		if config.clientmode.enabled.value:
-			setup_list.append(getConfigListEntry(_("Host receiver address type"), config.clientmode.serverAddressType,_('Select between entering an IP address or a domain.')))
+			self.list.append((_("Host receiver address type"), config.clientmode.serverAddressType,_('Select between entering an IP address or a domain.')))
 			if config.clientmode.serverAddressType.value == "ip":
-				setup_list.append(getConfigListEntry(_("Host receiver IP address"), config.clientmode.serverIP,_('Enter the IP address of the host receiver.')))
+				self.list.append((_("Host receiver IP address"), config.clientmode.serverIP,_('Enter the IP address of the host receiver.')))
 			else:
-				setup_list.append(getConfigListEntry(_("Host domain"), config.clientmode.serverDomain,_("Enter the domain of the host receiver. Do not include 'http://' or port number.")))
-			setup_list.append(getConfigListEntry(_("Host receiver streaming port"), config.clientmode.serverStreamingPort,_("Enter the streaming port of the host receiver (normally '8001').")))
-			setup_list.append(getConfigListEntry(_("Host receiver FTP username"), config.clientmode.serverFTPusername,_("Enter the FTP username of the host receiver (normally 'root').")))
-			setup_list.append(getConfigListEntry(_("Host receiver FTP password"), config.clientmode.serverFTPpassword,_("Enter the FTP password of the host receiver (normally just leave empty).")))
-			setup_list.append(getConfigListEntry(_("Host receiver FTP port"), config.clientmode.serverFTPPort,_("Enter the FTP port of the host receiver (normally '21').")))
-			setup_list.append(getConfigListEntry(_("FTP passive mode"), config.clientmode.passive,_("Should the FTP connection to the remote receiver be established in passive mode (normally 'no')?")))
-			setup_list.append(getConfigListEntry(_("Schedule EPG and channel list import"), config.clientmode.enableSchedule,_("Allows you to set a schedule to import the EPG and channels list. The EPG and channels list will always be imported on reboot or GUI restart.")))
+				self.list.append((_("Host domain"), config.clientmode.serverDomain,_("Enter the domain of the host receiver. Do not include 'http://' or port number.")))
+			self.list.append((_("Host receiver streaming port"), config.clientmode.serverStreamingPort,_("Enter the streaming port of the host receiver (normally '8001').")))
+			self.list.append((_("Host receiver FTP username"), config.clientmode.serverFTPusername,_("Enter the FTP username of the host receiver (normally 'root').")))
+			self.list.append((_("Host receiver FTP password"), config.clientmode.serverFTPpassword,_("Enter the FTP password of the host receiver (normally just leave empty).")))
+			self.list.append((_("Host receiver FTP port"), config.clientmode.serverFTPPort,_("Enter the FTP port of the host receiver (normally '21').")))
+			self.list.append((_("FTP passive mode"), config.clientmode.passive,_("Should the FTP connection to the remote receiver be established in passive mode (normally 'no')?")))
+			self.list.append((_("Schedule EPG and channel list import"), config.clientmode.enableSchedule,_("Allows you to set a schedule to import the EPG and channels list. The EPG and channels list will always be imported on reboot or GUI restart.")))
 			if config.clientmode.enableSchedule.value:
-				setup_list.append(getConfigListEntry(_("Repeat how often"), config.clientmode.scheduleRepeatInterval,_("Set the repeat interval of the schedule.")))
+				self.list.append((_("Repeat how often"), config.clientmode.scheduleRepeatInterval,_("Set the repeat interval of the schedule.")))
 				if config.clientmode.scheduleRepeatInterval.value in ("daily",):
-					setup_list.append(getConfigListEntry(_("Time import should start"), config.clientmode.scheduletime,_("Set the time of day to perform the import.")))
+					self.list.append((_("Time import should start"), config.clientmode.scheduletime,_("Set the time of day to perform the import.")))
 
-		self["config"].list = setup_list
-		self["config"].l.setList(setup_list)
-
-	def selectionChanged(self):
-		self["description"].setText(self.getCurrentDescription())
+		currentItem = self["config"].getCurrent()
+		self["config"].setList(self.list)
+		self.moveToItem(currentItem)
 
 	def run(self): # for start wizard
 		self.saveconfig()
 
-	def keyGo(self):
+	def keySave(self):
 		if config.clientmode.enabled.value and not self.checkFTPconnection():
 			mbox = self.session.open(MessageBox, _("Connection using the supplied FTP parameters failed. Please recheck the details and try again."), MessageBox.TYPE_ERROR)
 			mbox.setTitle(_("FTP connection failure"))
 			return
-		if self.initial_state != config.clientmode.enabled.value:
+		if config.clientmode.enabled.isChanged():
 			restartbox = self.session.openWithCallback(self.restartGUI, MessageBox,_("GUI needs a restart to switch modes\nDo you want to restart the GUI now?"), MessageBox.TYPE_YESNO)
 			restartbox.setTitle(_("Restart GUI now?"))
 		else:
@@ -91,7 +50,7 @@ class ClientModeScreen(ConfigListScreen, Screen):
 
 	def saveconfig(self):
 		nim_config_list = []
-		if self.initial_state != config.clientmode.enabled.value and self.initial_state == False:  # switching to client mode
+		if config.clientmode.enabled.isChanged() and config.clientmode.enabled.value:  # switching to client mode
 			# save normal mode config so it can be reinsated when returning to normal mode
 			nim_config_list = []
 			for x in config.Nims:
@@ -104,7 +63,7 @@ class ClientModeScreen(ConfigListScreen, Screen):
 		if config.clientmode.enabled.value:
 			config.usage.remote_fallback_enabled.value = True
 			config.usage.remote_fallback.value = "http://%s:%d" % (self.getRemoteAddress(), config.clientmode.serverStreamingPort.value)
-		elif self.initial_state != config.clientmode.enabled.value: # switching back to normal mode
+		elif config.clientmode.enabled.isChanged(): # switching back to normal mode
 			# load nim config from config.clientmode.nimcache
 			import json
 			nim_config_list = json.loads(config.clientmode.nim_cache.value)
@@ -153,12 +112,4 @@ class ClientModeScreen(ConfigListScreen, Screen):
 	def restartGUI(self, answer):
 		if answer is True:
 			self.saveconfig()
-			self.session.open(TryQuitMainloop, 3)
-
-	def keyLeft(self):
-		ConfigListScreen.keyLeft(self)
-		self.createSetup()
-
-	def keyRight(self):
-		ConfigListScreen.keyRight(self)
-		self.createSetup()
+			self.session.open(TryQuitMainloop, QUIT_RESTART)
