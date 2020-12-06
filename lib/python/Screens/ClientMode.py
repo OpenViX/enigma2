7b1a4a9bfd64fe95bfa67fc17066b4059ab2a56d
Huevos <rowland@moralet.com>
2020-12-06 01:14:01 +0100

    [ClientMode] Use setup.xml

--- lib/python/Screens/ClientMode.py
+++ lib/python/Screens/ClientMode.py
@@ -6,32 +6,7 @@ from Screens.Standby import TryQuitMainloop, QUIT_RESTART
 
 class ClientModeScreen(Setup):
 	def __init__(self, session):
-		Setup.__init__(self, session=session, setup=None)
-		self.title = _("Client Mode")
-
-	def createSetup(self):
-		self.list = []
-		self.list.append((_("Enable client mode"), config.clientmode.enabled,_('Client mode sets up this receiver to stream from another receiver. In this mode no local tuners will be available and channel lists, EPG, etc, will come from the remote receiver. All tuner settings will be cleared.')))
-		if config.clientmode.enabled.value:
-			self.list.append((_("Host receiver address type"), config.clientmode.serverAddressType,_('Select between entering an IP address or a domain.')))
-			if config.clientmode.serverAddressType.value == "ip":
-				self.list.append((_("Host receiver IP address"), config.clientmode.serverIP,_('Enter the IP address of the host receiver.')))
-			else:
-				self.list.append((_("Host domain"), config.clientmode.serverDomain,_("Enter the domain of the host receiver. Do not include 'http://' or port number.")))
-			self.list.append((_("Host receiver streaming port"), config.clientmode.serverStreamingPort,_("Enter the streaming port of the host receiver (normally '8001').")))
-			self.list.append((_("Host receiver FTP username"), config.clientmode.serverFTPusername,_("Enter the FTP username of the host receiver (normally 'root').")))
-			self.list.append((_("Host receiver FTP password"), config.clientmode.serverFTPpassword,_("Enter the FTP password of the host receiver (normally just leave empty).")))
-			self.list.append((_("Host receiver FTP port"), config.clientmode.serverFTPPort,_("Enter the FTP port of the host receiver (normally '21').")))
-			self.list.append((_("FTP passive mode"), config.clientmode.passive,_("Should the FTP connection to the remote receiver be established in passive mode (normally 'no')?")))
-			self.list.append((_("Schedule EPG and channel list import"), config.clientmode.enableSchedule,_("Allows you to set a schedule to import the EPG and channels list. The EPG and channels list will always be imported on reboot or GUI restart.")))
-			if config.clientmode.enableSchedule.value:
-				self.list.append((_("Repeat how often"), config.clientmode.scheduleRepeatInterval,_("Set the repeat interval of the schedule.")))
-				if config.clientmode.scheduleRepeatInterval.value in ("daily",):
-					self.list.append((_("Time import should start"), config.clientmode.scheduletime,_("Set the time of day to perform the import.")))
-
-		currentItem = self["config"].getCurrent()
-		self["config"].setList(self.list)
-		self.moveToItem(currentItem)
+		Setup.__init__(self, session=session, setup="clientmode")
 
 	def run(self): # for start wizard
 		self.saveconfig()
