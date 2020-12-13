7babffac61c6931020489570f837e29ab6413d38
Simon Capewell <SimonCapewell@users.noreply.github.com>
2020-12-13 00:42:04 +0000

    Add eServiceReference __repr__ method This renders the service name and channel number as well as the reference string when inspecting in a debugger

--- ServiceReference.py
+++ ServiceReference.py
@@ -37,6 +37,12 @@ eServiceReference.isPlayback = lambda serviceref: "0:0:0:0:0:0:0:0:0" in service
 # These are required for ServiceReference backwards compatibility
 eServiceReference.isRecordable = lambda serviceref: serviceref.flags & eServiceReference.isGroup or (serviceref.type == eServiceReference.idDVB or serviceref.type == eServiceReference.idDVB + 0x100 or serviceref.type == 0x2000 or serviceref.type == eServiceReference.idServiceMP3)
 
+def __repr(serviceref):
+	chnum = serviceref.getChannelNum()
+	chnum = ", ChannelNum=" + str(chnum) if chnum else ""
+	return "eServiceReference(Name=%s%s, String=%s)" % (serviceref.getServiceName(), chnum, serviceref.toString())
+eServiceReference.__repr__ = __repr
+
 def __toString(serviceref):
 	return serviceref.toString()
 eServiceReference.__str__ = __toString
