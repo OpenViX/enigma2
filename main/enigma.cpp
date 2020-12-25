353488360de2bf3f81d64266b19b3d940c5c8d27
Betacentauri <betacentauri@arcor.de>
2020-12-25 19:00:43 +0100

    Refactor eEPGCache
    Split code into 3 cpp files. One for the real epg cache, one
    for the epg transponder readers and the last for the epg channel
    data(which are used by the readers).

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -102,6 +102,7 @@ void keyEvent(const eRCKey &key)
 #include <lib/dvb/db.h>
 #include <lib/dvb/dvbtime.h>
 #include <lib/dvb/epgcache.h>
+#include <lib/dvb/epgtransponderdatareader.h>
 
 class eMain: public eApplication, public sigc::trackable
 {
@@ -112,6 +113,7 @@ class eMain: public eApplication, public sigc::trackable
 	ePtr<eDVBResourceManager> m_mgr;
 	ePtr<eDVBLocalTimeHandler> m_locale_time_handler;
 	ePtr<eEPGCache> m_epgcache;
+	ePtr<eEPGTransponderDataReader> m_epgtransponderdatareader;
 
 public:
 	eMain()
@@ -124,6 +126,7 @@ public:
 		m_mgr = new eDVBResourceManager();
 		m_locale_time_handler = new eDVBLocalTimeHandler();
 		m_epgcache = new eEPGCache();
+		m_epgtransponderdatareader = new eEPGTransponderDataReader();
 		m_mgr->setChannelList(m_dvbdb);
 	}
 
