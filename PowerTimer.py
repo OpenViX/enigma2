OpenViX/enigma2

commit 69fb13d2820585ca5239df300e736a5ade2f3d85

Option to remove finished timer logs Finished logs can be the largest constituent of a timers.xml when keeping timers for more than a week. This increases the time taken to build and serialise timers.xml. Offer an option to remove these logs after a configurable number of days, leaving the timers themselves intact for use by Autotimer

PowerTimer.py
@@ -71,7 +71,6 @@ class PowerTimerEntry(timer.TimerEntry, object):
 		self.autosleepdelay = 60
 		self.autosleeprepeat = 'once'
 
-		self.log_entries = []
 		self.resetState()
 
 	def __repr__(self):
@@ -458,7 +457,7 @@ class PowerTimer(timer.Timer):
 				self.addTimerEntry(w)
 			else:
 				# Remove old timers as set in config
-				self.cleanupDaily(config.recording.keep_timers.value)
+				self.cleanupDaily(config.recording.keep_timers.value, config.recording.keep_finished_timer_logs.value)
 				insort(self.processed_timers, w)
 		self.stateChanged(w)
 
@@ -516,8 +515,6 @@ class PowerTimer(timer.Timer):
 			AFTEREVENT.STANDBY: "standby",
 			AFTEREVENT.DEEPSTANDBY: "deepstandby"
 		}
-		now = time()
-		keepThreshold = config.recording.keep_timers.value > 0 and now - config.recording.keep_timers.value*86400
 
 		list = ['<?xml version="1.0" ?>\n<timers>\n']
 		for timer in self.timer_list + self.processed_timers:
@@ -546,17 +543,7 @@ class PowerTimer(timer.Timer):
 			if len(timer.log_entries) == 0:
 				list.append('/>\n')
 			else:
-				# Handle repeat entries, which never end and so never get pruned by cleanupDaily
-				# Repeating timers get autosleeprepeat="repeated" or repeated="127" (daily) or
-				# "31" (weekdays) [dow bitmap] etc.
-				ignore_before = 0
-				if config.recording.keep_timers.value > 0:
-					if str(timer.autosleeprepeat) == "repeated" or int(timer.repeated) > 0:
-						ignore_before = now - config.recording.keep_timers.value*86400
-
 				for log_time, code, msg in timer.log_entries:
-					if log_time < ignore_before:
-						continue
 					list.append('>\n<log code="%d" time="%d">%s</log' % (code, log_time, stringToXML(msg)))
 				list.append('>\n</timer>\n')
 
@@ -648,3 +635,14 @@ class PowerTimer(timer.Timer):
 
 	def shutdown(self):
 		self.saveTimer()
+
+	def cleanupDaily(self, days, finishedLogDays=None):
+		timer.TimerEntry.cleanupDaily(self, days, finishedLogDays)
+		if days > 0:
+			now = time()
+			keepThreshold = now - days * 86400
+			for entry in self.processed_timers:
+				if str(timer.autosleeprepeat) == "repeated":
+					# Handle repeat entries, which never end
+					# Repeating timers get autosleeprepeat="repeated" as well as the cases handled by TimerEntry
+					entry.log_entries = [entry for entry in entry.log_entries if entry.log_time > keepThreshold]

RecordTimer.py
@@ -257,7 +257,6 @@ class RecordTimerEntry(timer.TimerEntry, object):
 		self.autoTimerId = autoTimerId
 		self.wasInStandby = False
 
-		self.log_entries = []
 		self.flags = set()
 		self.resetState()
 
@@ -1002,7 +1001,7 @@ class RecordTimer(timer.Timer):
 				# check for disabled timers, if time as passed set to completed
 				self.cleanupDisabled()
 				# Remove old timers as set in config
-				self.cleanupDaily(config.recording.keep_timers.value)
+				self.cleanupDaily(config.recording.keep_timers.value, config.recording.keep_finished_timer_logs.value)
 				# If we want to keep done timers, re-insert in the active list
 				if config.recording.keep_timers.value > 0:
 					insort(self.processed_timers, w)
@@ -1075,8 +1074,6 @@ class RecordTimer(timer.Timer):
 			AFTEREVENT.DEEPSTANDBY: "deepstandby",
 			AFTEREVENT.AUTO: "auto"
 		}
-		now = time()
-		keepThreshold = config.recording.keep_timers.value > 0 and now - config.recording.keep_timers.value*86400
 
 		list = ['<?xml version="1.0" ?>\n<timers>\n']
 		for entry in self.timer_list + self.processed_timers:
@@ -1129,12 +1126,7 @@ class RecordTimer(timer.Timer):
 			if len(entry.log_entries) == 0:
 				list.append('/>\n')
 			else:
-				# Handle repeat entries, which never end and so never get pruned by cleanupDaily
-				# Repeating timers get, e.g., repeated="127" (dow bitmap)
-				ignore_before = entry.repeated > 0 and keepThreshold
 				for log_time, code, msg in entry.log_entries:
-					if log_time < ignore_before:
-						continue
 					list.append('>\n<log code="%d" time="%d">%s</log' % (code, log_time, stringToXML(msg)))
 				list.append('>\n</timer>\n')
 
@@ -1407,8 +1399,4 @@ class RecordTimer(timer.Timer):
 
 	def cleanup(self):
 		timer.Timer.cleanup(self)
-		self.saveTimer()
-
-	def cleanupDaily(self, days):
-		timer.Timer.cleanupDaily(self, days)
 		self.saveTimer()

data/setup.xml
@@ -206,6 +206,7 @@
 		<item level="1" text="Show picons in timer list" description="Configure if picons will be shown in the timer list.">config.usage.timerlist_showpicons</item>
 		<item level="1" text="Position of finished timers in timer list" description="Control how finished timers are shown in the timer list. If set to hide, disabled timers will still be shown">config.usage.timerlist_finished_timer_position</item>
 		<item level="2" text="Remove finished timers after (days)" description="Configure the number of days old timers are kept before they are automatically removed from the timer list.">config.recording.keep_timers</item>
+		<item level="2" text="Remove finished timer logs after (days)" description="Configure the number of days old timers' log details are kept before they're automatically removed.">config.recording.keep_finished_timer_logs</item>
 		<item level="2" text="Offline decode delay (ms)" description="Configure the offline decoding delay (in milliseconds). The configured delay is observed at each control word parity change.">config.recording.offline_decode_delay</item>
 		<item level="2" text="Default recording type" description="Descramble &amp; record ECM' gives the option to descramble afterwards if descrambling on recording failed. 'Don't descramble, record ECM' save a scramble recording that can be descrambled on playback. 'Normal' means descramble the recording and don't record ECM.">config.recording.ecm_data</item>
 	</setup>

lib/python/Components/RecordingConfig.py
@@ -9,6 +9,8 @@ def InitRecordingConfig():
 	config.recording.split_programme_minutes = ConfigSelectionNumber(min = 0, max = 30, stepwidth = 1, default = 15, wraparound = True)
 	config.recording.ascii_filenames = ConfigYesNo(default = False)
 	config.recording.keep_timers = ConfigSelectionNumber(min = 1, max = 120, stepwidth = 1, default = 7, wraparound = True)
+	choicelist = [(0, _("Keep logs"))] + [(i, str(i)) for i in range(1, 14)]
+	config.recording.keep_finished_timer_logs = ConfigSelection(default=0, choices=choicelist)
 	config.recording.filename_composition = ConfigSelection(default = "standard", choices = [
 		("standard", _("Date first")),
 		("event", _("Event name first")),

timer.py
@@ -25,6 +25,7 @@ class TimerEntry:
 
 		self.disabled = False
 		self.failed = False
+		self.log_entries = []
 
 	def resetState(self):
 		self.state = self.StateWaiting
@@ -181,9 +182,19 @@ class Timer:
 		for timer in disabled_timers:
 			timer.shouldSkip()
 
-	def cleanupDaily(self, days):
-		limit = time() - (days * 3600 * 24)
-		self.processed_timers = [entry for entry in self.processed_timers if (entry.disabled and entry.repeated) or (entry.end and (entry.end > limit))]
+	def cleanupDaily(self, days, finishedLogDays=None):
+		now = time()
+		keepThreshold = now - days * 86400 if days else 0
+		keepFinishedLogThreshold = now - finishedLogDays * 86400 if finishedLogDays else 0
+		self.processed_timers = [entry for entry in self.processed_timers if (entry.disabled and entry.repeated) or (entry.end and (entry.end > keepThreshold))]
+		for entry in self.processed_timers:
+			if entry.repeated:
+				# Handle repeat entries, which never end
+				# Repeating timers get, e.g., repeated="127" (day of week bitmap)
+				entry.log_entries = [entry for entry in entry.log_entries if entry.log_time > keepThreshold]
+			elif entry.end < keepFinishedLogThreshold and len(entry.log_entries) > 0:
+				# Clear logs on finished timers
+				entry.log_entries = []
 
 	def addTimerEntry(self, entry, noRecalc=0):
 		entry.processRepeated()

commit de6cdf38c4e3df52f2d6fe5deb39699ecad8b49e

Fix power timer crash introduced by commit 69fb13d2

PowerTimer.py
@@ -13,7 +13,7 @@ from Screens.MessageBox import MessageBox
 import Screens.Standby
 from Tools import Directories, Notifications
 from Tools.XMLTools import stringToXML
-import timer
+from timer import Timer, TimerEntry
 import NavigationInstance
 
 
@@ -48,9 +48,9 @@ class TIMERTYPE:
 	RESTART = 8
 
 # please do not translate log messages
-class PowerTimerEntry(timer.TimerEntry, object):
+class PowerTimerEntry(TimerEntry, object):
 	def __init__(self, begin, end, disabled = False, afterEvent = AFTEREVENT.NONE, timerType = TIMERTYPE.WAKEUP, checkOldTimers = False):
-		timer.TimerEntry.__init__(self, int(begin), int(end))
+		TimerEntry.__init__(self, int(begin), int(end))
 		if checkOldTimers:
 			if self.begin < time() - 1209600:
 				self.begin = int(time())
@@ -409,9 +409,9 @@ def createTimer(xml):
 
 	return entry
 
-class PowerTimer(timer.Timer):
+class PowerTimer(Timer):
 	def __init__(self):
-		timer.Timer.__init__(self)
+		Timer.__init__(self)
 
 		self.Filename = Directories.resolveFilename(Directories.SCOPE_CONFIG, "pm_timers.xml")
 
@@ -639,7 +639,7 @@ class PowerTimer(timer.Timer):
 		self.saveTimer()
 
 	def cleanupDaily(self, days, finishedLogDays=None):
-		timer.TimerEntry.cleanupDaily(self, days, finishedLogDays)
+		Timer.cleanupDaily(self, days, finishedLogDays)
 		if days > 0:
 			now = time()
 			keepThreshold = now - days * 86400

RecordTimer.py
@@ -15,7 +15,7 @@ import Components.ParentalControl
 from Tools import Directories, Notifications, ASCIItranslit, Trashcan
 from Tools.XMLTools import stringToXML
 
-import timer
+from timer import Timer, TimerEntry
 import xml.etree.cElementTree
 import NavigationInstance
 
@@ -174,9 +174,9 @@ service_types_tv = '1:7:1:0:0:0:0:0:0:0:(type == 1) || (type == 17) || (type == 
 wasRecTimerWakeup = False
 
 # please do not translate log messages
-class RecordTimerEntry(timer.TimerEntry, object):
+class RecordTimerEntry(TimerEntry, object):
 	def __init__(self, serviceref, begin, end, name, description, eit, disabled=False, justplay=False, afterEvent=AFTEREVENT.AUTO, checkOldTimers=False, dirname=None, tags=None, descramble='notset', record_ecm='notset', isAutoTimer=False, always_zap=False, rename_repeat=True, conflict_detection=True, pipzap=False, autoTimerId=None):
-		timer.TimerEntry.__init__(self, int(begin), int(end))
+		TimerEntry.__init__(self, int(begin), int(end))
 		if checkOldTimers:
 			if self.begin < time() - 1209600:
 				self.begin = int(time())
@@ -954,9 +954,9 @@ def createTimer(xml):
 
 	return entry
 
-class RecordTimer(timer.Timer):
+class RecordTimer(Timer):
 	def __init__(self):
-		timer.Timer.__init__(self)
+		Timer.__init__(self)
 
 		self.Filename = Directories.resolveFilename(Directories.SCOPE_CONFIG, "timers.xml")
 
@@ -1397,5 +1397,5 @@ class RecordTimer(timer.Timer):
 		self.saveTimer()
 
 	def cleanup(self):
-		timer.Timer.cleanup(self)
+		Timer.cleanup(self)
 		self.saveTimer()