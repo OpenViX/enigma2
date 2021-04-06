47e3369ac2cbc457f988d6675904fd4d6eaab060
tony whalley <awhalley@waitrose.com>
2021-04-06 13:33:11 +0200

    Language.py - changes for python3

--- lib/python/Components/Language.py
+++ lib/python/Components/Language.py
@@ -1,4 +1,7 @@
 # -*- coding: UTF-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+
 import gettext
 import locale
 import os
@@ -10,7 +13,7 @@ Lpackagename = "enigma2-locale-"
 
 class Language:
 	def __init__(self):
-		gettext.install('enigma2', resolveFilename(SCOPE_LANGUAGE, ""), unicode=0, codeset="utf-8")
+		gettext.install('enigma2', resolveFilename(SCOPE_LANGUAGE, ""), codeset="utf-8")
 		gettext.bindtextdomain("enigma2", resolveFilename(SCOPE_LANGUAGE))
 		gettext.textdomain("enigma2")
 		self.activeLanguage = 0
@@ -77,17 +80,17 @@ class Language:
 				self.langlist.append(str(lang + "_" + country))
 
 		except:
-			print "[Language] Language " + str(name) + " not found"
+			print("[Language] Language " + str(name) + " not found")
 		self.langlistselection.append((str(lang + "_" + country), name))
 
 	def activateLanguage_TRY(self, index):
 		try:
 			if index not in self.lang:
-				print "[Language] Selected language %s is not installed, fallback to en_US!" % index
+				print("[Language] Selected language %s is not installed, fallback to en_US!" % index)
 				index = "en_US"
 				Notifications.AddNotification(MessageBox, _("The selected langugage is unavailable - using en_US"), MessageBox.TYPE_INFO, timeout=3)
 			lang = self.lang[index]
-			print "[Language] Activating language " + lang[0]
+			print("[Language] Activating language " + lang[0])
 			self.catalog = gettext.translation('enigma2', resolveFilename(SCOPE_LANGUAGE, ""), languages=[index], fallback=True)
 			self.catalog.install(names=("ngettext", "pgettext"))
 			self.activeLanguage = index
@@ -95,7 +98,7 @@ class Language:
 				if x:
 					x()
 		except:
-			print "[Language] Selected language does not exist!"
+			print("[Language] Selected language does not exist!")
 			return False
 
 		# NOTE: we do not use LC_ALL, because LC_ALL will not set any of the categories, when one of the categories fails.
@@ -132,7 +135,7 @@ class Language:
 		from Tools import Notifications
 		from Screens.MessageBox import MessageBox
 		if not self.activateLanguage_TRY(index):
-			print "[Language] - retry with ", "en_US"
+			print("[Language] - retry with ", "en_US")
 			Notifications.AddNotification(MessageBox, _("The selected langugage is unavailable - using en_US"), MessageBox.TYPE_INFO, timeout=3)
 			self.activateLanguage_TRY("en_US")
 
@@ -181,25 +184,25 @@ class Language:
 
 		if delLang:
 			lang = config.osd.language.value
-			print "[Language] DELETE LANG", delLang
+			print("[Language] DELETE LANG", delLang)
 			if delLang[:2] == "en":
-				print "[Language] Default Language can not be deleted !!"
+				print("[Language] Default Language can not be deleted !!")
 				return
 			elif delLang == "pt_BR":
 				delLang = delLang.lower()
-				delLang = delLang.replace('_','-')
+				delLang = delLang.replace('_', '-')
 				os.system("opkg remove --autoremove --force-depends " + Lpackagename + delLang)
 			else:
 				os.system("opkg remove --autoremove --force-depends " + Lpackagename + delLang[:2])
 		else:
 			lang = self.activeLanguage
-			print "[Language] Delete all lang except ", lang
+			print("[Language] Delete all lang except ", lang)
 			ll = os.listdir(LPATH)
 			for x in ll:
 				if len(x) > 2:
 					if x != lang and x[:2] != "en":
 						x = x.lower()
-						x = x.replace('_','-')
+						x = x.replace('_', '-')
 						os.system("opkg remove --autoremove --force-depends " + Lpackagename + x)
 				else:
 					if x != lang[:2] and x != "en":
@@ -215,7 +218,7 @@ class Language:
 	def updateLanguageCache(self):
 		t = localtime(time())
 		createdate = strftime("%d.%m.%Y  %H:%M:%S", t)
-		f = open('/usr/lib/enigma2/python/Components/Language_cache.py','w')
+		f = open('/usr/lib/enigma2/python/Components/Language_cache.py', 'w')
 		f.write('# -*- coding: UTF-8 -*-\n')
 		f.write('# date: ' + createdate + '\n#\n\n')
 		f.write('LANG_TEXT = {\n')
