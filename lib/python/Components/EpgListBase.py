780591b330b65e4e0c5ec169064d82f6835c86e1
Huevos <rowland@moralet.com>
2021-04-21 01:14:47 +0200

    [EpgLists] add formula processing in applySkin

--- lib/python/Components/EpgListBase.py
+++ lib/python/Components/EpgListBase.py
@@ -3,6 +3,7 @@ from enigma import eEPGCache, eListbox, eListboxPythonMultiContent, eServiceRefe
 from Components.GUIComponent import GUIComponent
 from Tools.Alternatives import CompareWithAlternatives
 from Tools.Directories import SCOPE_CURRENT_SKIN, resolveFilename
+from skin import parseScale
 
 
 class EPGListBase(GUIComponent):
@@ -47,7 +48,7 @@ class EPGListBase(GUIComponent):
 			attribs = []
 			for (attrib, value) in self.skinAttributes:
 				if attrib == "itemHeight":
-					self.skinItemHeight = int(value)
+					self.skinItemHeight = parseScale(value)
 				elif attrib == "NumberOfRows": # for compatibility with ATV skins
 					self.numberOfRows = int(value)
 				else:
