a16aaa886fd81ffceb02cd28c500094e2a1e0009
Dimitrij <Dima-73@inbox.lv>
2021-04-27 07:51:32 +0300

    Add SystemInfo["HasPhysicalLoopthrough"]
    -this is name slot "Vuplus DVB-S NIM(AVL2108)", "GIGA DVB-S2 NIM
    (Internal)"
    -"Has_Outputs: yes" not in /proc/bus/nim_sockets NIM, but the physical
    loopthrough exist

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -926,8 +926,8 @@ class NimManager:
 				entry["type"] = None
 			if not ("i2c" in entry):
 				entry["i2c"] = None
-			if not ("has_outputs" in entry):
-				entry["has_outputs"] = True
+			if "has_outputs" not in entry:
+				entry["has_outputs"] = entry["name"] in SystemInfo["HasPhysicalLoopthrough"] # "Has_Outputs: yes" not in /proc/bus/nim_sockets NIM, but the physical loopthrough exist
 			if "frontend_device" in entry: # check if internally connectable
 				if os.path.exists("/proc/stb/frontend/%d/rf_switch" % entry["frontend_device"]) and (not id or entries[id]["name"] == entries[id - 1]["name"]):
 					entry["internally_connectable"] = entry["frontend_device"] - 1
