ad5d000ebac8ad71a70e6b1d39da0ecdcd0607a8
Huevos <rowland@moralet.com>
2021-04-29 00:08:16 +0200

    [Renderer.Progress] Remove junk

--- lib/python/Components/Renderer/Progress.py
+++ lib/python/Components/Renderer/Progress.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 
 from Components.Renderer.Renderer import Renderer
 from Components.VariableValue import VariableValue
-from Renderer import Renderer
 
 from enigma import eSlider
 
