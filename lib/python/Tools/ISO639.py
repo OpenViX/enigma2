88cc37297697a7dec70607a32a098e6b069e2fe3
Huevos <rowland@moralet.com>
2021-04-28 16:06:11 +0200

    [Tools.ISO639] add encoding to pickle.load()
    <   562.1125> 13:13:25.4677 [PluginComponent] Plugin  Extensions/OpenWebif failed to load: 'ascii' codec can't decode byte 0xc3 in position 6: ordinal not in range(128)
    <   562.1177> 13:13:25.4729 Traceback (most recent call last):
    <   562.1180> 13:13:25.4732   File "/usr/lib/enigma2/python/Components/PluginComponent.py", line 59, in readPluginList
        plugin = my_import('.'.join(["Plugins", c, pluginname, "plugin"]))
    <   562.1180> 13:13:25.4733   File "/usr/lib/enigma2/python/Tools/Import.py", line 2, in my_import
        mod = __import__(name)
    <   562.1181> 13:13:25.4734   File "/usr/lib/enigma2/python/Plugins/Extensions/OpenWebif/plugin.py", line 36, in <module>
        from Plugins.Extensions.OpenWebif.httpserver import HttpdStart, HttpdStop, HttpdRestart
    <   562.1182> 13:13:25.4734   File "/usr/lib/enigma2/python/Plugins/Extensions/OpenWebif/httpserver.py", line 34, in <module>
        from Plugins.Extensions.OpenWebif.controllers.root import RootController
    <   562.1182> 13:13:25.4735   File "/usr/lib/enigma2/python/Plugins/Extensions/OpenWebif/controllers/root.py", line 32, in <module>
        from Plugins.Extensions.OpenWebif.controllers.web import WebController, ApiController
    <   562.1183> 13:13:25.4735   File "/usr/lib/enigma2/python/Plugins/Extensions/OpenWebif/controllers/web.py", line 29, in <module>
        from .models.audiotrack import getAudioTracks, setAudioTrack
    <   562.1183> 13:13:25.4736   File "/usr/lib/enigma2/python/Plugins/Extensions/OpenWebif/controllers/models/audiotrack.py", line 3, in <module>
        from Tools.ISO639 import LanguageCodes
    <   562.1184> 13:13:25.4736   File "/usr/lib/enigma2/python/Tools/ISO639.py", line 9, in <module>
        LanguageCodes = pickle.load(f)
    <   562.1185> 13:13:25.4737 UnicodeDecodeError: 'ascii' codec can't decode byte 0xc3 in position 6: ordinal not in range(128)

--- lib/python/Tools/ISO639.py
+++ lib/python/Tools/ISO639.py
@@ -6,4 +6,7 @@ else:
 	import cPickle as pickle
 import enigma
 with open(enigma.eEnv.resolve("${datadir}/enigma2/iso-639-3.pck"), 'rb') as f:
-	LanguageCodes = pickle.load(f)
+	if version_info >= (3, 0):
+		LanguageCodes = pickle.load(f, encoding='utf-8')
+	else:
+		LanguageCodes = pickle.load(f)
