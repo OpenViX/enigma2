90b493a24d7d78a918b06b77ff2f4653e0d50a53
Dimitrij <Dima-73@inbox.lv>
2021-05-03 03:41:59 +0300

    [RecordTimer] fix zap timer use as PiP if play media file (#2969)
    -add iptv service  check

--- lib/python/RecordTimer.py
+++ lib/python/RecordTimer.py
@@ -594,9 +594,11 @@ class RecordTimerEntry(TimerEntry, object):
 				else:
 					self.sendactivesource()
 					notify = config.usage.show_message_when_recording_starts.value and self.InfoBarInstance and self.InfoBarInstance.execing
-					if self.pipzap:
-						cur_ref = NavigationInstance.instance.getCurrentlyPlayingServiceOrGroup()
-						if cur_ref and cur_ref != self.service_ref.ref and self.InfoBarInstance and hasattr(self.InfoBarInstance.session, 'pipshown') and not Components.ParentalControl.parentalControl.isProtected(self.service_ref.ref):
+					cur_ref = NavigationInstance.instance.getCurrentlyPlayingServiceReference()
+					pip_zap = self.pipzap or (cur_ref and cur_ref.getPath() and '%3a//' not in cur_ref.toString() and SystemInfo["PIPAvailable"])
+					if pip_zap:
+						cur_ref_group = NavigationInstance.instance.getCurrentlyPlayingServiceOrGroup()
+						if cur_ref_group and cur_ref_group != self.service_ref.ref and self.InfoBarInstance and hasattr(self.InfoBarInstance.session, 'pipshown') and not Components.ParentalControl.parentalControl.isProtected(self.service_ref.ref):
 							if self.InfoBarInstance.session.pipshown:
 								hasattr(self.InfoBarInstance, "showPiP") and self.InfoBarInstance.showPiP()
 							if hasattr(self.InfoBarInstance.session, 'pip'):
