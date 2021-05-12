ba9a67cfe05a4fde21b0c8dfc2165191c888bc8e
Huevos <rowland@moralet.com>
2021-05-12 23:42:10 +0200

    [epgcache] Test some code suggested by LraiZer

--- lib/service/event.cpp
+++ lib/service/event.cpp
@@ -206,7 +206,7 @@ bool eServiceEvent::loadLanguage(Event *evt, const std::string &lang, int tsidon
 	}
 
 	// hack to fix split titles
-	// undoAbbreviation(m_event_name, m_short_description);
+	undoAbbreviation(m_event_name, m_short_description);
 
 	return retval;
 }
