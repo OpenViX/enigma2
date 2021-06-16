381143c59d8b77742b09009591500a0c94d55b50
Twol <tonywhalley@t-online.de>
2021-06-16 15:15:27 +0200

    Hardisk - correct ERROR show on bustype due to invalid path check

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -582,7 +582,7 @@ class Harddisk:
 
 	def deviceState(self, device):
 		hotplugBuses = ("usb", "mmc", "pcmcia", "ieee1394", "firewire")
-		if not self.phys_path.startswith("/sys/devices/platform/"):
+		if not self.phys_path.startswith("/sys/devices/"):
 			return (False, "ERROR")
 		match = None
 		for bus in hotplugBuses:
@@ -591,7 +591,7 @@ class Harddisk:
 				break
 		if match:
 			# print "[Harddisk] DEBUG: Device is removable.  (device='%s', match='%s')" % (device, match)
-			return (False, match)
+			return (False, match.upper())
 		else:
 			# print "[Harddisk] DEBUG: Device is not removable.  (device='%s', No bus)" % (device)
 			return (True, "ATA")
