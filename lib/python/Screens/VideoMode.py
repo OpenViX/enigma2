97f4fe4ec933cf5d5f99c7e52fbea21dbc74a3f6
Huevos <rowland@moralet.com>
2021-07-23 21:51:55 +0200

    [VideoMode.py] Tweak autores for 480 and 576

--- lib/python/Screens/VideoMode.py
+++ lib/python/Screens/VideoMode.py
@@ -396,9 +396,9 @@ class AutoVideoMode(Screen):
 
 		if video_height and video_width and video_pol and video_rate:
 			resolutionlabel["content"].setText(_("Video content: %ix%i%s %iHz") % (video_width, video_height, video_pol, (video_rate + 500) / 1000))
-			if (700 < video_width <= 720) and video_height <= 480 and video_rate in (23976, 24000, 29970, 59940):
+			if (1 < video_width <= 1024) and video_height <= 480 and video_rate in (23976, 24000, 25000, 29970, 50000, 59940):
 				new_res = "480"
-			elif (700 < video_width <= 720) and video_height <= 576 and video_rate in (25000, 50000):
+			elif (1 < video_width <= 1024) and video_height <= 576 and video_rate in (23976, 24000, 25000, 50000):
 				new_res = "576"
 			elif (video_width == 1280) and video_height <= 720:
 				new_res = "720"
