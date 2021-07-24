93a2d4af7b9308c4f13274b31e83e5fbefb57703
Huevos <rowland@moralet.com>
2021-07-24 13:20:04 +0200

    [PiconBG.py] Use the full path to the Renderer module

--- lib/python/Components/Renderer/PiconBg.py
+++ lib/python/Components/Renderer/PiconBg.py
@@ -1,4 +1,4 @@
-from Renderer import Renderer
+from Components.Renderer.Renderer import Renderer
 from enigma import ePixmap
 from Components.config import config
 from Tools.Directories import SCOPE_CURRENT_SKIN, resolveFilename
