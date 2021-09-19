d4757346a0ff459e37d27f9f4759a84ca0e4d74f
Twol <tonywhalley@t-online.de>
2021-09-19 08:56:30 +0200

    correct handling of TrafficLight in py3 and also handle non-ViX online feeds in User Release/Dev builds

--- lib/python/Screens/PluginBrowser.py
+++ lib/python/Screens/PluginBrowser.py
@@ -504,7 +504,7 @@ class PluginDownloadBrowser(Screen):
 		self.listHeight = listsize.height()
 		if self.type == self.DOWNLOAD:
 			self.type = self.UPDATE
-			if (getImageType() != 'release' and feedsstatuscheck.getFeedsBool() != 'unknown') or (getImageType() == 'release' and feedsstatuscheck.getFeedsBool() not in ('stable', 'unstable')):
+			if (getImageType() != "release" and feedsstatuscheck.getFeedsBool() not in  ("unknown", "alien")) or (getImageType() == "release" and feedsstatuscheck.getFeedsBool() not in ("stable", "unstable", "alien")):
 				self["text"].setText(feedsstatuscheck.getFeedsErrorMessage())
 			elif getImageType() != 'release' or (config.softwareupdate.updateisunstable.value == '1' and config.softwareupdate.updatebeta.value):
 				self["text"].setText(_("WARNING: feeds may be unstable.") + '\n' + _("Downloading plugin information. Please wait..."))
