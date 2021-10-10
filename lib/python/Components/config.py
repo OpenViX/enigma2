8c3ecd65ef92411f196973fe6b7433d463e8098b
IanSav <IanSav@users.noreply.github.com>
2021-10-10 18:46:49 +1100

    [config.py] Allow cancel() to operate on ConfigSubSection
    This change allows a developer to use the cancel() method to operate on a SubSection and implies running the cancel() method for all elements of the SubSection.

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -2052,6 +2052,10 @@ class ConfigSubsection(object):
 		for x in self.content.items.values():
 			x.load()
 
+	def cancel(self):
+		for x in self.content.items.values():
+			x.cancel()
+
 	def dict(self):
 		return self.content.items
 
