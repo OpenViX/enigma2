d3614886a5b45cc96fe87786127de509c4273275
prl <prl@ozemail.com.au>
2021-10-19 15:14:29 +1100

    [EpgList,TimerList] Add IceTV timer icons
    Display IceTV as well as Autotimer icons for generated timers.

--- lib/python/Components/TimerList.py
+++ lib/python/Components/TimerList.py
@@ -88,7 +88,9 @@ class TimerList(GUIComponent):
 		if not processed:
 			if timer.state == TimerEntry.StateWaiting:
 				state = _("waiting")
-				if timer.isAutoTimer:
+				if self.iconIceTVTimer and hasattr(timer, "ice_timer_id") and timer.ice_timer_id:
+					icon = self.iconIceTVTimer
+				elif timer.isAutoTimer:
 					icon = self.iconAutoTimer
 				else:
 					icon = self.iconWait
@@ -156,6 +158,11 @@ class TimerList(GUIComponent):
 		self.iconDisabled = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/timer_off.png"))
 		self.iconFailed = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/timer_failed.png"))
 		self.iconAutoTimer = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/timer_autotimer.png"))
+		try:
+			from Plugins.SystemPlugins.IceTV import loadIceTVIcon
+			self.iconIceTVTimer = loadIceTVIcon("timer_icetv.png")
+		except ImportError:
+			self.iconIceTVTimer = None
 
 	def applySkin(self, desktop, parent):
 		def itemHeight(value):
