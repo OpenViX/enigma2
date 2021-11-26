949cb45100e6970b471cab180fa08c5bc845e1f9
Huevos <rowland@moralet.com>
2021-11-27 00:45:22 +0100

    [OScamInfo] add key_red to more screens

--- lib/python/Screens/OScamInfo.py
+++ lib/python/Screens/OScamInfo.py
@@ -910,11 +910,12 @@ class oscEntitlements(Screen, OscamInfo):
 		self.mlist = oscMenuList([])
 		self.cccamreader = reader
 		self["output"] = List([])
-		self["actions"] = ActionMap(["OkCancelActions"],
+		self["actions"] = ActionMap(["SetupActions"],
 					{
 						"ok": self.showData,
 						"cancel": self.exit
 					}, -1)
+		self["key_red"] = StaticText(_("Close"))
 		self.onLayoutFinish.append(self.showData)
 
 	def exit(self):
@@ -1051,11 +1052,12 @@ class oscReaderStats(Screen, OscamInfo):
 		self.reader = reader
 		self.mlist = oscMenuList([])
 		self["output"] = List([])
-		self["actions"] = ActionMap(["OkCancelActions"],
+		self["actions"] = ActionMap(["SetupActions"],
 					{
 						"ok": self.showData,
 						"cancel": self.exit
 					}, -1)
+		self["key_red"] = StaticText(_("Close"))
 		self.onLayoutFinish.append(self.showData)
 
 	def exit(self):
