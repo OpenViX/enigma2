d3d776a8addea1da463c31af446e3b7705e871bd
Huevos <rowland@moralet.com>
2021-12-30 09:48:53 +0100

    [PiPSetup] Force some values to int

--- lib/python/Screens/PiPSetup.py
+++ lib/python/Screens/PiPSetup.py
@@ -82,22 +82,22 @@ class PiPSetup(Screen):
 
 	def setPiPPosition(self):
 		self.pip.resize(int(self.size[0]), int(self.size[1]))
-		self.pip.move(self.pos[0], self.pos[1])
+		self.pip.move(int(self.pos[0]), int(self.pos[1]))
 
 	def resizePiP(self, resize):
 		resize += 100 # resize is in percent, so resize=+20 means: 120%
 
 		oldsize = self.size
 		if self.mode != "split":
-			w = clip(self.size[0] * resize / 100, MIN_W, MAX_W)
-			h = clip(self.size[1] * resize / 100, MIN_H, MAX_H)
+			w = clip(self.size[0] * resize // 100, MIN_W, MAX_W)
+			h = clip(self.size[1] * resize // 100, MIN_H, MAX_H)
 		else:
-			w = clip(self.size[0] * resize / 100, MAX_X / 2, MAX_X)
-			h = clip(self.size[1] * resize / 100, MAX_Y / 2, MAX_Y)
+			w = clip(self.size[0] * resize // 100, MAX_X // 2, MAX_X)
+			h = clip(self.size[1] * resize // 100, MAX_Y // 2, MAX_Y)
 
 		# calculate offset from center
-		mx = (oldsize[0] - w) / 2
-		my = (oldsize[1] - h) / 2
+		mx = (oldsize[0] - w) // 2
+		my = (oldsize[1] - h) // 2
 
 		self.size = (w, h)
 		# reclip, account for new center
@@ -133,16 +133,16 @@ class PiPSetup(Screen):
 
 	def keyNumberGlobal(self, number):
 		if number > 0 and self.mode == "standard":
-			colsize = MAX_X / 3
-			rowsize = MAX_Y / 3
+			colsize = MAX_X // 3
+			rowsize = MAX_Y // 3
 			col = (number - 1) % 3
-			row = (number - 1) / 3
+			row = (number - 1) // 3
 
 			self.size = (180, 135)
 
 			# offset to keep center
-			ox = (colsize - self.size[0]) / 2
-			oy = (rowsize - self.size[1]) / 2
+			ox = (colsize - self.size[0]) // 2
+			oy = (rowsize - self.size[1]) // 2
 
 			self.pos = (col * colsize + ox, row * rowsize + oy)
 		elif number == 0:
