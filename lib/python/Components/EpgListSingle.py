c6164c251e4f6bdd0b3c68406ffb1c150b7350c5
Huevos <rowland@moralet.com>
2022-01-08 18:25:03 +0100

    [EpgListSingle] cast time() to int() to avoid... TypeError: 'float' object cannot be interpreted as an integer

--- lib/python/Components/EpgListSingle.py
+++ lib/python/Components/EpgListSingle.py
@@ -108,7 +108,7 @@ class EPGListSingle(EPGListBase):
 		self.selectionChanged()
 
 	def fillEPG(self, service):
-		now = time()
+		now = int(time())
 		epgTime = now - config.epg.histminutes.value * SECS_IN_MIN
 		test = ['RIBDT', (service.toString(), 0, epgTime, -1)]
 		self.list = self.queryEPG(test)
