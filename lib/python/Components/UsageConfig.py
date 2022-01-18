02b33cbbf83258db32eccfdb3452ec2f14d7ebf3
Twol <tonywhalley@t-online.de>
2022-01-18 09:57:40 +0100

    HdmiCec - allow user to set fixed Cec address

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1195,6 +1195,7 @@ def InitUsageConfig():
 		"textview": _("Text View On"),
 		},
 		default="imageview")
+	config.hdmicec.fixed_physical_address = ConfigText(default="0.0.0.0")		
 	config.hdmicec.volume_forwarding = ConfigYesNo(default=False)
 	config.hdmicec.control_receiver_wakeup = ConfigYesNo(default=False)
 	config.hdmicec.control_receiver_standby = ConfigYesNo(default=False)
