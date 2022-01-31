fc47f6091f4c97285f6fe30cf25a76c5f50bbf7a
Huevos <rowland@moralet.com>
2022-02-01 00:43:24 +0100

    [Downloader.py] Twisted works with byte strings

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -15,14 +15,14 @@ except ImportError: # Python 2
 
 class HTTPProgressDownloader(client.HTTPDownloader):
 	def __init__(self, url, outfile, headers=None):
-		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="%s %s Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenViX;;;)" % (getMachineBrand(), getMachineName()))
+		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent=b"%s %s Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenViX;;;)" % (getMachineBrand(), getMachineName()))
 		self.status = self.progress_callback = self.error_callback = self.end_callback = None
 		self.deferred = defer.Deferred()
 
 	def noPage(self, reason):
 		if self.status == b"304":
 			print(reason.getErrorMessage())
-			client.HTTPDownloader.page(self, "")
+			client.HTTPDownloader.page(self, b"")
 		else:
 			client.HTTPDownloader.noPage(self, reason)
 		if self.error_callback:
