bd37e6bd074a9fa34a6bc538aa3014af98768612
Pr2 <pr2git@users.noreply.github.com>
2022-02-06 15:30:46 +0100

    Fix theme assignment based on ETSI EN 300 707 V1.2.1 (en_300707v010201p.pdf), current implementation was based on draft
    https://github.com/OpenPLi/enigma2/commit/77d61daba9fc93fd9148ef31d65038c2de41e587

--- lib/python/Components/Converter/genre.py
+++ lib/python/Components/Converter/genre.py
@@ -9,31 +9,27 @@
 class GenresETSI:
 	maintype = (
 		_("Reserved"),
-		_("Movie/Drama"),
-		_("News/Current Affairs"),
-		_("Show/Games show"),
+		_("Drama and Films"),
+		_("News/Current Affairs/Social"),
+		_("Show/Game Show/Leisure hobbies"),
 		_("Sports"),
-		_("Children/Youth"),
+		_("Children/Youth/Education/Science"),
 		_("Music/Ballet/Dance"),
-		_("Arts/Culture"),
-		_("Social/Political/Economics"),
-		_("Education/Science/Factual"),
-		_("Leisure hobbies"),
-		_("Other")
+		_("Arts/Culture (without music)")
 	)
 
 	subtype = {
 		# Movie/Drama
 		1: (
-			_("movie/drama (general)"),
+			_("movie (general)"),
 			_("detective/thriller"),
 			_("adventure/western/war"),
 			_("science fiction/fantasy/horror"),
 			_("comedy"),
 			_("soap/melodrama/folkloric"),
 			_("romance"),
-			_("serious/classical/religious/historical movie/drama"),
-			_("adult movie/drama")
+			_("serious/classical/religious/historical drama"),
+			_("adult movie")
 		),
 		# News/Current Affairs
 		2: (
@@ -41,14 +37,25 @@ class GenresETSI:
 			_("news/weather report"),
 			_("news magazine"),
 			_("documentary"),
-			_("discussion/interview/debate")
+			_("discussion/interview/debate"),
+			_("social/political issues/economics (general)"),
+			_("magazines/reports/documentary"),
+			_("economics/social advisory"),
+			_("remarkable people")
 		),
 		# Show/Game show
 		3: (
 			_("show/game show (general)"),
 			_("game show/quiz/contest"),
 			_("variety show"),
-			_("talk show")
+			_("talk show"),
+			_("leisure hobbies (general)"),
+			_("tourism/travel"),
+			_("handicraft"),
+			_("motoring"),
+			_("fitness and health"),
+			_("cooking"),
+			_("advertisement/shopping")
 		),
 		# Sports
 		4: (
@@ -57,22 +64,31 @@ class GenresETSI:
 			_("sports magazine"),
 			_("football/soccer"),
 			_("tennis/squash"),
-			_("team sports"),
+			_("team sports/excluding football"),
 			_("athletics"),
 			_("motor sport"),
 			_("water sport"),
 			_("winter sport"),
 			_("equestrian"),
-			_("martial sports")
+			_("martial sports"),
+			_("local sports")
 		),
 		# Children/Youth
 		5: (
-			_("children's/youth program (general)"),
+			_("childrens's youth program (general)"),
 			_("pre-school children's program"),
 			_("entertainment (6-14 year old)"),
 			_("entertainment (10-16 year old)"),
 			_("information/education/school program"),
-			_("cartoon/puppets")
+			_("cartoon/puppets"),
+			_("educational/science/factual topics (general)"),
+			_("nature/animals/environment"),
+			_("technology/natural sciences"),
+			_("medicine/physiology/psychology"),
+			_("foreign countries/expeditions"),
+			_("social/spiritual sciences"),
+			_("further education"),
+			_("languages")
 		),
 		# Music/Ballet/Dance
 		6: (
