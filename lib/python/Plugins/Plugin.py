c1daa54447d2d6cb23d9383706ed511356d29e35
Huevos <rowland@moralet.com>
2022-02-13 00:02:59 +0100

    [Plugins/Plugin.py] Fix variable name

--- lib/python/Plugins/Plugin.py
+++ lib/python/Plugins/Plugin.py
@@ -132,10 +132,10 @@ class PluginDescriptor():
 			return self._icon
 
 	def __eq__(self, other):
-		return self.fnc == other._fnc
+		return self.fnc == other.fnc
 
 	def __ne__(self, other):
-		return self.fnc != other._fnc
+		return self.fnc != other.fnc
 
 	def __lt__(self, other):
 		if self.weight < other.weight:
