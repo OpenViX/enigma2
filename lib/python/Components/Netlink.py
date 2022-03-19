02d81d40af9c62a7e6421e60de1b511ba9c4af6d
Twol <tonywhalley@t-online.de>
2022-03-19 12:16:52 +0100

    Netlink - import only getpid from os

--- lib/python/Components/Netlink.py
+++ lib/python/Components/Netlink.py
@@ -1,6 +1,6 @@
 # Listen to hotplug events. Can be used to listen for hotplug events and
 # similar things, like network connections being (un)plugged.
-import os
+from os import getpid
 import socket
 import six
 
@@ -9,7 +9,7 @@ class NetlinkSocket(socket.socket):
 	def __init__(self):
 		NETLINK_KOBJECT_UEVENT = 15 # hasn't landed in socket yet, see linux/netlink.h
 		socket.socket.__init__(self, socket.AF_NETLINK, socket.SOCK_DGRAM, NETLINK_KOBJECT_UEVENT)
-		self.bind((os.getpid(), -1))
+		self.bind((getpid(), -1))
 
 	def parse(self):
 		data = six.ensure_str(self.recv(512), encoding="ascii", errors='ignore')
