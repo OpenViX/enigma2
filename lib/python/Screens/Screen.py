dcae6ab8c75f0e143b1f182a0198f255fadb1062
Huevos <rowland@moralet.com>
2022-03-22 11:31:35 +0100

    [Screen] removeSummary: add sanity for possible missing summary

--- lib/python/Screens/Screen.py
+++ lib/python/Screens/Screen.py
@@ -288,7 +288,7 @@ class Screen(dict):
 			self.summaries.append(summary)
 
 	def removeSummary(self, summary):
-		if summary is not None:
+		if summary is not None and summary in self.summaries:
 			self.summaries.remove(summary)
 
 
