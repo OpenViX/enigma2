589cab1c59e0daf3be754acf54c2e110d18aa9fb
Huevos <rowland@moralet.com>
2022-04-05 17:38:53 +0200

    Revert "[FIFOList] change self.list to self.fifoList..."
    This reverts commit 5d4f7ea3dbe61e873ef0ee009616385b5ae3db4a.

--- lib/python/Components/FIFOList.py
+++ lib/python/Components/FIFOList.py
@@ -4,20 +4,20 @@ from Components.MenuList import MenuList
 class FIFOList(MenuList):
 	def __init__(self, list=[], len=10):
 		self.len = len
-		self.fifoList = list
-		MenuList.__init__(self, self.fifoList)
+		self.list = list
+		MenuList.__init__(self, self.list)
 
 	def addItem(self, item):
-		self.fifoList.append(item)
-		self.setList(self.fifoList[-self.len:])
+		self.list.append(item)
+		self.l.setList(self.list[-self.len:])
 
 	def clear(self):
-		del self.fifoList[:]
-		self.setList(self.fifoList)
+		del self.list[:]
+		self.l.setList(self.list)
 
 	def getCurrentSelection(self):
-		return self.fifoList and self.getCurrent() or None
+		return self.list and self.getCurrent() or None
 
 	def listAll(self):
-		self.setList(self.fifoList)
+		self.l.setList(self.list)
 		self.selectionEnabled(True)
