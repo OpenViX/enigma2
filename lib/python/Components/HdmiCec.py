bfd99429789d57d3e1ad955eac6a9a955340c1f9
Twol <tonywhalley@t-online.de>
2022-04-19 15:57:43 +0200

    Deep Standby - place hdmicec code in Standby, as some boxes drop into Deep Standby before HdmiCec routine can send through C++

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -361,7 +361,7 @@ class HdmiCec:
 			setFixedPhysicalAddress("0.0.0.0")			# no fixed physical address send 0 to eHdmiCec C++ driver	
 		eHdmiCEC.getInstance().messageReceived.get().append(self.messageReceived)
 		config.misc.standbyCounter.addNotifier(self.onEnterStandby, initial_call=False)
-		config.misc.DeepStandby.addNotifier(self.onEnterDeepStandby, initial_call=False)
+#		config.misc.DeepStandby.addNotifier(self.onEnterDeepStandby, initial_call=False)
 		self.volumeForwardingEnabled = False
 		self.volumeForwardingDestination = 0
 		self.wakeup_from_tv = False
