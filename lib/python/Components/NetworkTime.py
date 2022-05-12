4ff4b7b89e6406b9e997b1e803c4364a530629be
Huevos <rowland@moralet.com>
2022-05-12 22:21:47 +0200

    [NetworkTime] move setUseDVBTime out of the conditional

--- lib/python/Components/NetworkTime.py
+++ lib/python/Components/NetworkTime.py
@@ -34,7 +34,7 @@ class NTPSyncPoller:
 
 	def timecheck(self):
 		if config.misc.SyncTimeUsing.value == "ntp":
-			print('[NetworkTime] Updating')
+			print('[NetworkTime] Updating from NTP')
 			self.Console.ePopen(["/usr/sbin/ntpd", "/usr/sbin/ntpd", "-nq", "-p", config.misc.NTPserver.value], self.update_schedule)
 		else:
 			self.update_schedule()
@@ -42,11 +42,11 @@ class NTPSyncPoller:
 	def update_schedule(self, result=None, retval=None, extra_args=None):
 		if retval and result:
 			print("[NetworkTime] Error %d: Unable to synchronize the time!\n%s" % (retval, result.strip()))
+		eDVBLocalTimeHandler.getInstance().setUseDVBTime(config.misc.SyncTimeUsing.value == "dvb")
 		nowTime = time()
 		if nowTime > 10000:
 			print('[NetworkTime] setting E2 time:', nowTime)
 			setRTCtime(nowTime)
-			eDVBLocalTimeHandler.getInstance().setUseDVBTime(config.misc.SyncTimeUsing.value == "dvb")
 			eEPGCache.getInstance().timeUpdated()
 			self.timer.startLongTimer(int(config.misc.useNTPminutes.value if config.misc.SyncTimeUsing.value == "ntp" else config.misc.useNTPminutes.default) * 60)
 		else:
