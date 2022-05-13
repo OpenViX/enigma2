e0edc2668430b490e515515b849f967ce3ed47cc
Twol <tonywhalley@t-online.de>
2022-05-13 15:11:48 +0200

    StartEnigma - RcModel is not used by StartEnigma so delete load

--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -691,8 +691,6 @@ profile("Init:CI")
 import Screens.Ci
 Screens.Ci.InitCiConfig()
 
-profile("RcModel")
-import Components.RcModel
 
 if config.clientmode.enabled.value:
 	import Components.ChannelsImporter
