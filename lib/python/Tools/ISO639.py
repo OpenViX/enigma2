3f5ad80319688fe3545a1f1522c87db5d71617aa
Twol <tonywhalley@t-online.de>
2022-05-27 13:43:37 +0200

    ISO639 - remove python 2 code

--- lib/python/Tools/ISO639.py
+++ lib/python/Tools/ISO639.py
@@ -1,12 +1,4 @@
-from sys import version_info
-
-if version_info >= (3, 0):
-	import pickle
-else:
-	import cPickle as pickle
+import pickle
 import enigma
 with open(enigma.eEnv.resolve("${datadir}/enigma2/iso-639-3.pck"), 'rb') as f:
-	if version_info >= (3, 0):
-		LanguageCodes = pickle.load(f, encoding='utf-8')
-	else:
-		LanguageCodes = pickle.load(f)
+	LanguageCodes = pickle.load(f, encoding='utf-8')
