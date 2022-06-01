d98c94b23bca54d0534d868325e64ef8f1dd6fee
sfeary <sfeary@users.noreply.github.com>
2022-06-01 19:55:39 +0100

    Update InfoBar.py
    Minor addition to allow the exit button to behave the same as the stop button and exit the media player. Needed from an accessibility perspective for a user with limited mobility.

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -360,6 +360,8 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, InfoBarLongKeyDetection, InfoBar
 			self.session.openWithCallback(self.leavePlayerOnExitCallback, MessageBox, _("Exit movie player?"), simple=True)
 		elif config.usage.leave_movieplayer_onExit.value == "without popup":
 			self.leavePlayerOnExitCallback(True)
+		elif config.usage.leave_movieplayer_onExit.value == "stop":
+			self.leavePlayer()
 
 	def leavePlayerOnExitCallback(self, answer):
 		if answer:
