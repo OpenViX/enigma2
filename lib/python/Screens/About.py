33f2846554341dbd50d9632bea1a3a08519ef0cd
Orlandoxx <95180049+Orlandoxx@users.noreply.github.com>
2022-06-18 18:55:20 +0300

    One localization fix of About.py
    Fixing localisation.

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -332,7 +332,7 @@ class Devices(Screen):
 		if pathExists("/media/autofs"):
 			for entry in sorted(listdir("/media/autofs")):
 				mountEntry = path.join("/media/autofs", entry)
-				self.mountinfo += _("\n %s is also enabled for autofs network mount" % (mountEntry))
+				self.mountinfo += _("\n %s is also enabled for autofs network mount") % (mountEntry)
 		if self.mountinfo:
 			self["mounts"].setText(self.mountinfo)
 		else:
