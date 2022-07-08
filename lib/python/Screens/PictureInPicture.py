57995d9c6f6da88fd4f74ee45668b621c5b22516
Twol <tonywhalley@t-online.de>
2022-07-08 16:28:55 +0200

    PictureInPicture - stop freeze when user initiates pipswap, if servicetype is 4097 & currently playing

--- lib/python/Screens/PictureInPicture.py
+++ lib/python/Screens/PictureInPicture.py
@@ -227,6 +227,10 @@ class PictureInPicture(Screen):
 				if not config.usage.hide_zap_errors.value:
 					Tools.Notifications.AddPopup(text="PiP...\n" + _("Connected transcoding, limit - no PiP!"), type=MessageBox.TYPE_ERROR, timeout=5, id="ZapPipError")
 				return False
+			if ref and "4097" in ref.toString():
+				self.pipservice = None
+				Tools.Notifications.AddPopup(text=_("Service type 4097 incorrect for PiP!"), type=MessageBox.TYPE_ERROR, timeout=5, id="ZapPipError")			
+				return False				
 			if self.isPlayableForPipService(ref):
 				print("[PictureInPicture] playing pip service", ref and ref.toString())
 			else:
