b004c472d77187bdf29372189f59aef6cab4ec7e
Betacentauri <betacentauri@arcor.de>
2022-09-03 13:35:19 +0200

    Add proper return values in start procedures
    to properly handle problems e.g. with the tuner or streaming
    
    (cherry picked from commit f9b70c685552140a40bb76cd9b4e7ecd7351e540)

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1333,6 +1333,7 @@ RESULT eDVBServicePlay::start()
 	eServiceReferenceDVB service = (eServiceReferenceDVB&)m_reference;
 	bool scrambled = true;
 	int packetsize = 188;
+	RESULT ret = 0;
 	eDVBServicePMTHandler::serviceType type = eDVBServicePMTHandler::livetv;
 	eDebug("[servicedvb][eDVBServicePlay][start] m_is_stream set to: %d", m_is_stream);
 	eDebug("[servicedvb][eDVBServicePlay][start] m_is_pvr set to; %d", m_is_pvr);
@@ -1381,7 +1382,7 @@ RESULT eDVBServicePlay::start()
 
 	m_first_program_info = 1;
 	ePtr<iTsSource> source = createTsSource(service, packetsize);
-	m_service_handler.tuneExt(service, source, service.path.c_str(), m_cue, false, m_dvb_service, type, scrambled);
+	ret = m_service_handler.tuneExt(service, source, service.path.c_str(), m_cue, false, m_dvb_service, type, scrambled);
 
 	if (m_is_pvr)
 	{
@@ -1398,7 +1399,7 @@ RESULT eDVBServicePlay::start()
 		}
 		m_event(this, evStart);
 	}
-	return 0;
+	return ret;
 }
 
 RESULT eDVBServicePlay::stop()
