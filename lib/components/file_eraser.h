31177f0f6fbf286d2a7fb6597cacbb392ee03780
Twol <tonywhalley@t-online.de>
2022-09-18 15:00:21 +0200

    eraser.h remove unused flags etc after last commit

--- lib/components/file_eraser.h
+++ lib/components/file_eraser.h
@@ -22,8 +22,6 @@ class eBackgroundFileEraser: public eMainloop, private eThread, public sigc::tra
 	void thread();
 	void idle();
 	ePtr<eTimer> stop_thread_timer;
-	off_t erase_speed;
-	int erase_flags;
 #ifndef SWIG
 public:
 #endif
@@ -41,8 +39,6 @@ public:
 #endif
 	void erase(const std::string& filename);
 	void erase(const char* filename2);
-	void setEraseSpeed(int inMBperSecond);
-	void setEraseFlags(int flags);
 	static eBackgroundFileEraser *getInstance() { return instance; }
 	static const int ERASE_FLAG_HDD = 1;
 	static const int ERASE_FLAG_OTHER = 2;
