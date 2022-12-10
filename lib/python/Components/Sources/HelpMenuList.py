69d6c870f96c7b623b903d4f8427a30d9b9d4940
Rowland Creed <rowland@moralet.com>
2022-12-10 14:44:27 +0100

    [HelpMenu] add cmp (missing in Python 3)

--- lib/python/Components/Sources/HelpMenuList.py
+++ lib/python/Components/Sources/HelpMenuList.py
@@ -4,6 +4,17 @@ from Components.config import config
 from collections import defaultdict
 from functools import cmp_to_key
 
+def cmp(x, y):
+	"""
+	Replacement for built-in function cmp that was removed in Python 3
+
+	Compare the two objects x and y and return an integer according to
+	the outcome. The return value is negative if x < y, zero if x == y
+	and strictly positive if x > y.
+	"""
+	return (x > y) - (x < y)
+
+
 # Helplist structure:
 # [ ( actionmap, context, [(action, help), (action, help), ...] ), (actionmap, ... ), ... ]
 
