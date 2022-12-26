c3e6b24ee8527e0ff208c6f053bcaa809aafab88
Rowland Creed <rowland@moralet.com>
2022-12-26 21:06:34 +0100

    [RcPositions] read remaps
    Simplify previous commits.
    
    Store parsed remote to avoid reading xml every time the class is used.

--- lib/python/Screens/Rc.py
+++ lib/python/Screens/Rc.py
@@ -144,52 +144,51 @@ class Rc:
 
 
 class RcPositions:
+	rc = None
 	def __init__(self):
-		self._makeLabels()
+		if RcPositions.rc is not None:
+			return
+		descriptions = [{v[0]:k for k,v in x.items()} for x in keyDescriptions] # used by wizards and legacy xml format
 		file = resolveFilename(SCOPE_SKIN, "rcpositions.xml") if SystemInfo["rc_default"] else SystemInfo["RCMapping"]
 		rcs = fileReadXML(file, "<rcs />")
-		self.rcs = {}
+		remotes = {}
 		machine_id = 2
 		for rc in rcs.findall("rc"):
-			self.rc_id = int(rc.attrib.get("id", machine_id))
-			self.rcs[self.rc_id] = {}
-			self.rcs[self.rc_id]["keyIds"] = []
+			rc_id = int(rc.attrib.get("id", machine_id))
+			remotes[rc_id] = {}
+			remotes[rc_id]["keyIds"] = []
+			remotes[rc_id]["remaps"] = {}
+			remotes[rc_id]["keyDescriptions"] = descriptions[rc_id]
 			for key in rc.findall("button"):
-				if "name" in key.attrib: # legacy
-					keyId = self.keyDescriptions[self.rc_id].get(key.attrib["name"])
-					if keyId:
-						self.rcs[self.rc_id]["keyIds"].append(keyId)
-						self.rcs[self.rc_id][keyId] = {}
-						self.rcs[self.rc_id][keyId]["label"] = key.attrib["name"]
-						self.rcs[self.rc_id][keyId]["pos"] = [int(x.strip()) for x in key.attrib.get("pos", "0,0").split(",")]
+				if "name" in key.attrib: # legacy xml format
+					keyId = descriptions[rc_id].get(key.attrib["name"])
 				elif  "id" in key.attrib: # oe-remotes
 					keyId = KEYIDS.get(key.attrib["id"])
-					if keyId:
-						self.rcs[self.rc_id]["keyIds"].append(keyId)
-						self.rcs[self.rc_id][keyId] = {}
-						self.rcs[self.rc_id][keyId]["label"] = key.attrib.get("label", "UNKNOWN")
-						self.rcs[self.rc_id][keyId]["pos"] = [int(x.strip()) for x in key.attrib.get("pos", "0,0").split(",")]
+				if keyId:
+					remotes[rc_id]["keyIds"].append(keyId)
+					remotes[rc_id][keyId] = {}
+					remotes[rc_id][keyId]["label"] = key.attrib.get("name") or key.attrib.get("label", "UNKNOWN")
+					remotes[rc_id][keyId]["pos"] = [int(x.strip()) for x in key.attrib.get("pos", "0,0").split(",")]
+					remap = key.attrib.get("remap")
+					if remap is not None and remap in KEYIDS:
+						remapId = KEYIDS[remap]
+						remotes[rc_id]["remaps"][keyId] = remapId
+						remotes[rc_id][remapId] = remotes[rc_id][keyId] # so the button remaps in the help screen
+
 		if SystemInfo["rc_default"]:
-			self.rc = self.rcs[config.misc.rcused.value]
+			RcPositions.rc = remotes[config.misc.rcused.value]
 		else:
 			try:
-				self.rc = self.rcs[machine_id]
-				self.rc_id = machine_id
+				RcPositions.rc = remotes[machine_id]
 			except:
-				self.rc = self.rcs[config.misc.rcused.value]
-				self.rc_id = config.misc.rcused.value
+				RcPositions.rc = remotes[config.misc.rcused.value]
 
-	def _makeLabels(self): # legacy
-		self.keyDescriptions = []
-		for n in range(len(keyDescriptions)):
-			self.keyDescriptions.append({v[0]:k for k,v in keyDescriptions[n].items()})
-	
 	def getRc(self):
 		return self.rc
 
 	def getRcKeyPos(self, keyId):
-		if isinstance(keyId, str):
-			keyId = self.keyDescriptions[self.rc_id].get(keyId, 0)
+		if isinstance(keyId, str): # used by wizards and available to legacy code
+			keyId = self.rc["keyDescriptions"].get(keyId, 0)
 		if keyId in self.rc:
 			return self.rc[keyId]["pos"]
 		return None
@@ -201,3 +200,6 @@ class RcPositions:
 
 	def getRcKeyList(self):
 		return self.rc["keyIds"]
+
+	def getRcKeyRemaps(self):
+		return self.rc["remaps"]
