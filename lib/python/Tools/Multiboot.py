3c693cc9b0f66a705d2db3baaa90f88c1c205e69
Twol <tonywhalley@t-online.de>
2023-02-09 18:06:44 +0100

    MultiBoot - simplify active slot selection

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -88,8 +88,8 @@ def getMultibootslots():
 		print("[Multiboot][MultiBootSlot] bootArgs:", bootArgs)
 		if fileHas("/proc/cmdline", "kexec=1") and SystemInfo["HasRootSubdir"]:							# Kexec Vu+ receiver
 			rootsubdir = [x for x in bootArgs.split() if x.startswith("rootsubdir")]
-			linuxroot = rootsubdir[0].split("=")[1]
-			SystemInfo["MultiBootSlot"] = int(linuxroot[11:]) if len(linuxroot) > 11 else int(linuxroot[-1])
+			linuxSlot = rootsubdir[0].split("=")[1].replace("linuxrootfs", "")
+			SystemInfo["MultiBootSlot"] = int(linuxSlot)
 			print("[Multiboot][MultiBootSlot]0 current slot used:", SystemInfo["MultiBootSlot"])				
 		elif SystemInfo["HasRootSubdir"] and "root=/dev/sda" not in bootArgs:							# RootSubdir receiver or sf8008 receiver with root in eMMC slot 
 			slot = [x[-1] for x in bootArgs.split() if x.startswith("rootsubdir")]
