b322ca110cd70481d0cf7364c10ca32f0c6876ee
Twol <tonywhalley@t-online.de>
2023-02-22 12:23:09 +0100

    Vu Multiboot STARTUP's - change bootwait delay for Duo4K & Duo4kse

--- lib/python/Screens/MultiBootSelector.py
+++ lib/python/Screens/MultiBootSelector.py
@@ -202,7 +202,9 @@ class MultiBootSelector(Screen, HelpableScreen):
 			boxmodel = getBoxType()[2:]
 			for usbslot in range(hiKey+1, hiKey+5):
 				STARTUP_usbslot = "kernel=%s/linuxrootfs%d/zImage root=%s rootsubdir=%s/linuxrootfs%d" % (boxmodel, usbslot, SystemInfo["VuUUIDSlot"][0], boxmodel, usbslot) # /STARTUP_<n>
-				if boxmodel in ("duo4k", "duo4kse"):
+				if boxmodel in ("duo4k"):
+					STARTUP_usbslot += " rootwait=40"
+				elif boxmodel in ("duo4kse"):
 					STARTUP_usbslot += " rootwait=35"
 				with open("/%s/STARTUP_%d" % (self.tmp_dir, usbslot), 'w') as f:
 					f.write(STARTUP_usbslot)
@@ -219,7 +221,9 @@ class MultiBootSelector(Screen, HelpableScreen):
 
 		for usbslot in range(4,8):
 			STARTUP_usbslot = "kernel=%s/linuxrootfs%d/zImage root=%s rootsubdir=%s/linuxrootfs%d" % (boxmodel, usbslot, self.device_uuid, boxmodel, usbslot) # /STARTUP_<n>
-			if boxmodel in ("duo4k", "duo4kse"):
+			if boxmodel in ("duo4k"):
+				STARTUP_usbslot += " rootwait=40"
+			elif boxmodel in ("duo4kse"):
 				STARTUP_usbslot += " rootwait=35"
 			print("[MultiBootSelector] STARTUP_%d --> %s, self.tmp_dir: %s" % (usbslot, STARTUP_usbslot, self.tmp_dir))
 			with open("/%s/STARTUP_%d" % (self.tmp_dir, usbslot), 'w') as f:
