a978c830181453a5904c11c62ded21408da29242
Rowland Creed <rowland@moralet.com>
2023-03-10 11:11:40 +0100

    Revert "[elistbox] temp test"
    This reverts commit 3219a4b0a3d7aa51e2ef91c4662ed118bb8d3beb.

--- lib/gui/elistbox.cpp
+++ lib/gui/elistbox.cpp
@@ -73,7 +73,7 @@ void eListbox::allowNativeKeys(bool allow)
 		ePtr<eActionMap> ptr;
 		eActionMap::getInstance(ptr);
 		if (allow)
-			ptr->bindAction("ListboxActions", 2, 0, this);
+			ptr->bindAction("ListboxActions", 0, 0, this);
 		else
 			ptr->unbindAction(this, 0);
 		m_native_keys_bound = allow;
