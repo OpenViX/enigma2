0078895f2ab44b73e24b359b0832a70aea576160
Rowland Creed <rowland@moralet.com>
2023-05-21 21:14:27 +0200

    [About] simplify date format commits

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -17,21 +17,20 @@ def getVersionString():
 def getFlashDateString():
 	if ospath.isfile('/etc/install'):
 		with open("/etc/install", "r") as f:
-			Date = f.read()
-			from Components.config import config
-			return config.usage.date.dateFormatAbout.value % {"year": Date[0:4], "month": Date[4:6], "day": Date[6:8]}
+			return _formatDate(f.read())
 	else:
 		return _("unknown")
 
 def driversDate(): 
-	Date = getDriverDate()
-	from Components.config import config
-	return config.usage.date.dateFormatAbout.value % {"year": Date[0:4], "month": Date[4:6], "day": Date[6:8]}
+	return _formatDate(getDriverDate())
 
 def getLastUpdate():
-	Date = getEnigmaVersionString()
+	return _formatDate(getEnigmaVersionString().replace("-", ""))
+
+def _formatDate(Date):
+	# expected input = "YYYYMMDD"
 	from Components.config import config
-	return config.usage.date.dateFormatAbout.value % {"year": Date[0:4], "month": Date[5:7], "day": Date[8:]}
+	return config.usage.date.dateFormatAbout.value % {"year": Date[0:4], "month": Date[4:6], "day": Date[6:8]}
 
 def getGStreamerVersionString():
 	try:
