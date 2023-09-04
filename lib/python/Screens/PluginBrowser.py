12a39792874d3a9651b60ae9b286a4dc6173a975
Huevos <rowland@moralet.com>
2023-09-04 14:27:43 +0200

    [PluginBrowser] fix sorting self.pluginlist 500 times where once is desired.

--- lib/python/Screens/PluginBrowser.py
+++ lib/python/Screens/PluginBrowser.py
@@ -619,7 +619,7 @@ class PluginDownloadBrowser(Screen):
 										plugin.append('')
 								plugin.append(plugin[0][15:])
 								self.pluginlist.append(plugin)
-			self.pluginlist.sort()
+		self.pluginlist.sort()
 
 	def updateList(self):
 		updatedlist = []
