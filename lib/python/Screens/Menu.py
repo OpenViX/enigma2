cf61b5261f7ed360982d388d5fa2a9d1cefd4601
Huevos <rowland@moralet.com>
2023-09-10 17:40:56 +0200

    [Menu] simplify description

--- lib/python/Screens/Menu.py
+++ lib/python/Screens/Menu.py
@@ -103,8 +103,7 @@ class Menu(Screen, HelpableScreen, ProtectedScreen):
 			return
 		menu_text = _(x) if (x := node.get("text")) else "* fix me *"
 		weight = node.get("weight", 50)
-		description = node.get("description", "") or None
-		description = description and _(description)
+		description = _(x) if (x := node.get("description", "")) else None
 		menupng = MenuEntryPixmap(key, self.png_cache)
 		x = node.get("flushConfigOnClose")
 		if x:
@@ -141,8 +140,7 @@ class Menu(Screen, HelpableScreen, ProtectedScreen):
 			return
 		item_text = _(x) if (x := node.get("text")) else "* fix me *"
 		weight = node.get("weight", 50)
-		description = node.get("description", "") or None
-		description = description and _(description)
+		description = _(x) if (x := node.get("description", "")) else None
 		menupng = MenuEntryPixmap(key, self.png_cache)
 		for x in node:
 			if x.tag == 'screen':
@@ -303,7 +301,7 @@ class Menu(Screen, HelpableScreen, ProtectedScreen):
 					if x[2] == plugin_menuid:
 						self.list.remove(x)
 						break
-				description = ""  # plugins.getDescriptionForMenuEntryID(self.menuID, plugin_menuid) # commented out as it is super slow
+				description = None  # plugins.getDescriptionForMenuEntryID(self.menuID, plugin_menuid) # commented out as it is super slow
 				menupng = MenuEntryPixmap(l[2], self.png_cache)
 				self.list.append((l[0], boundFunction(l[1], self.session, close=self.close), l[2], l[3] or 50, description, menupng))
 
