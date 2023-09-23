03b51aa315247c3c09e19009792facab55d4fdb2
Huevos <rowland@moralet.com>
2023-09-24 01:20:16 +0200

    [ServicePosition] unused variable

--- lib/python/Components/Converter/ServicePosition.py
+++ lib/python/Components/Converter/ServicePosition.py
@@ -251,7 +251,6 @@ class ServicePosition(Poll, Converter):
 						except:
 							return ""
 				elif self.type == self.TYPE_REMAINING:
-					test = 0
 					if config.usage.swap_time_remaining_on_osd.value == "1":  # Elapsed
 						try:
 							return sign_p + "%d%%" % ((float(p + 0.0) / float(l + 0.0)) * 100)
