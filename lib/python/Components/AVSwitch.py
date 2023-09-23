7ab607c6685c7c2ed72c2378270922374eae5fb1
Huevos <rowland@moralet.com>
2023-09-23 22:44:42 +0200

    [AVSwitch] unused imports

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -1,7 +1,7 @@
 from os import path
 
 from enigma import eAVSwitch, getDesktop
-from boxbranding import getBoxType, getBrandOEM, getHaveAVJACK, getHaveRCA, getHaveSCART, getHaveSCARTYUV, getHaveYUV
+from boxbranding import getBoxType, getBrandOEM
 
 from Components.config import ConfigBoolean, ConfigEnableDisable, ConfigNothing, ConfigSelection, ConfigSelectionNumber, ConfigSlider, ConfigSubDict, ConfigSubsection, ConfigYesNo, NoSave, config
 from Components.SystemInfo import SystemInfo
