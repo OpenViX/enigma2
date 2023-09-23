8a87587c7df0d8d2c29852d9ef125bf736b06101
Huevos <rowland@moralet.com>
2023-09-23 22:47:10 +0200

    [AnalogueClock] unused imports

--- lib/python/Components/Converter/AnalogClock2.py
+++ lib/python/Components/Converter/AnalogClock2.py
@@ -1,6 +1,6 @@
 from Components.Converter.Converter import Converter
 from Components.Element import cached
-from time import localtime, strftime
+from time import localtime
 
 
 class AnalogClock2(Converter):
