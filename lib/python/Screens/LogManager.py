391d0aa85f4a825d9eced96023f4216215c21606
Twol <tonywhalley@t-online.de>
2023-09-25 11:15:13 +0200

    remove unused imports ctime, Desktop

--- lib/python/Screens/LogManager.py
+++ lib/python/Screens/LogManager.py
@@ -1,8 +1,7 @@
 from os import path, remove, walk, stat, rmdir
-from time import time, ctime
+from time import time
 
-
-from enigma import eTimer, eBackgroundFileEraser, eLabel, getDesktop, gFont, fontRenderClass
+from enigma import eTimer, eBackgroundFileEraser, eLabel, gFont, fontRenderClass
 
 from Components.ActionMap import ActionMap
 from Components.Button import Button
@@ -159,7 +158,7 @@ class LogManager(Screen):
 		self["list"] = self.filelist
 		self["LogsSize"] = self.logsinfo = LogInfo(config.crash.debug_path.value, LogInfo.USED, update=False)
 		self.onLayoutFinish.append(self.layoutFinished)
-		if not self.selectionChanged in self["list"].onSelectionChanged:
+		if self.selectionChanged not in self["list"].onSelectionChanged:
 			self["list"].onSelectionChanged.append(self.selectionChanged)
 
 	def createSummary(self):
