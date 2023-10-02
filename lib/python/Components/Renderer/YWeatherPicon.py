af72dc62196dab706444be69abf64b04eb31a480
Twol <tonywhalley@t-online.de>
2023-10-02 11:26:55 +0200

    [YWeatherPicon] remove from config import *, as cannot see config used

--- lib/python/Components/Renderer/YWeatherPicon.py
+++ lib/python/Components/Renderer/YWeatherPicon.py
@@ -1,10 +1,9 @@
-#(c) 2boom mod 2012
+# (c) 2boom mod 2012
 
 from Components.Renderer.Renderer import Renderer
 from enigma import ePixmap, eTimer
 from Tools.Directories import fileExists, SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, resolveFilename
 from Tools.LoadPixmap import LoadPixmap
-from Components.config import *  # This needs fixing. We need to know what we are importing.
 
 
 class YWeatherPicon(Renderer):
