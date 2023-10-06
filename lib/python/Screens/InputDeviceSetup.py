fc3f929ce8f54c62085475e245212f1c5e67d104
Huevos <rowland@moralet.com>
2023-10-07 00:42:52 +0200

    [InputDeviceSetup] pep noise

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -50,11 +50,10 @@ class InputDeviceSelection(Screen, HelpableScreen):
 
 	def buildInterfaceList(self, device, description, type, isinputdevice=True):
 		divpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, "div-h.png"))
-		activepng = None
 		devicepng = None
 		enabled = iInputDevices.getDeviceAttribute(device, 'enabled')
 		# print("[InputDevice] device = %s, description = %s, type = %s, isinputdevice = %s, enabled = %s" % (device, description, type, isinputdevice, enabled))
-		if type == None:
+		if type is None:
 			devicepng = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/input_rcold-configured.png"))
 		elif type == 'remote':
 			if config.misc.rcused.value == 0:
@@ -83,7 +82,7 @@ class InputDeviceSelection(Screen, HelpableScreen):
 
 	def updateList(self):
 		self.list = []
-#		print("[InputDevice] iRcTypeControl.multipleRcSupported = {}".format(iRcTypeControl.multipleRcSupported()))
+		# print("[InputDevice] iRcTypeControl.multipleRcSupported = {}".format(iRcTypeControl.multipleRcSupported()))
 
 		if iRcTypeControl.multipleRcSupported():
 			self.list.append(self.buildInterfaceList('rctype', _('Select to configure remote control type'), None, False))
@@ -91,16 +90,16 @@ class InputDeviceSelection(Screen, HelpableScreen):
 		for x in self.devices:
 			dev_type = iInputDevices.getDeviceAttribute(x[1], 'type')
 			self.list.append(self.buildInterfaceList(x[1], _(x[0]), dev_type))
-#		print("[InputDevice] list = {}, self.currentIndex = {}".format(self.list, self.currentIndex))
+		# print("[InputDevice] list = {}, self.currentIndex = {}".format(self.list, self.currentIndex))
 		self["list"].setList(self.list)
 		self["list"].setIndex(self.currentIndex)
 
 	def okbuttonClick(self):
 		selection = self["list"].getCurrent()
 		self.currentIndex = self["list"].getIndex()
-#		print "[InputDevice] selection = {}".format(selection)
+		# print "[InputDevice] selection = {}".format(selection)
 		if selection is not None:
-#			print("[InputDevice] selection[0] = {}".format(selection[0]))
+			# print("[InputDevice] selection[0] = {}".format(selection[0]))
 			if selection[0] == 'rctype':
 				self.session.open(RemoteControlType)
 			else:
@@ -129,7 +128,7 @@ class InputDeviceSetup(ConfigListScreen, Screen):
 		self["introduction"] = StaticText()
 
 		# for generating strings into .po only
-		devicenames = [_("%s %s front panel") % (getMachineBrand(), getMachineName()), _("%s %s front panel") % (getMachineBrand(), getMachineName()), _("%s %s remote control (native)") % (getMachineBrand(), getMachineName()), _("%s %s advanced remote control (native)") % (getMachineBrand(), getMachineName()), _("%s %s ir keyboard") % (getMachineBrand(), getMachineName()), _("%s %s ir mouse") % (getMachineBrand(), getMachineName())]
+		devicenames = [_("%s %s front panel") % (getMachineBrand(), getMachineName()), _("%s %s remote control (native)") % (getMachineBrand(), getMachineName()), _("%s %s advanced remote control (native)") % (getMachineBrand(), getMachineName()), _("%s %s ir keyboard") % (getMachineBrand(), getMachineName()), _("%s %s ir mouse") % (getMachineBrand(), getMachineName())]  # noqa: F841
 
 		self.createSetup()
 		self.onLayoutFinish.append(self.layoutFinished)
@@ -167,7 +166,7 @@ class InputDeviceSetup(ConfigListScreen, Screen):
 				self["config"].invalidate(self.nameEntry)
 
 		self["config"].list = self.list
-		if not self.selectionChanged in self["config"].onSelectionChanged:
+		if self.selectionChanged not in self["config"].onSelectionChanged:
 			self["config"].onSelectionChanged.append(self.selectionChanged)
 		self.selectionChanged()
 
@@ -218,73 +217,73 @@ class RemoteControlType(ConfigListScreen, Screen):
 		odinRemote = "MaraM9"
 
 	rcList = [
-			("0", _("Default")),
-			("3", _(odinRemote)),
-			("4", _("DMM normal")),
-			("5", _("et9000/et9100")),
-			("6", _("DMM advanced")),
-			("7", _("et5000/6000")),
-			("8", _("VU+")),
-			("9", _("et8000/et10000")),
-			("11", _("et9200/9500/6500")),
-			("13", _("et4000")),
-			("14", _("XP1000")),
-			("16", _("HD11/HD51/HD1100/HD1200/HD1265/HD1500/HD500C/HD530C/et7x00/et8500")),
-			("17", _("XP3000")),
-			("18", _("F1/F3/F4/F4-TURBO/TRIPLEX")),
-			("19", _("HD2400")),
-			("20", _("Zgemma Star S/2S/H1/H2")),
-			("21", _("Zgemma H.S/H.2S/H.2H/H5")),
-			("22", _("Zgemma i55")),
-			("23", _("WWIO 4K")),
-			("24", _("Axas E4HD Ultra")),
-			("25", _("Zgemma H9/I55Plus old Model")),
-			("26", _("Protek 4K UHD/HD61")),
-			("27", _("HD60")),
-			("28", _("H7/H9/H9COMBO/H10 new Model"))
-			]
+		("0", _("Default")),
+		("3", _(odinRemote)),
+		("4", _("DMM normal")),
+		("5", _("et9000/et9100")),
+		("6", _("DMM advanced")),
+		("7", _("et5000/6000")),
+		("8", _("VU+")),
+		("9", _("et8000/et10000")),
+		("11", _("et9200/9500/6500")),
+		("13", _("et4000")),
+		("14", _("XP1000")),
+		("16", _("HD11/HD51/HD1100/HD1200/HD1265/HD1500/HD500C/HD530C/et7x00/et8500")),
+		("17", _("XP3000")),
+		("18", _("F1/F3/F4/F4-TURBO/TRIPLEX")),
+		("19", _("HD2400")),
+		("20", _("Zgemma Star S/2S/H1/H2")),
+		("21", _("Zgemma H.S/H.2S/H.2H/H5")),
+		("22", _("Zgemma i55")),
+		("23", _("WWIO 4K")),
+		("24", _("Axas E4HD Ultra")),
+		("25", _("Zgemma H9/I55Plus old Model")),
+		("26", _("Protek 4K UHD/HD61")),
+		("27", _("HD60")),
+		("28", _("H7/H9/H9COMBO/H10 new Model"))
+	]
 
 	defaultRcList = [
-			("default", 0),
-			("et4000", 13),
-			("et5000", 7),
-			("et6000", 7),
-			("et6500", 11),
-			("et7x00", 16),
-			("et8000", 9),
-			("et8500", 16),
-			("et9000", 5),
-			("et9100", 5),
-			("et9200", 11),
-			("et9500", 11),
-			("et10000", 9),
-			("formuler1", 18),
-			("formuler3", 18),
-			("hd11", 16),
-			("hd51", 16),
-			("hd52", 16),
-			("hd1100", 16),
-			("hd1200", 16),
-			("hd1265", 16),
-			("hd500c", 16),
-			("hd530c", 16),
-			("hd2400", 19),
-			("h3", 21),
-			("h5", 21),
-			#("h7", 21),# old model
-			("i55", 22),
-			("bre2ze4k", 23),
-			("e4hd", 24),
-			#("h9", 25),# old model
-			("i55plus", 25),
-			("protek4k", 26),
-			("hd61", 26),
-			("hd60", 27),
-			("h7", 28),  # new model
-			("h9", 28),  # new model
-			("h9combo", 28),
-			("h10", 28)
-		]
+		("default", 0),
+		("et4000", 13),
+		("et5000", 7),
+		("et6000", 7),
+		("et6500", 11),
+		("et7x00", 16),
+		("et8000", 9),
+		("et8500", 16),
+		("et9000", 5),
+		("et9100", 5),
+		("et9200", 11),
+		("et9500", 11),
+		("et10000", 9),
+		("formuler1", 18),
+		("formuler3", 18),
+		("hd11", 16),
+		("hd51", 16),
+		("hd52", 16),
+		("hd1100", 16),
+		("hd1200", 16),
+		("hd1265", 16),
+		("hd500c", 16),
+		("hd530c", 16),
+		("hd2400", 19),
+		("h3", 21),
+		("h5", 21),
+		# ("h7", 21),# old model
+		("i55", 22),
+		("bre2ze4k", 23),
+		("e4hd", 24),
+		# ("h9", 25),# old model
+		("i55plus", 25),
+		("protek4k", 26),
+		("hd61", 26),
+		("hd60", 27),
+		("h7", 28),  # new model
+		("h9", 28),  # new model
+		("h9combo", 28),
+		("h10", 28)
+	]
 
 	def __init__(self, session):
 		Screen.__init__(self, session)
@@ -318,12 +317,12 @@ class RemoteControlType(ConfigListScreen, Screen):
 			if x[0] in boxtype or x[0] in procBoxtype:
 				self.defaultRcType = x[1]
 				break
-# If there is none in the list, use the current value...
-#
-#		print("[InputDevice] self.defaultRcType 1 = {}".format(self.defaultRcType))
+		# If there is none in the list, use the current value...
+		#
+		# print("[InputDevice] self.defaultRcType 1 = {}".format(self.defaultRcType))
 		if self.defaultRcType == 0:
 			self.defaultRcType = iRcTypeControl.readRcType()
-#		print("[InputDevice] self.defaultRcType 2 = {}".format(self.defaultRcType))
+		# print("[InputDevice] self.defaultRcType 2 = {}".format(self.defaultRcType))
 
 	def setDefaultRcType(self):
 		iRcTypeControl.writeRcType(self.defaultRcType)
