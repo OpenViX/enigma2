015b51f783164cd61c4e2106fa1259fca1e28ed8
Huevos <rowland@moralet.com>
2023-10-06 16:22:22 +0200

    [Pixmap] allows the image in ePixmap to be set directly from Pixmap.py if required
    Author: @DimitarCC

--- lib/python/Components/Pixmap.py
+++ lib/python/Components/Pixmap.py
@@ -19,6 +19,9 @@ class Pixmap(GUIComponent):
 		s = self.instance.size()
 		return s.width(), s.height()
 
+	def setPixmap(self, pixmap):
+		self.instance.setPixmap(pixmap)
+
 	def applySkin(self, desktop, screen):
 		if self.skinAttributes is not None:
 			attribs = []
