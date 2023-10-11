a99b67a54bef79173452e6946703c85008965df0
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-10-11 09:59:43 +0300

    - make possible to get max text width among the items of Listbox based lists (an provide it in python)

--- lib/gui/elistboxcontent.h
+++ lib/gui/elistboxcontent.h
@@ -20,6 +20,7 @@ public:
 	void invalidateEntry(int index);
 	void invalidate();
 	eSize getItemSize() { return m_itemsize; }
+	int getMaxItemTextWidth() { return m_max_text_width; }
 #ifndef SWIG
 protected:
 	void cursorHome();
@@ -53,6 +54,7 @@ protected:
 	ePtr<gFont> m_font;
 	int m_itemheight;
 	int m_itemwidth;
+	int m_max_text_width;
 	int m_orientation;
 #endif
 };
