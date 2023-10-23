dbd018053fd78772e09a77a4e1800ae145f0e492
Twol <tonywhalley@t-online.de>
2023-10-23 14:47:43 +0200

    [TimerEntry] - fix indented code, unused imports, PEP8 complaints

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from time import localtime, time, strftime
+from time import time
 
 from enigma import eEPGCache
 
@@ -36,7 +35,7 @@ class TimerEntry(TimerEntryBase):
 			AFTEREVENT.DEEPSTANDBY: "deepstandby",
 			AFTEREVENT.STANDBY: "standby",
 			AFTEREVENT.AUTO: "auto"
-			}[self.timer.afterEvent]
+			}[self.timer.afterEvent]  # noqa: E123
 
 		if self.timer.record_ecm and self.timer.descramble:
 			recordingtype = "descrambled+ecm"
@@ -59,10 +58,10 @@ class TimerEntry(TimerEntryBase):
 		self.timerentry_tags = self.timer.tags[:]
 		# if no tags found, make name of event default tag set.
 		if not self.timerentry_tags:
-				tagname = self.timer.name.strip()
-				if tagname:
-					tagname = tagname[0].upper() + tagname[1:].replace(" ", "_")
-					self.timerentry_tags.append(tagname)
+			tagname = self.timer.name.strip()
+			if tagname:
+				tagname = tagname[0].upper() + tagname[1:].replace(" ", "_")
+				self.timerentry_tags.append(tagname)
 
 		self.timerentry_tagsset = ConfigSelection(choices=[not self.timerentry_tags and "None" or " ".join(self.timerentry_tags)])
 
@@ -161,10 +160,9 @@ class TimerEntry(TimerEntryBase):
 
 	def selectChannelSelector(self, *args):
 		self.session.openWithCallback(
-				self.finishedChannelSelectionCorrection,
-				Screens.ChannelSelection.SimpleChannelSelection,
-				_("Select channel to record from")
-			)
+			self.finishedChannelSelectionCorrection,
+			Screens.ChannelSelection.SimpleChannelSelection,
+			_("Select channel to record from"))
 
 	def finishedChannelSelectionCorrection(self, *args):
 		if args:
@@ -190,7 +188,7 @@ class TimerEntry(TimerEntryBase):
 			"deepstandby": AFTEREVENT.DEEPSTANDBY,
 			"standby": AFTEREVENT.STANDBY,
 			"auto": AFTEREVENT.AUTO
-			}[self.timerentry_afterevent.value]
+			}[self.timerentry_afterevent.value]  # noqa: E123
 		# There is no point doing anything after a Zap-only timer!
 		# For a start, you can't actually configure anything in the menu, but
 		# leaving it as AUTO means that the code may try to shutdown at Zap time
@@ -202,12 +200,12 @@ class TimerEntry(TimerEntryBase):
 			"normal": True,
 			"descrambled+ecm": True,
 			"scrambled+ecm": False,
-			}[self.timerentry_recordingtype.value]
+			}[self.timerentry_recordingtype.value]  # noqa: E123
 		self.timer.record_ecm = {
 			"normal": False,
 			"descrambled+ecm": True,
 			"scrambled+ecm": True,
-			}[self.timerentry_recordingtype.value]
+			}[self.timerentry_recordingtype.value]  # noqa: E123
 		self.timer.service_ref = self.timerentry_service_ref
 		self.timer.tags = self.timerentry_tags
 
