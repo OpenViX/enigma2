6d49677bd702a4e2da2d87a1a779cc114d2b2fd4
Twol <tonywhalley@t-online.de>
2023-10-23 14:54:44 +0200

    [StartWizard] - PEP8 complaints

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,6 +1,6 @@
 from Components.config import config, ConfigBoolean, configfile
 from Components.Pixmap import Pixmap
-from Screens.LanguageSelection import LanguageWizard
+from Screens.LanguageSelection import LanguageWizard  # noqa: F401
 from Screens.Rc import Rc
 from Screens.WizardLanguage import WizardLanguage
 from Screens.WizardUserInterfacePositioner import UserInterfacePositionerWizard
@@ -37,7 +37,7 @@ class StartWizard(WizardLanguage, Rc):
 		configfile.save()
 
 
-#wizardManager.registerWizard(VideoWizard, config.misc.Vuwizardenabled.value, priority=2)
+# wizardManager.registerWizard(VideoWizard, config.misc.Vuwizardenabled.value, priority=2)
 wizardManager.registerWizard(VuWizard, config.misc.Vuwizardenabled.value, priority=3)
 wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.value, priority=10)
 wizardManager.registerWizard(UserInterfacePositionerWizard, config.misc.firstrun.value, priority=20)
