62ea2e571af246e7301880e3432f8236e87e09f0
Twol <tonywhalley@t-online.de>
2023-10-24 16:32:13 +0200

    [Canvas] don,t use variable l PEP8

--- lib/python/Components/Renderer/Canvas.py
+++ lib/python/Components/Renderer/Canvas.py
@@ -29,17 +29,17 @@ class Canvas(Renderer):
 		self.draw_count = len(list[1])
 
 	def draw(self, list):
-		for l in list:
-			if l[0] == 1:
-				self.instance.fillRect(eRect(l[1], l[2], l[3], l[4]), gRGB(l[5]))
-			elif l[0] == 2:
-				self.instance.writeText(eRect(l[1], l[2], l[3], l[4]), gRGB(l[5]), gRGB(l[6]), l[7], l[8], l[9])
-			elif l[0] == 3:
-				self.instance.drawLine(l[1], l[2], l[3], l[4], gRGB(l[5]))
-			elif l[0] == 4:
-				self.instance.drawRotatedLine(l[1], l[2], l[3], l[4], l[5], l[6], l[7], l[8], gRGB(l[9]))
+		for element in list:
+			if element[0] == 1:
+				self.instance.fillRect(eRect(element[1], element[2], element[3], element[4]), gRGB(element[5]))
+			elif element[0] == 2:
+				self.instance.writeText(eRect(element[1], element[2], element[3], element[4]), gRGB(element[5]), gRGB(element[6]), element[7], element[8], element[9])
+			elif element[0] == 3:
+				self.instance.drawLine(element[1], element[2], element[3], element[4], gRGB(element[5]))
+			elif element[0] == 4:
+				self.instance.drawRotatedLine(element[1], element[2], element[3], element[4], element[5], element[6], element[7], element[8], gRGB(element[9]))
 			else:
-				print("drawlist entry:", l)
+				print("drawlist entry:", element)
 				raise RuntimeError("invalid drawlist entry")
 
 	def changed(self, what):
