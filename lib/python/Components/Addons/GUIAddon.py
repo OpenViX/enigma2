31f682f9fa0d43e436c4159c0692a4960298801a
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-10-26 15:01:49 +0300

    - added new addon for dynamic button sequences

--- lib/python/Components/Addons/GUIAddon.py
+++ lib/python/Components/Addons/GUIAddon.py
@@ -4,9 +4,17 @@ from Components.GUIComponent import GUIComponent
 class GUIAddon(GUIComponent):
 	def __init__(self):
 		GUIComponent.__init__(self)
+		self.sources = {}
 
 	def connectRelatedElement(self, relatedElementName, container):
-		self.source = container[relatedElementName]
+		relatedElementNames = relatedElementName.split(",")
+		if len(relatedElementNames) == 1:
+			self.source = container[relatedElementName]
+		elif len(relatedElementNames) > 1:
+			for x in relatedElementNames:
+				x = x.strip()
+				if x in container:
+					self.sources[x] = container[x]
 		container.onShow.append(self.onContainerShown)
 
 	def onContainerShown(self):
