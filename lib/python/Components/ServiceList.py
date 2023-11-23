8599107d92ba5b21582dd22672f98b0947951f52
Ev0-BH <Ev0_BhT@outlook.com>
2023-11-23 15:02:31 +0000

    [ServiceList] Fix for old 720 skins

--- lib/python/Components/ServiceList.py
+++ lib/python/Components/ServiceList.py
@@ -244,7 +244,7 @@ class ServiceList(GUIComponent):
 		def serviceItemHeight(value):  # for legacy support
 			itemHeight(value)
 
-		for (attrib, value) in sorted(self.skinAttributes, key=lambda x: 1 if x[0] == "itemHeight" else 0):
+		for (attrib, value) in sorted(self.skinAttributes, key=lambda x: 0 if x[0] == "itemHeight" else 1):
 			try:
 				locals().get(attrib)(value)
 				self.skinAttributes.remove((attrib, value))
