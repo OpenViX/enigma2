fab2c23233b6dac6ede79283e4325694de1a2b2f
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2024-03-13 12:21:52 +0200

    [Added] Possibility to detect which CI slot do the descrambling (for use to display that in skins later)

--- lib/dvb_ci/dvbci.h
+++ lib/dvb_ci/dvbci.h
@@ -243,7 +243,8 @@ public:
 			slotStateChanged,
 			mmiSessionDestroyed,
 			mmiDataReceived,
-			appNameChanged
+			appNameChanged,
+			slotDecodingStateChanged
 		};
 		int m_type;
 		int m_slotid;
