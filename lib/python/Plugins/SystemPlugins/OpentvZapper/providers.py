862bbc093a42edc8a21323c6ba9cc3276f965e97
nzepg <230123556+nzepg@users.noreply.github.com>
2025-09-07 22:18:06 +1200

    Update providers.py
    @Huevos Largley left astra 28.2 alone.. just added a provider name..
    I am curious that it works as there doesn't see to be a transponder frequency published for 11778000
    Previos attempts to commit a change to this file, before never changed any settings.. just added comments to end of each setting
    
    This change is mostly adding in new provider for NZ, this has been tested an works in NZ
    
    and also, drafting a provider for Hotbird 13.0, based on limited CrossEPG data. that someone will need to test and adjust.
    
    Someone may want to alter the other code, as astra 28.2 is hardcoded as the default provider, so if someone ways playing with their own providers file, they need to keep astra 28.2, else opentvzapper will crash
    
    I hope this is ok, maybe you can getsome one who uses 28.2 to test these chanegs don't impact them before closing the change

--- lib/python/Plugins/SystemPlugins/OpentvZapper/providers.py
+++ lib/python/Plugins/SystemPlugins/OpentvZapper/providers.py
@@ -40,7 +40,7 @@ providers = {
 			'inversion': 2,					# 0=Off, 1=On, 2=Auto
 			'symbol_rate': 30000000,		# ks/s
 			'namespace': 104857600,
-			'system': 1,					# 0 = DVB-S, 1 = DVB-S2
+			'system': 1,					# 0 = DVB-S, 1 =DVB-S2
 			'polarization': 0,				# 0 = H, 1 = V
 			'original_network_id': 169,
 			'fec_inner': 7,					# 0=Auto, 1=1/2, 2=2/3, 3=3/4, 4=5/6, 5=7/8, 6=8/9, 7=3/5, 8=4/5, 9=9/10, 10=6/7, 15=None
