d6c2de9ed53f1638f7e61d2503b4234f1abe0f1c
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2025-09-30 13:25:12 +0300

    [Updated] Some rating renderer adjustments

--- lib/python/Components/Renderer/RatingIconLabel.py
+++ lib/python/Components/Renderer/RatingIconLabel.py
@@ -14,26 +14,30 @@ class RatingIconLabel(Renderer):
 		self.changed((self.CHANGED_DEFAULT,))
 
 	def applySkin(self, desktop, parent):
-		attribs = self.skinAttributes[:]
+		attribs = []
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "colors":
 				self.colors = {int(k): parseColor(v) for k, v in (item.split(":") for item in value.split(","))}
+			else:
+				attribs.append((attrib, value))
 		self.skinAttributes = attribs
 		rc = Renderer.applySkin(self, desktop, parent)
 		self.changed((self.CHANGED_DEFAULT,))
 		return rc
 
+	def hideLabel(self):
+		self.instance.setText("")
+		self.instance.hide()
+
 	def changed(self, what):
 		if self.source and hasattr(self.source, "text") and self.instance:
 			if what[0] == self.CHANGED_CLEAR:
-				self.instance.setText("")
-				self.instance.setPixmap(None)
+				self.hideLabel()
 			else:
 				if self.source.text:
 					age = int(self.source.text.replace("+", ""))
 					if age == 0:
-						self.instance.setText("")
-						self.instance.hide()
+						self.hideLabel()
 						return
 					if age <= 15:
 						age += 3
@@ -42,5 +46,4 @@ class RatingIconLabel(Renderer):
 					self.instance.setBackgroundColor(gRGB(self.colors.get(age, 0x10000000)))
 					self.instance.show()
 				else:
-					self.instance.setText("")
-					self.instance.hide()
+					self.hideLabel()
