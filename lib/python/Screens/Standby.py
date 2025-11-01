116304c3ecfc79182851ad38392de0501b325330
Huevos <rowland@moralet.com>
2025-11-01 10:30:01 +0100

    [Screens.Standby] sanity

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -124,7 +124,7 @@ class Standby2(Screen):
 			else:
 				self.timeHandler.m_timeUpdated.get().append(self.stopService)
 
-		if self.session.pipshown:
+		if hasattr(self.session, "pipshown") and self.session.pipshown:
 			self.infoBarInstance and hasattr(self.infoBarInstance, "showPiP") and self.infoBarInstance.showPiP()
 
 		if SystemInfo["ScartSwitch"]:
