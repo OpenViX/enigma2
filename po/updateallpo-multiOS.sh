aa4d88b1a449f90dda4a67c1538eed4167a87016
Huevos <rowland@moralet.com>
2026-02-06 20:13:16 +0100

    [updateallpo-multiOS.sh] add https://github.com/OpenViX/enigma2/pull/1218/changes/dc1477e94c070ea1491c9f2b545562d08299a95e

--- po/updateallpo-multiOS.sh
+++ po/updateallpo-multiOS.sh
@@ -165,6 +165,12 @@ languages=($(gawk ' BEGIN { FS=" " }
 # If you want to define the language locally in this script uncomment and defined languages
 #languages=("ar" "bg" "ca" "cs" "da" "de" "el" "en" "es" "et" "fa" "fi" "fr" "fy" "he" "hk" "hr" "hu" "id" "is" "it" "ku" "lt" "lv" "nl" "nb" "nn" "pl" "pt" "pt_BR" "ro" "ru" "sk" "sl" "sr" "sv" "th" "tr" "uk" "zh")
 
+# Check if a specific language was provided as argument
+if [ -n "$1" ]; then
+	languages=("$1")
+	printf "Processing only language: %s\n" "$1"
+fi
+
 #
 # Arguments to generate the pot and po files are not retrieved from the Makefile.
 # So if parameters are changed in Makefile please report the same changes in this script.
@@ -174,6 +180,7 @@ printf "Creating temporary file enigma2-py.pot\n"
 find $findoptions .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d enigma2 -s -o enigma2-py.pot {} \+
 "$localgsed" --in-place enigma2-py.pot --expression=s/CHARSET/UTF-8/
 printf "Creating temporary file enigma2-xml.pot\n"
+export PYTHONIOENCODING=utf-8
 find $findoptions .. -name "*.xml" -exec "$python" "$xml2po" {} \+ > enigma2-xml.pot
 printf "Merging pot files to create: enigma2.pot\n"
 cat enigma2-py.pot enigma2-xml.pot | msguniq --no-wrap --no-location -o enigma2.pot -
@@ -182,8 +189,8 @@ IFS=" "
 for lang in "${languages[@]}" ; do
 	if [ -f $lang.po ]; then \
 		printf "Updating existing translation file %s.po\n" $lang
-		msgmerge --backup=none --no-wrap --no-location -s -U $lang.po enigma2.pot && touch $lang.po; \
-		msgattrib --no-wrap --no-obsolete $lang.po -o $lang.po; \
+		msgmerge --backup=none --no-wrap --no-location --no-fuzzy-matching -s -U $lang.po enigma2.pot && touch $lang.po; \
+		msgattrib --no-wrap --no-obsolete --no-fuzzy $lang.po -o $lang.po; \
 		msgfmt -o $lang.mo $lang.po; \
 	else \
 		printf "New file created: %s.po, please add it to github before commit\n" $lang
