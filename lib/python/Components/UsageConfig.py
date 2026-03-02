4c1f20b1da949f917beeadb20b74111dcb3dd5a6
TwolDE <tonywhalley@t-online.de>
2026-03-02 16:20:31 +0100

    [UsageConfig] set default for softcsa syncmode
    gb7252 receivers use sync not auto

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -11,7 +11,8 @@ from Tools.camcontrol import CamControl
 from Tools.Directories import resolveFilename, SCOPE_HDD, SCOPE_TIMESHIFT, defaultRecordingLocation
 from Components.NimManager import nimmanager
 from Components.ServiceList import refreshServiceList
-from Components.SystemInfo import SystemInfo
+from Components.SystemInfo import SystemInfo, MODEL
+
 
 # A raw writer for config changes to be read by the logger without
 # getting a time-stamp prepended.
@@ -1196,12 +1197,13 @@ def InitUsageConfig():
 	config.misc.softcamrestarts = ConfigSelection(default="", choices=[
 		("", _("Don't restart")),
 		("s", _("Restart softcam"))])
+	defaultValue = 1 if MODEL in ("gb7252", ) else 0
 	config.misc.softcsa = ConfigSubsection()
 	config.misc.softcsa.decoderRelease = ConfigSelection(default=0, choices=[
 			(0, _("Quick")),
 			(1, _("Normal"))
 	])
-	config.misc.softcsa.syncMode = ConfigSelection(default=0, choices=[
+	config.misc.softcsa.syncMode = ConfigSelection(default=defaultValue, choices=[
 			(0, _("Automatic")),
 			(1, _("Synchronous"))
 	])
