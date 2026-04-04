aa2e65823e785de8d61539d6dd176bb4bf330f3f
TwolDE <tonywhalley@t-online.de>
2026-04-04 12:05:17 +0200

    [servicemp3] - change useragent to better handle some streams

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -518,7 +518,7 @@ eServiceMP3::eServiceMP3(eServiceReference ref):
 	m_cuesheet_loaded = false; /* cuesheet CVR */
 	m_use_chapter_entries = false; /* TOC chapter support CVR */
 	m_last_seek_pos = 0; /* CVR last seek position */
-	m_useragent = "HbbTV/1.1.1 (+PVR+RTSP+DL; Sonic; TV44; 1.32.455; 2.002) Bee/3.5";
+	m_useragent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
 	m_extra_headers = "";
 	m_download_buffer_path = "";
 	m_prev_decoder_time = -1;
