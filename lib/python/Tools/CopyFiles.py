32b43f83d2e384a606a61dbeb9b8c53fe8a53226
Huevos <rowland@moralet.com>
2026-06-10 23:26:59 +0200

    [CopyFiles][download_progress] use "bytesToHumanReadable" for display output

--- lib/python/Tools/CopyFiles.py
+++ lib/python/Tools/CopyFiles.py
@@ -1,3 +1,4 @@
+from Components.Harddisk import bytesToHumanReadable
 from Components.Task import PythonTask, Task, Job, job_manager as JobManager, Condition
 from Tools.Directories import fileExists
 from enigma import eTimer
@@ -107,15 +108,12 @@ class DownloadTask(Task):
 
 	def download_progress(self, recvbytes, totalbytes):
 		if (recvbytes - self.last_recvbytes) > 100000:  # anti-flicker
-			self.progress = int(100 * (float(recvbytes) / float(totalbytes)))
-			if (((float(totalbytes) / 1024) / 1024) / 1024) >= 1:
-				self.name = _("Downloading") + ' ' + _("%s of %s GB") % (str(round((((float(recvbytes) / 1024) / 1024) / 1024), 2)), str(round((((float(totalbytes) / 1024) / 1024) / 1024), 2)))
-			elif ((float(totalbytes) / 1024) / 1024) >= 1:
-				self.name = _("Downloading") + ' ' + _("%s of %s MB") % (str(round(((float(recvbytes) / 1024) / 1024), 2)), str(round(((float(totalbytes) / 1024) / 1024), 2)))
-			elif (totalbytes / 1024) >= 1:
-				self.name = _("Downloading") + ' ' + _("%d of %d KB") % (recvbytes / 1024, totalbytes / 1024)
+			if totalbytes > 0:  # avoid ZeroDivisionError if content-length is not available
+				self.progress = int(100 * (float(recvbytes) / float(totalbytes)))
+				self.name = _("Downloading %s of %s") % (bytesToHumanReadable(recvbytes), bytesToHumanReadable(totalbytes))
 			else:
-				self.name = _("Downloading") + ' ' + _("%d of %d Bytes") % (recvbytes, totalbytes)
+				self.progress = 0  # required to force display update
+				self.name = _("Downloading %s") % bytesToHumanReadable(recvbytes)
 			self.last_recvbytes = recvbytes
 
 	def download_failed(self, failure_instance=None, error_message=""):
