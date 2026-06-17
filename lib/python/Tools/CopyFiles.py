OpenViX/enigma2

commit 2709d13bb2ad03fecbd653f7a35837ba84ab8fd1

[CopyFiles] add DownloadTask.

lib/python/Tools/CopyFiles.py
@@ -1,4 +1,4 @@
-from Components.Task import PythonTask, Task, Job, job_manager as JobManager
+from Components.Task import PythonTask, Task, Job, job_manager as JobManager, Condition
 from Tools.Directories import fileExists
 from enigma import eTimer
 from os import path
@@ -57,6 +57,63 @@ class AddFileProcessTask(Task):
 		self.ProgressTimer.stop()
 
 
+class DownloadProcessTask(Job):
+	def __init__(self, url, filename, file):
+		Job.__init__(self, _("%s") % file)
+		DownloadTask(self, url, filename)
+
+class DownloaderPostcondition(Condition):
+	def check(self, task):
+		return task.returncode == 0
+
+	def getErrorMessage(self, task):
+		return self.error_message
+
+class DownloadTask(Task):
+	def __init__(self, job, url, path):
+		Task.__init__(self, job, _("Downloading"))
+		self.postconditions.append(DownloaderPostcondition())
+		self.job = job
+		self.url = url
+		self.path = path
+		self.error_message = ""
+		self.last_recvbytes = 0
+		self.error_message = None
+		self.download = None
+		self.aborted = False
+
+	def run(self, callback):
+		from Tools.Downloader import downloadWithProgress
+		self.callback = callback
+		self.download = downloadWithProgress(self.url,self.path)
+		self.download.addProgress(self.download_progress)
+		self.download.start().addCallback(self.download_finished).addErrback(self.download_failed)
+		print "[DownloadTask] downloading", self.url, "to", self.path
+
+	def abort(self):
+		print "[DownloadTask] aborting", self.url
+		if self.download:
+			self.download.stop()
+		self.aborted = True
+
+	def download_progress(self, recvbytes, totalbytes):
+		if ( recvbytes - self.last_recvbytes  ) > 100000: # anti-flicker
+			self.progress = int(100*(float(recvbytes)/float(totalbytes)))
+			self.name = _("Downloading") + ' ' + _("%d of %d kBytes") % (recvbytes/1024, totalbytes/1024)
+			self.last_recvbytes = recvbytes
+
+	def download_failed(self, failure_instance=None, error_message=""):
+		self.error_message = error_message
+		if error_message == "" and failure_instance is not None:
+			self.error_message = failure_instance.getErrorMessage()
+		Task.processFinished(self, 1)
+
+	def download_finished(self, string=""):
+		if self.aborted:
+			self.finish(aborted = True)
+		else:
+			Task.processFinished(self, 0)
+
 def copyFiles(fileList, name):
 	for src, dst in fileList:
 		if path.isdir(src) or int(path.getsize(src))/1000/1000 > 100:
@@ -76,3 +133,6 @@ def deleteFiles(fileList, name):
 	task = DeleteFolderTask(job, name)
 	task.openFiles(fileList)
 	JobManager.AddJob(job)
+
+def downloadFile(url, file_name, sel):
+	JobManager.AddJob(DownloadProcessTask(url, file_name, sel))

commit c6a3ed0e07b58f995bb130f64dc1c272ebeb781c

[CopyFiles.DownloaderPostcondition] fix a since-forever bug Introduced by https://github.com/OpenViX/enigma2/commit/2709d13bb2ad03fecbd653f7a35837ba84ab8fd1
Incorrect object referenced in DownloaderPostcondition, "self", should be "task". If download fails this leads to blocking the Task queue until next reboot.

With the fix the Task exits correctly and moves to the next item in the queue, if any.

lib/python/Tools/CopyFiles.py
@@ -73,7 +73,7 @@ class DownloaderPostcondition(Condition):
 		return task.returncode == 0
 
 	def getErrorMessage(self, task):
-		return self.error_message
+		return task.error_message or ""
 
 
 class DownloadTask(Task):
@@ -84,7 +84,6 @@ class DownloadTask(Task):
 		self.job = job
 		self.url = url.decode() if isinstance(url, bytes) else url
 		self.path = path
-		self.error_message = ""
 		self.error_message = None
 		self.download = None
 		self.aborted = False