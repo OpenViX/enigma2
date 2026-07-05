359259d3de2ac9289560d94cdbb76bac442fc2b9
Huevos <rowland@moralet.com>
2026-07-06 00:41:47 +0200

    [NetworkTime] use ClockRealtimeMonitor to trigger callbacks

--- lib/python/Components/NetworkTime.py
+++ lib/python/Components/NetworkTime.py
@@ -3,9 +3,7 @@ from Components.config import config
 from enigma import eTimer, eDVBLocalTimeHandler, eEPGCache
 from Tools.StbHardware import setRTCtime
 from time import time
-
-# _session = None
-#
+import Components.ClockRealtimeMonitor
 
 
 def AutoNTPSync(session=None, **kwargs):
@@ -21,11 +19,8 @@ class NTPSyncPoller:
 		# Init Timer
 		self.timer = eTimer()
 		self.Console = Console()
-		self.previous = 0
 		self.onTimeUpdated = []
-		timeHandlerCallbacks = eDVBLocalTimeHandler.getInstance().m_timeUpdatedMinutes.get()
-		if self._timeUpdated not in timeHandlerCallbacks:
-			timeHandlerCallbacks.append(self._timeUpdated)
+		Components.ClockRealtimeMonitor.realtimeMonitor.addRealtimeChangedCallback(self._timeUpdated)
 
 	def start(self):
 		if self.timecheck not in self.timer.callback:
@@ -40,11 +35,6 @@ class NTPSyncPoller:
 	def timecheck(self):
 		if config.misc.SyncTimeUsing.value == "ntp":
 			print('[NetworkTime] Updating from NTP')
-			self.previous = time()
-			# ntpd from BusyBox.
-			# -n = Run in foreground
-			# -q = Quit after clock is set
-			# -p [keyno:NUM:]PEER... Obtain time from PEER (may be repeated)... Use key NUM for authentication... If -p is not given, 'server HOST' lines from /etc/ntp.conf are used.
 			self.Console.ePopen(["/usr/sbin/ntpd", "/usr/sbin/ntpd", "-nq", "-p", config.misc.NTPserver.value], self.update_schedule)
 		else:
 			self.update_schedule()
@@ -59,8 +49,6 @@ class NTPSyncPoller:
 			eDVBLocalTimeHandler.getInstance().setUseDVBTime(config.misc.SyncTimeUsing.value == "dvb")
 			eEPGCache.getInstance().timeUpdated()
 			self.timer.startLongTimer(int(config.misc.useNTPminutes.value if config.misc.SyncTimeUsing.value == "ntp" else config.misc.useNTPminutes.default) * 60)
-			if config.misc.SyncTimeUsing.value == "ntp" and abs(time() - self.previous) > 60:
-				self._timeUpdated("NTP")
 		else:
 			print('[NetworkTime] NO TIME SET')
 			self.timer.startLongTimer(10)
@@ -69,8 +57,8 @@ class NTPSyncPoller:
 		self.timer.stop()  # stop current timer if this is an update from Time.py
 		self.timer.startLongTimer(0)
 
-	def _timeUpdated(self, using="eDVBLocalTimerHandler"):
-		print("[NetworkTime] system clock was updated by", using)
+	def _timeUpdated(self):
+		print("[NetworkTime] system clock was stepped")
 		for f in self.onTimeUpdated:
 			if callable(f):
 				f()
@@ -84,6 +72,7 @@ class NTPSyncPoller:
 			self.onTimeUpdated.remove(f)
 
 	def __del__(self):
-		timeHandlerCallbacks = eDVBLocalTimeHandler.getInstance().m_timeUpdated.get()
-		if self._timeUpdated in timeHandlerCallbacks:
-			timeHandlerCallbacks.remove(self._timeUpdated)
+		try:
+			Components.ClockRealtimeMonitor.realtimeMonitor.removeRealtimeChangedCallback(self._timeUpdated)
+		except Exception:
+			pass
