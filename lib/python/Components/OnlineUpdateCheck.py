16cca75fc76c8987a3ee8036918e99cde6a67d03
Huevos <rowland@moralet.com>
2026-07-06 14:29:02 +0200

    [OnlineUpdateCheck] increase timeout when checking for software updates

--- lib/python/Components/OnlineUpdateCheck.py
+++ lib/python/Components/OnlineUpdateCheck.py
@@ -82,7 +82,7 @@ class FeedsStatusCheck:
 					try:
 						print("[OnlineUpdateCheck][getFeedStatus] checking feeds state")
 						req = Request("http://openvix.co.uk/TrafficLightState.php")
-						d = urlopen(req, timeout=3)
+						d = urlopen(req, timeout=10)
 						trafficLight = d.read().decode()
 						if trafficLight == "stable":
 							status = 0
