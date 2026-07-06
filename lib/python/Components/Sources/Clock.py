71d161d2b3d9f94c334dc468658da05ef4698f3a
Huevos <rowland@moralet.com>
2026-07-06 03:12:56 +0200

    [Clock] use ClockRealtimeMonitor and eTimer.startEpochAligned

--- lib/python/Components/Sources/Clock.py
+++ lib/python/Components/Sources/Clock.py
@@ -2,6 +2,8 @@ from time import time as getTime
 
 from enigma import eTimer
 
+import Components.ClockRealtimeMonitor
+
 from Components.Element import cached
 from Components.Sources.Source import Source
 
@@ -11,7 +13,8 @@ class Clock(Source):
 		Source.__init__(self)
 		self.clock_timer = eTimer()
 		self.clock_timer.callback.append(self.poll)
-		self.clock_timer.start(1000)
+		self.clock_timer.startEpochAligned(1000)
+		Components.ClockRealtimeMonitor.realtimeMonitor.addRealtimeChangedCallback(self._timeUpdated)
 
 	@cached
 	def getClock(self):
@@ -20,15 +23,22 @@ class Clock(Source):
 	time = property(getClock)
 
 	def poll(self):
+		print("[Clock] poll")  # temporary debug
 		self.changed((self.CHANGED_POLL,))
+
+	def _timeUpdated(self):
+		# Re-align after CLOCK_REALTIME discontinuity.
+		self.clock_timer.startEpochAligned(1000)
+		self.poll()
 
 	def doSuspend(self, suspended):
 		if suspended:
 			self.clock_timer.stop()
 		else:
-			self.clock_timer.start(1000)
+			self.clock_timer.startEpochAligned(1000)
 			self.poll()
 
 	def destroy(self):
+		Components.ClockRealtimeMonitor.realtimeMonitor.removeRealtimeChangedCallback(self._timeUpdated)
 		self.clock_timer.callback.remove(self.poll)
 		Source.destroy(self)
