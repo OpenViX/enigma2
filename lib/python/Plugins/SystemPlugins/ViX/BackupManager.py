a722e049fcee7c565f75e4425bf6e86edc086261
Huevos <rowland@moralet.com>
2026-07-13 02:04:19 +0200

    [BackupManager] remove dead code

--- lib/python/Plugins/SystemPlugins/ViX/BackupManager.py
+++ lib/python/Plugins/SystemPlugins/ViX/BackupManager.py
@@ -881,7 +881,6 @@ class AutoBackupManagerTimer:
 		self.backuptimer.stop()
 		now = int(time())
 		wake = self.getBackupTime()
-		atLeast = 0  # noqa: F841 if we're close enough, we're okay...
 		if wake - now < 60:
 			print("[BackupManager] Backup onTimer occured at", strftime("%c", localtime(now)))
 			from Screens.Standby import inStandby
