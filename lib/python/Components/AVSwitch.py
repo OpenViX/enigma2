6e88a6809390b2a9e6723366a7937cb894ef375b
TwolDE <tonywhalley@t-online.de>
2026-08-02 12:38:58 +0200

    [AVSwitch] modify  fileWriteLine call

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -539,7 +539,7 @@ def InitAVSwitch():
 	SystemInfo["havehdmihdrosd"] = bool(hdrOsd)
 	if hdrOsd:
 		def setHDMIHdrOsd(configElement):
-			fileWriteLine("/proc/stb/video/hdmi_hdr_osd", configElement.value, source=MODULE_NAME)
+			fileWriteLine("/proc/stb/video/hdmi_hdr_osd", configElement.value)
 
 		hdrOsdChoices = [
 			("32767 0 -16384", _("GigaBlue optimized")),
