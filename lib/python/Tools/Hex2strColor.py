e7afb2c9a11eded348b7b36052579eb9cf15458e
Huevos <rowland@moralet.com>
2026-08-05 03:19:09 +0200

    [Hex2strColor] add ColorizeText class

--- lib/python/Tools/Hex2strColor.py
+++ lib/python/Tools/Hex2strColor.py
@@ -1,6 +1,23 @@
 # Converts hex colors to formatted strings,
 # suitable for embedding in python code.
 
+from skin import parameters
+
 
 def Hex2strColor(rgb):
 	return r"\c%08x" % rgb  # noqa: W605
+
+
+class ColorizeText:
+	# wraps text in colors imported from skin <parameters>, e.g.
+	# <parameter name="AboutColors" value="#00ffc000"/>
+
+	def __init__(self, session, param_name):
+		self.colors = parameters.get(param_name, [])
+		if isinstance(self.colors, int):  # a single entry in skin parameters would not be comma separated and therefore an int, not a list
+			self.colors = [self.colors]
+
+	def addColor(self, text, i=0):
+		if i < len(self.colors):
+			text = Hex2strColor(self.colors[i]) + text + r"\C"
+		return text
