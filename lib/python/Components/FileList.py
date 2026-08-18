924a0d5d6ffecc15baacdfecdec746e4f413a5e5
Huevos <rowland@moralet.com>
2026-08-18 14:48:52 +0200

    [FileList] fix long-standing bug directory always has a trailing slash, whereas return value from "path.realpath" has no trailing slash, so this condition always returned False

--- lib/python/Components/FileList.py
+++ lib/python/Components/FileList.py
@@ -132,7 +132,7 @@ class FileList(MenuList):
 		return False
 
 	def getMountpointLink(self, directory):
-		if path.realpath(directory) == directory:
+		if path.realpath(directory) == path.normpath(directory):  # not a symlink
 			return self.getMountpoint(directory)
 		else:
 			if directory[-1] == "/":
